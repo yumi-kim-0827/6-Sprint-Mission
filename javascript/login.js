c71698144dbb9d01fecb52b386944f60dff97fda
JaesungKo <jaesungko.dev@gmail.com>
2024-03-29 17:17:41 +0900

    feat: eye 아이콘 토글 적용

--- javascript/login.js
+++ javascript/login.js
@@ -1,6 +1,7 @@
 const loginEmail = document.getElementById("email");
 const loginPw = document.getElementById("password");
 const loginBtn = document.getElementById("btn-login");
+const togglePw = document.getElementById("password-toggle");
 
 function color() {
   if (
@@ -17,5 +18,16 @@ function color() {
   }
 }
 
+function toggle() {
+  if (loginPw.type === "password") {
+    loginPw.type = "text";
+    togglePw.src = "/images/btn_visibility_on.png";
+  } else {
+    loginPw.type = "password";
+    togglePw.src = "/images/btn_visibility_off.png";
+  }
+}
+
 loginEmail.addEventListener("keyup", color);
 loginPw.addEventListener("keyup", color);
+togglePw.addEventListener("click", toggle);
--- javascript/signup.js
+++ javascript/signup.js
@@ -3,6 +3,8 @@ const signupPw = document.getElementById("password");
 const signupName = document.getElementById("nickname");
 const signupPwCheck = document.getElementById("password-check");
 const signupBtn = document.getElementById("btn-signup");
+const eyePw = document.getElementById("password-toggle");
+const eyePwCheck = document.getElementById("password-check-toggle");
 
 function color() {
   if (
@@ -21,7 +23,29 @@ function color() {
   }
 }
 
+function togglePw() {
+  if (signupPw.type === "password") {
+    signupPw.type = "text";
+    eyePw.src = "/images/btn_visibility_on.png";
+  } else {
+    signupPw.type = "password";
+    eyePw.src = "/images/btn_visibility_off.png";
+  }
+}
+
+function togglepwCheck() {
+  if (signupPwCheck.type === "password") {
+    signupPwCheck.type = "text";
+    eyePwCheck.src = "/images/btn_visibility_on.png";
+  } else {
+    signupPwCheck.type = "password";
+    eyePwCheck.src = "/images/btn_visibility_off.png";
+  }
+}
+
 signupEmail.addEventListener("keyup", color);
 signupName.addEventListener("keyup", color);
 signupPw.addEventListener("keyup", color);
 signupPwCheck.addEventListener("keyup", color);
+eyePw.addEventListener("click", togglePw);
+eyePwCheck.addEventListener("click", togglepwCheck);
