ce6ff73fb24d2cb1460cd992727306e577b5421f
WooGi1020 <jungwk1020@gmail.com>
2024-04-02 10:27:09 +0900

    🔨 비밀번호 아이콘 동작 추가 및 input 타입 변경 함수 추가

--- JS/login.js
+++ JS/login.js
@@ -4,3 +4,16 @@ const loginBtn = document.querySelector(".login-btn");
 const emailInput = document.querySelector(".email input");
 const pwInput = document.querySelector(".pw input");
 const pwIcon = document.querySelector(".con .pw-icon");
+
+let isShow = false;
+
+pwIcon.addEventListener("click", () => {
+  if (isShow) {
+    pwIcon.src = "../login/assets/show-pw.png";
+    pwInput.setAttribute("type", "text");
+  } else {
+    pwIcon.src = "../login/assets/non-show-pw.png";
+    pwInput.setAttribute("type", "password");
+  }
+  isShow = !isShow;
+});
