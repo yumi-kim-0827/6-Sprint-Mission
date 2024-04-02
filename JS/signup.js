112e27c44523bdf09f997c75e4a86746e5ec5f65
WooGi1020 <jungwk1020@gmail.com>
2024-04-02 10:22:11 +0900

    🔧 signup.js 파일 추가 및 변수 생성

---
+++ JS/signup.js
@@ -0,0 +1,4 @@
+const pwInput = document.querySelector(".pw #pw");
+const rePwInput = document.querySelector(".re-pw #re-pw");
+const pwIcon = document.querySelector(".con .pw-icon");
+const rePwIcon = document.querySelector(".con .re-pw-icon");
