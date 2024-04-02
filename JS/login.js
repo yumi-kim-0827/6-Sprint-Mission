1aaa4688b142fb72691d7cd5c1768ad0436a4339
WooGi1020 <jungwk1020@gmail.com>
2024-04-02 10:29:13 +0900

    🔨 input 테두리 및 에러 메세지 출력 함수 추가

--- JS/login.js
+++ JS/login.js
@@ -18,6 +18,36 @@ pwIcon.addEventListener("click", () => {
   isShow = !isShow;
 });
 
+const handleInput = (test, element) => {
+  if (!test) {
+    element.classList.add("wrong");
+    if (element.type === "email") {
+      if (emailContainer.children.length > 2) {
+        emailContainer.removeChild(emailContainer.lastChild);
+      }
+      const wrongEmail = document.createElement("p");
+      wrongEmail.classList.add("wrong-email");
+      wrongEmail.textContent = "잘못된 이메일입니다";
+      emailContainer.append(wrongEmail);
+    } else if (element.type === "password") {
+      if (pwContainer.children.length > 2) {
+        pwContainer.removeChild(pwContainer.lastChild);
+      }
+      const wrongPw = document.createElement("p");
+      wrongPw.classList.add("wrong-pw");
+      wrongPw.textContent = "비밀번호를 8자 이상 입력해주세요";
+      pwContainer.append(wrongPw);
+    }
+  } else {
+    element.classList.remove("wrong");
+    if (emailContainer.children.length > 2) {
+      emailContainer.removeChild(emailContainer.lastChild);
+    } else if (pwContainer.children.length > 2) {
+      pwContainer.removeChild(pwContainer.lastChild);
+    }
+  }
+};
+
 const isValidEmail = () => {
   const isValid = /[a-z0-9]+@[a-z]+.[a-z]{2,3}/;
   const test = isValid.test(emailInput.value);
