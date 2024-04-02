69e03285db81fb1acf866c4dc2b118490c57b430
WooGi1020 <jungwk1020@gmail.com>
2024-04-02 17:47:18 +0900

    🔨 input 및 button 이벤트 리스너 및 유효성 검사 함수 추가

--- JS/signup.js
+++ JS/signup.js
@@ -1,5 +1,13 @@
+const emailContainer = document.querySelector(".email");
+const nickContainer = document.querySelector(".nick");
+const pwContainer = document.querySelector(".pw");
+const rePwContainer = document.querySelector(".re-pw");
+
+const emailInput = document.querySelector(".email input");
+const nickInput = document.querySelector(".nick input");
 const pwInput = document.querySelector(".pw #pw");
 const rePwInput = document.querySelector(".re-pw #re-pw");
+
 const pwIcon = document.querySelector(".con .pw-icon");
 const rePwIcon = document.querySelector(".con .re-pw-icon");
 
@@ -26,4 +34,42 @@ rePwIcon.addEventListener("click", () => {
     rePwIcon.src = "../login/assets/non-show-pw.png";
     rePwInput.setAttribute("type", "password");
   }
+});
+
+const isValidEmail = () => {
+  const isValid = /[a-z0-9]+@[a-z]+.[a-z]{2,3}/;
+  const test = isValid.test(emailInput.value);
+  handleInput(test, emailInput);
+};
+
+const isValidNick = () => {
+  const isFilled = false;
+  if (!nickInput.value === "") {
+    isFilled = true;
+  } else {
+    isFilled = false;
+  }
+  handleInput(isFilled, nickInput);
+};
+
+const isValidPw = () => {
+  const isValid = /.{8,}/;
+  const test = isValid.test(pwInput.value);
+  handleInput(test, pwInput);
+};
+
+const isValidRePw = () => {
+  const isValid = pwInput.value;
+  const test = isValid.test(rePwInput.value);
+  handleInput(test, rePwInput);
+};
+
+emailInput.addEventListener("input", isValidEmail);
+nickInput.addEventListener("input", isValidNick);
+pwInput.addEventListener("input", isValidPw);
+rePwInput.addEventListener("input", isValidRePw);
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  location.href = "../items.html";
 });
