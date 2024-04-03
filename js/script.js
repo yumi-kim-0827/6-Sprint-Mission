087b116abe4407cf7870fe36967c6b7bf15d0ddc
yumi-kim-0827 <116349476+yumi-kim-0827@users.noreply.github.com>
2024-04-03 12:35:17 +0900

    이메일 입력 유효성 및 경고 메세지 기능 추가

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
-window.onload = function(){
+window.onload = function(){ // js start !!
 
-const psw_icon = document.querySelector('.psw_input .psw_view');
-const psw_insert = document.querySelector('.psw_insert');
+    // ** 이메일 BLUR 에러 표시 **
 
-const psw_confirm_icon = document.querySelector('.psw_confirm_input .psw_view');
-const psw_confirm = document.querySelector('.psw_confirm');
+    const email_insertId = document.getElementById('email_insertId'); //이메일입력폼
+    const email_pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/; //이메일 유효성
+    
+    //이메일 포커스아웃할 때,
+    email_insertId.addEventListener('blur', function(event){
+      //이메일 형식이 맞지 않는 경우 > 에러메세지
+      if( event.target.value.length > 0 && !email_pattern.test(event.target.value)){
+        document.querySelector('.email_notice').textContent='잘못된 이메일 형식입니다.';
+      //아무것도 안적힌 경우 > 에러메세지
+      }else if( event.target.value.length < 1 ){ 
+        document.querySelector('.email_notice').textContent='이메일을 입력해주세요';
+      }else{
+        document.querySelector('.email_notice').textContent='';
+      }
+    })
 
-  // 비밀번호 보기 토글
-  psw_icon.addEventListener('click', function() {
-    if(psw_insert.type == 'password'){
-      psw_insert.type = 'text';
-    }else{
-      psw_insert.type = 'password';
-    }
-  });
-  // 비밀번호 확인 보기 토글
-  psw_confirm_icon.addEventListener('click', function() {
-    if(psw_confirm.type == 'password'){
-      psw_confirm.type = 'text';
-    }else{
-      psw_confirm.type = 'password';
-    }
-  });
 
-  
-  
-}
+  } // js end !!
