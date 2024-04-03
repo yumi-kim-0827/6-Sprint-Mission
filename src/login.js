ef1f9a9cddee2c88b5d8dd19e8cdb8de6cf0d5b3
seo02wow <rlatjdud9188@naver.com>
2024-04-03 13:07:22 +0900

    refactor : 이메일 input focusout 이벤트에서 input 이벤트로 변경
    - foucsout 되는 경우와 input에 값을 작성하는 순간에도 input 하단 메세지를 확인할 수 있게 하기 위함

--- src/login.js
+++ src/login.js
@@ -12,17 +12,28 @@ function emailCheck(email_address) {
   }
 }
 
-email.addEventListener("focusout", function (event) {
+function deleteOutLine(event) {
   const inputTarget = event.target;
-  if (!inputTarget.value) {
-    emailMessage.textContent = "이메일을 입력해주세요";
-    inputTarget.style.margin = 0;
-    inputTarget.style.outline = "2px solid #f74747";
-    inputTarget.after(emailMessage);
-  } else if (!emailCheck(inputTarget.value)) {
+  if (emailCheck(inputTarget.value)) {
+    inputTarget.style.outline = "none";
+  }
+}
+
+function checkEmail(event) {
+  const inputTarget = event.target;
+  if (!emailCheck(inputTarget.value)) {
+    // 이메일 형식에 맞지 않는 경우
     emailMessage.textContent = "잘못된 이메일 형식입니다";
     inputTarget.style.margin = 0;
     inputTarget.style.outline = "2px solid #f74747";
     inputTarget.after(emailMessage);
+  } else {
+    // 이메일 형식을 지킨 경우
+    emailMessage.remove();
+    inputTarget.style.marginBottom = "24px";
+    inputTarget.style.outline = "2px solid #3182f6";
   }
-});
+}
+
+email.addEventListener("input", checkEmail);
+email.addEventListener("focusout", deleteOutLine);
