884ac18eef1ef10e7790fd779f9bfd223281956b
yumi-kim-0827 <116349476+yumi-kim-0827@users.noreply.github.com>
2024-04-03 22:50:51 +0900

    회원가입 입력폼 유효성 확인 및 회원가입 버튼 활성화 기능 추가

--- js/script.js
+++ js/script.js
@@ -11,15 +11,18 @@ window.onload = function(){ // js start !!
       //이메일 형식이 맞지 않는 경우 ... 에러메세지
       if( email.length > 0 && !emailPattern.test(email)){
         document.querySelector('.email_notice').textContent='잘못된 이메일 형식입니다.';
+        emailValid = false;
       }else if( email.length < 1 ){
       //아무것도 안적힌 경우 ... 에러메세지
         document.querySelector('.email_notice').textContent='이메일을 입력해주세요';
+        emailValid = false;
       }else{
       //올바른 이메일 형식 ... 에러메세지 제거, 유효성 true
         document.querySelector('.email_notice').textContent='';
         emailValid = true;
       }
-    })
+      console.log(pswValid)
+    });
 
 
     // ** psw BLUR 에러 표시 **
@@ -31,11 +34,11 @@ window.onload = function(){ // js start !!
       //비밀번호에 아무것도 안적혀있을 때 ... 에러메세지
       if( password.length > 0 && password.length < 8 ){
         document.querySelector('.psw_notice').textContent='비밀번호를 8자 이상 입력해주세요.';
-        return false;
+        pswValid = false;
       }else if( password.length <1 ){
       //비밀번호가 적혔는데 8자 미만일 때 ... 에러메세지
         document.querySelector('.psw_notice').textContent='비밀번호를 입력해주세요.';
-        return false;
+        pswValid = false;
       } else {
       //잘 적혀있을 때 ... 에러메세지 제거, 유효성 true
         document.querySelector('.psw_notice').textContent='';
@@ -47,7 +50,7 @@ window.onload = function(){ // js start !!
         document.querySelector('.login_btn').style.backgroundColor = '#3692FF';
         document.querySelector('.login_btn').style.color = '#ffffff';
       }
-    })
+    });
 
     // ** psw 보기, 아이콘 토글 **
     const pswView = document.querySelector('.psw_view');//패쓰워드 입력폼 아이콘
@@ -60,33 +63,44 @@ window.onload = function(){ // js start !!
         pswInsertId.type = 'password';
         pswView.firstElementChild.src = './images/psw_nonvisible.png';
       }
-    })
+    });
 
     // ** 닉네임 BLUR 에러 표시 **
     const nicknameInsertId = document.getElementById('nickname_insertId'); //비밀번호 입력폼
+    let nickNameValid = false; //닉네임 유효성
+
     nicknameInsertId.addEventListener('blur', function(event){
       const email = event.target.value;
-      //이메일 형식이 맞지 않는 경우 ... 에러메세지
+      //닉네임을 안적은 경우 ... 에러메세지
       if( email.length < 1 ){
         document.querySelector('.nickname_notice').textContent='닉네임을 입력해주세요.';
+        nickNameValid = false;
       }else{
       //올바른 이메일 형식 ... 에러메세지 제거, 유효성 true
         document.querySelector('.nickname_notice').textContent='';
-        emailValid = true;
+        nickNameValid = true;
       }
-    })
+    });
 
     // ** psw 확인 일치 **
     const psw_confirmId = document.getElementById('psw_confirmId'); //비밀번호 입력폼
-      
+    let pswConfirmValid = false; //비번 확인 유효성
+
     psw_confirmId.addEventListener('blur', function(event){
       if( pswInsertId.value !== event.target.value){
         document.querySelector('.psw_confim_notice').textContent='비밀번호가 일치하지 않습니다.';
+        pswConfirmValid = trfalseue;
       }else{
         document.querySelector('.psw_confim_notice').textContent='';
+        pswConfirmValid = true;
       }
-      console.log(psw_confim_notice)
-    })
+      //이메일, 닉네임, 비밀번호 전부 return true일 때, 로그인 버튼 활성화
+      if( emailValid && pswValid && pswConfirmValid && nickNameValid){
+        document.querySelector('.signIn_btn').disabled = false;
+        document.querySelector('.signIn_btn').style.backgroundColor = '#3692FF';
+        document.querySelector('.signIn_btn').style.color = '#ffffff';
+      }
+    });
     
 
 
