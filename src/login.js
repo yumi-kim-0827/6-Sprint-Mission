09e2943211e46924786ba610fb12d69986319154
seo02wow <rlatjdud9188@naver.com>
2024-04-03 13:21:45 +0900

    feat : 이메일 input 포커스 아웃 시 값이 없을 때 메세지 표시 기능 추가

--- src/login.js
+++ src/login.js
@@ -1,6 +1,7 @@
 const email = document.querySelector("#email");
-const emailMessage = document.createElement("p");
-emailMessage.className = "inputUnderMessage";
+const password = document.querySelector("#password");
+const inputUnderMessage = document.createElement("p");
+inputUnderMessage.className = "inputUnderMessage";
 
 // 이메일 형식 유효성 체크
 function emailCheck(email_address) {
@@ -12,28 +13,39 @@ function emailCheck(email_address) {
   }
 }
 
-function deleteOutLine(event) {
-  const inputTarget = event.target;
-  if (emailCheck(inputTarget.value)) {
-    inputTarget.style.outline = "none";
-  }
-}
-
 function checkEmail(event) {
   const inputTarget = event.target;
   if (!emailCheck(inputTarget.value)) {
     // 이메일 형식에 맞지 않는 경우
-    emailMessage.textContent = "잘못된 이메일 형식입니다";
+    inputUnderMessage.textContent = "잘못된 이메일 형식입니다";
     inputTarget.style.margin = 0;
     inputTarget.style.outline = "2px solid #f74747";
-    inputTarget.after(emailMessage);
+    inputTarget.after(inputUnderMessage);
   } else {
     // 이메일 형식을 지킨 경우
-    emailMessage.remove();
+    inputUnderMessage.remove();
     inputTarget.style.marginBottom = "24px";
     inputTarget.style.outline = "2px solid #3182f6";
   }
 }
 
+function deleteOutLine(event) {
+  const inputTarget = event.target;
+  if (!inputTarget.value) {
+    inputUnderMessage.textContent = "이메일을 입력해주세요.";
+    inputTarget.style.margin = 0;
+    inputTarget.style.outline = "2px solid #f74747";
+    inputTarget.after(inputUnderMessage);
+  } else if (emailCheck(inputTarget.value)) {
+    inputTarget.style.outline = "none";
+  }
+}
+
 email.addEventListener("input", checkEmail);
 email.addEventListener("focusout", deleteOutLine);
+
+function checkPassword(event) {
+  const inputTarget = event.target;
+}
+
+password.addEventListener("input", checkPassword);
