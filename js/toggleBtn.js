3ea9b59c0e5a4080fdf666575d466a0642b787ca
haze <skdml20022@gmail.com>
2024-04-04 23:08:03 +0900

    feat: signup 페이지 js 파일 추가 및 checkPwd 버튼 이벤트핸들러 추가

--- js/toggleBtn.js
+++ js/toggleBtn.js
@@ -1,4 +1,5 @@
 const pwdBtnElement = document.getElementById('password-btn');
+const checkPwdBtnElement = document.getElementById('check-password-btn');
 
 /**
  * 비밀번호를 보여주는 버튼에 아이콘 변경 클래스 추가
@@ -9,4 +10,5 @@ function toggleBtnClassName(event) {
   event.target.classList.toggle('visibility');
 }
 
+checkPwdBtnElement.addEventListener('click', toggleBtnClassName);
 pwdBtnElement.addEventListener('click', toggleBtnClassName);
