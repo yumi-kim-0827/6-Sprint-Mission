cf718eea6472465d6aed64038f24ed7f2808b2b6
gohansaem92 <gohansaem@gmail.com>
2024-04-04 18:46:10 +0900

    로그인, 회원가입 인풋 에러메세지 구현

---
+++ signup.js
@@ -0,0 +1,73 @@
+const emailInput = document.getElementById("email");
+const nicknameInput = document.getElementById("nickname");
+const pwdInput = document.getElementById("pwd");
+const pwdCheckInput = document.getElementById("pwd_check");
+const errorEmailWrong = document.querySelector(".error_email_wrong");
+const errorEmailUndefined = document.querySelector(".error_email_undefined");
+const errorPwdWrong = document.querySelector(".error_pwd_wrong");
+const errorPwdUndefined = document.querySelector(".error_pwd_undefined");
+const errorPwdCheck = document.querySelector(".error_pwd_check");
+const errorNickname = document.querySelector(".error_nickname");
+
+function emailCheck(email_address) {
+  const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+  if (!email_regex.test(email_address)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+emailInput.addEventListener("focusout", (event) => {
+  if (event.target.value == "") {
+    errorEmailUndefined.classList.remove("hide");
+    errorEmailWrong.classList.add("hide");
+  } else {
+    if (emailCheck(event.target.value)) {
+      errorEmailUndefined.classList.add("hide");
+      event.target.classList.remove("error_border");
+      errorEmailWrong.classList.add("hide");
+    } else {
+      errorEmailUndefined.classList.add("hide");
+      event.target.classList.add("error_border");
+      errorEmailWrong.classList.remove("hide");
+    }
+  }
+});
+
+nicknameInput.addEventListener("focusout", (event) => {
+  if (event.target.value == "") {
+    errorNickname.classList.remove("hide");
+    event.target.classList.add("error_border");
+  } else {
+    errorNickname.classList.add("hide");
+    event.target.classList.remove("error_border");
+  }
+});
+
+pwdInput.addEventListener("focusout", (event) => {
+  if (event.target.value == "") {
+    errorPwdUndefined.classList.remove("hide");
+    errorPwdWrong.classList.add("hide");
+  } else {
+    if (event.target.value.length >= 8) {
+      errorPwdUndefined.classList.add("hide");
+      event.target.classList.remove("error_border");
+      errorPwdWrong.classList.add("hide");
+    } else {
+      errorPwdUndefined.classList.add("hide");
+      event.target.classList.add("error_border");
+      errorPwdWrong.classList.remove("hide");
+    }
+  }
+});
+
+pwdCheckInput.addEventListener("focusout", (event) => {
+  if (pwdInput.value === pwdCheckInput.value) {
+    errorPwdCheck.classList.add("hide");
+    event.target.classList.remove("error_border");
+  } else {
+    errorPwdCheck.classList.remove("hide");
+    event.target.classList.add("error_border");
+  }
+});
