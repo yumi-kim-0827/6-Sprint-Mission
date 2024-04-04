aaad872945d3311fa96306085bf7ec18227d203d
songa <thddk0121@gmail.com>
2024-04-04 14:09:52 +0900

    Refactor: Replace jQuery AJAX with fetch API for header and footer loading

--- js/include.js
+++ js/include.js
@@ -1,4 +1,21 @@
-$(function () {
-  $('#header').load('/layout/header.html');
-  $('#footer').load('/layout/footer.html');
-});
+fetch('./layout/header.html')
+  .then(response => response.text())
+  .then(html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const newHeader = doc.getElementById('header');
+    
+    const existingHeader = document.getElementById('header');
+    existingHeader.parentNode.replaceChild(newHeader, existingHeader);
+  });
+
+fetch('./layout/footer.html')
+  .then(response => response.text())
+  .then(html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const newFooter = doc.getElementById('footer');
+    
+    const existingFooter = document.getElementById('footer');
+    existingFooter.parentNode.replaceChild(newFooter, existingFooter);
+  });
