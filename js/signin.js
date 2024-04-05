0dae7e29ffe81e0118bff3af9dfbedf0a574975e
User850413 <csyou1228@gmail.com>
2024-04-05 17:27:04 +0900

    feat : 유효성 검사에 따른 로그인 버튼 활성화

--- js/signin.js
+++ js/signin.js
@@ -1,9 +1,13 @@
+const inputs = document.querySelectorAll("input");
 const email = document.querySelector("input#email");
+const nickname = document.querySelector("input#nickname");
 const pwd = document.querySelector("input#pwd");
-const ico_hide = document.querySelector(".ico_hide img");
+const re_pwd = document.querySelector("input#re_pwd");
+const ico_hides = document.querySelectorAll(".ico_hide img");
 const submitBtn = document.querySelector(".form button");
 
-//  warning 클래스 및 메세지 추가 함수
+//  warning 클래스 및 경고 메세지 추가/제거 함수
+
 function warningMessage(input, message) {
   input.nextElementSibling.textContent = message;
   input.classList.add("warning");
@@ -14,6 +18,7 @@ function removeWarning(input) {
 }
 
 // 유효성 검사 : email
+
 function focusOutEmail(el) {
   if (!el.target.value) {
     warningMessage(email, "이메일을 입력해 주세요");
@@ -25,31 +30,46 @@ function focusOutEmail(el) {
 }
 email.addEventListener("focusout", focusOutEmail);
 
+// 유효성 검사 : nickname
+
+function focusOutnickName(el) {
+  !el.target.value
+    ? warningMessage(nickname, "닉네임을 입력해주세요")
+    : removeWarning(nickname);
+}
+
+nickname ? nickname.addEventListener("focusout", focusOutnickName) : undefined;
+
 // 유효성 검사 : pwd
 
 function focusOutPwd(el) {
   if (!el.target.value) {
     warningMessage(pwd, "비밀번호를 입력해주세요");
-    ico_hide.parentElement.classList.add("warned");
+    ico_hides[0].parentElement.classList.add("warned");
   } else if (el.target.value.length < 8) {
     warningMessage(pwd, "비밀번호를 8자 이상 입력해주세요");
-    ico_hide.parentElement.classList.add("warned");
   } else {
     removeWarning(pwd);
-    ico_hide.parentElement.classList.remove("warned");
   }
 }
 pwd.addEventListener("focusout", focusOutPwd);
 
+// 유효성 검사 : re pwd
+
+function focusOutPwdCheck(el) {
+  el.target.value !== pwd.value
+    ? warningMessage(re_pwd, "비밀번호가 일치하지 않습니다!")
+    : removeWarning(re_pwd);
+}
+
+re_pwd ? re_pwd.addEventListener("focusout", focusOutPwdCheck) : undefined;
+
 // 로그인 버튼 활성화
 
-function activateLogin() {
-  if (
-    email.value &&
-    pwd.value &&
-    !email.classList.contains("warning") &&
-    !pwd.classList.contains("warning")
-  ) {
+// 로그인 버튼 활성화 - 활성화 함수
+
+function activateLogin(state) {
+  if (state) {
     submitBtn.classList.add("active");
     submitBtn.setAttribute("onclick", "location.href='./items.html'");
   } else {
@@ -58,20 +78,39 @@ function activateLogin() {
   }
 }
 
-email.addEventListener("focusout", activateLogin);
-pwd.addEventListener("focusout", activateLogin);
+// 로그인 버튼 활성화 - 유효성 체크
+
+function checkLogin() {
+  const values = [];
+  let warnings = 0;
+  for (let input of [...inputs]) {
+    input.value ? values.push(input.value) : undefined;
+    [...input.classList].includes("warning") ? (warnings += 1) : undefined;
+    if (values.length === inputs.length && warnings === 0) {
+      activateLogin(true);
+    } else {
+      activateLogin(false);
+    }
+  }
+}
+
+// 로그인 버튼 활성화 - 모든 input에 적용
+
+for (let input of inputs) {
+  input.addEventListener("focusout", checkLogin);
+}
 
 // 눈 모양 아이콘 클릭 시 pwd 창 변화
 
 function convert(el) {
   if (el.target.attributes.src.value == "../images/ico_hide_pwd.svg") {
-    el.target.parentElement.previousElementSibling.previousElementSibling.setAttribute(
+    el.target.parentElement.previousElementSibling.nextElementSibling.nextElementSibling.setAttribute(
       "type",
       "text"
     );
     el.target.attributes.src.value = "../images/ico_hide_open.svg";
   } else {
-    el.target.parentElement.previousElementSibling.previousElementSibling.setAttribute(
+    el.target.parentElement.previousElementSibling.nextElementSibling.nextElementSibling.setAttribute(
       "type",
       "password"
     );
@@ -79,6 +118,6 @@ function convert(el) {
   }
 }
 
-ico_hide.addEventListener("click", convert);
-// .previousElementSibling
-// ico_hide.parentElement
+for (let ico_hide of ico_hides) {
+  ico_hide.addEventListener("click", convert);
+}
