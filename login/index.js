4788982471f8f886b5c531b02cd9e3853871314d
easyhyun00 <leejihyun0324@gmail.com>
2024-04-05 11:13:17 +0900

    🔨 refactor: 입력 값 유무 유효성 검사 함수 분리

--- login/index.js
+++ login/index.js
@@ -4,26 +4,22 @@ const emailError = document.querySelector('#email-error');
 const passwordInput = document.querySelector('#password');
 const passwordError = document.querySelector('#password-error');
 
-emailInput.addEventListener('focusout', () => {
-  if (emailInput.value === '') {
-    emailInput.classList.add('error-input');
-    emailError.style.display = 'block';
-    emailError.textContent = '이메일을 입력해주세요.';
+const handleInputValid = (inputEl, errorEl, errMsg) => {
+  if (inputEl.value === '') {
+    inputEl.classList.add('error-input');
+    errorEl.style.display = 'block';
+    errorEl.textContent = errMsg;
   } else {
-    emailInput.classList.remove('error-input');
-    emailError.style.display = 'none';
-    emailError.textContent = '';
+    inputEl.classList.remove('error-input');
+    errorEl.style.display = 'none';
+    errorEl.textContent = '';
   }
+};
+
+emailInput.addEventListener('focusout', () => {
+  handleInputValid(emailInput, emailError, '이메일을 입력해주세요.');
 });
 
 passwordInput.addEventListener('focusout', () => {
-  if (passwordInput.value === '') {
-    passwordInput.classList.add('error-input');
-    passwordError.style.display = 'block';
-    passwordError.textContent = '비밀번호를 입력해주세요.';
-  } else {
-    passwordInput.classList.remove('error-input');
-    passwordError.style.display = 'none';
-    passwordError.textContent = '';
-  }
+  handleInputValid(passwordInput, passwordError, '비밀번호를 입력해주세요.');
 });
