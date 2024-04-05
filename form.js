fbcb249b7c5abef9703c40d3174cd146342d342a
doh-yo <doyo159090@gmail.com>
2024-04-05 12:23:51 +0900

    Feat: Add eventlistener method

--- form.js
+++ form.js
@@ -3,7 +3,7 @@ const myNickname = document.getElementById('nickname');
 const myPassword = document.getElementById('password');
 const passwordCheck = document.getElementById('password-repeat');
 
-//에러메세지 객체
+//에러메세지
 const EMAIL_ERROR = {
     noValue : '이메일을 입력해주세요.',
     failValue : '잘못된 이메일 형식입니다.',
@@ -18,3 +18,9 @@ const PASSWORD_ERROR  = {
     failValue : '비밀번호를 8자 이상 입력해주세요.',
     checkValue : '비밀번호가 일치하지 않습니다.',
 }
+
+//이벤트 리스너 등록
+myEmail.addEventListener("focusout",checkId);
+myNickname.addEventListener("focusout", checkId);
+myPassword.addEventListener("focusout", checkPw);
+passwordCheck.addEventListener("focusout", recheckPw);
