79cabcc90d0dccdaf61008fd7f158d297668df94
songa <thddk0121@gmail.com>
2024-04-05 17:15:06 +0900

    feat: Implement signup form functionality

--- js/loginSignupButtons.js
+++ js/loginSignupButtons.js
@@ -1,21 +1,25 @@
-const password = document.querySelector('#password');
-
 // 로그인 버튼 이벤트 핸들러
 function loginBtnClickEvent() {
-	window.location.href = '/pages/items';
+  window.location.href = '/pages/items';
+}
+
+// 회원가입 버튼 이벤트 핸들러
+function signupBtnClickEvent() {
+  window.location.href = '/pages/login';
 }
 
 // 비밀번호 표시 이벤트 핸들러
 function visibleIconClickEvent(e) {
-	if ( !e.target.classList.contains('on') ) {
-		e.target.classList.add('on');
-		password.setAttribute('type', 'text');
-		password.setAttribute('autocomplete', 'off');
-	} else {
-		e.target.classList.remove('on');
-		password.setAttribute('type', 'password');
-		password.setAttribute('autocomplete', 'current-password');
-	}
+  const password = e.target.parentElement.querySelector('.password');
+  if (!e.target.classList.contains('on')) {
+    e.target.classList.add('on');
+    password.setAttribute('type', 'text');
+    password.setAttribute('autocomplete', 'off');
+  } else {
+    e.target.classList.remove('on');
+    password.setAttribute('type', 'password');
+    password.setAttribute('autocomplete', 'current-password');
+  }
 }
 
-export { loginBtnClickEvent, visibleIconClickEvent };
+export { loginBtnClickEvent, signupBtnClickEvent, visibleIconClickEvent };
