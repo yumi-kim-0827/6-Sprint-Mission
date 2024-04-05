6fd37f2e04e34dba7e180fe83524b3e039a3fbe4
doh-yo <doyo159090@gmail.com>
2024-04-05 12:21:35 +0900

    Feat: Add error message

--- form.js
+++ form.js
@@ -2,3 +2,19 @@ const myEmail = document.getElementById('email');
 const myNickname = document.getElementById('nickname');
 const myPassword = document.getElementById('password');
 const passwordCheck = document.getElementById('password-repeat');
+
+//에러메세지 객체
+const EMAIL_ERROR = {
+    noValue : '이메일을 입력해주세요.',
+    failValue : '잘못된 이메일 형식입니다.',
+}
+
+const NICKNAME_ERROR = {
+    noValue : '닉네임을 입력해주세요.'
+}
+
+const PASSWORD_ERROR  = {
+    noValue : '비밀번호를 입력해주세요.',
+    failValue : '비밀번호를 8자 이상 입력해주세요.',
+    checkValue : '비밀번호가 일치하지 않습니다.',
+}
