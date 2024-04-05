38fdabebbed845e1eec1c0fa88c85d1d693c4574
doh-yo <doyo159090@gmail.com>
2024-04-05 12:17:15 +0900

    Feat: Add error div and Get id elements

---
+++ form.js
@@ -0,0 +1,4 @@
+const myEmail = document.getElementById('email');
+const myNickname = document.getElementById('nickname');
+const myPassword = document.getElementById('password');
+const passwordCheck = document.getElementById('password-repeat');
