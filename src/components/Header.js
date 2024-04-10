7c966a49c206e2b1e1a1221819d516876c6d3c50
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-04-10 20:06:22 +0900

    Design : 헤더 , 상품란 레이아웃 제작, feat: api 호출로 상품정보 가져오기

---
+++ src/components/Header.js
@@ -0,0 +1,100 @@
+import styled, { css } from "styled-components";
+import Logo from "../assets/images/logo.png";
+import { useNavigate, useLocation } from "react-router-dom";
+import "../css/style.css";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <HeaderContainer>
+      <MenuContainer>
+        <div onClick={() => navigate("/")}>
+          <img src={Logo} alt="판다마켓 홈" width="153" />
+        </div>
+        <FreeBoard>자유게시판</FreeBoard>
+        <UsedMarket
+          active={location.pathname === "/items"}
+          onClick={() => navigate("/items")}
+        >
+          중고마켓
+        </UsedMarket>
+      </MenuContainer>
+      <GoLogin onClick={handleLoginClick}>로그인</GoLogin>
+    </HeaderContainer>
+  );
+};
+
+const HeaderContainer = styled.div`
+  position: sticky;
+  top: 0px;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 200px;
+  background-color: #ffffff;
+  border-bottom: 1px solid #dfdfdf;
+
+  @media (max-width: 1023px) {
+    padding: 0 24px;
+  }
+
+  @media (max-width: 767px) {
+    padding: 0 5px;
+  }
+`;
+
+const GoLogin = styled.a`
+  background-color: #3692ff;
+  color: #ffffff;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  padding: 10px 20px;
+  border-radius: 10px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 218px;
+  height: 69px;
+  gap: 0px;
+  opacity: 1;
+`;
+
+const FreeBoard = styled.div`
+  font-family: Pretendard;
+  font-size: 15px;
+  font-weight: 700;
+  text-align: center;
+  padding-left: 15px;
+  cursor: pointer;
+`;
+
+const UsedMarket = styled.div`
+  font-family: Pretendard;
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 21.48px;
+  text-align: center;
+  padding-left: 15px;
+  cursor: pointer;
+  ${(props) =>
+    props.active &&
+    css`
+      color: #3692ff;
+    `}
+`;
+
+export default Header;
