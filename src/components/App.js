9d4c91dc16e37b88a9cc39af87929c9c68507393
songa <thddk0121@gmail.com>
2024-04-10 21:22:39 +0900

    style: Reset 스타일 및 GlobalStyle 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import Navbar from './Navbar';
 import { BrowserRouter } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import GlobalStyle from '../styles/GlobalStyles';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Reset />
+      <GlobalStyle />
       <Navbar />
     </BrowserRouter>
   );
