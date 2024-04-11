b717fbbe3332887db004d824b040b76d2dac2980
김보미 <kimbomi2172@naver.com>
2024-04-11 15:29:58 +0900

    ✨ feat: 헤더 nav 추가

--- pandamarket/src/components/Header.js
+++ pandamarket/src/components/Header.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import '../styles/style.css'
-import styles from '../styles/main.module.css'
+import React from "react";
+import "../styles/style.css";
+import styles from "../styles/main.module.css";
 
 function Header() {
   return (
     <header>
-        <picture>
-          <source
-            srcset="assets/logo_text.png"
-            media="all and (max-width: 768px)"
-          />
-          <img src="assets/logo.png" className={styles.logo} alt="로고" />
-        </picture>
+      <picture>
+        <source
+          srcset="assets/logo_text.png"
+          media="all and (max-width: 768px)"
+        />
+        <img src="assets/logo.png" className={styles.logo} alt="로고" />
+      </picture>
+      <nav className={styles['header-nav']}>
+        <p>자유게시판</p>
+        <p>중고마켓</p>
+      </nav>
       <button id="btn_small" onclick="location.href='/signin.html'">
         로그인
       </button>
