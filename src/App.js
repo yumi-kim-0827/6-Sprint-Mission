31b161c0a955a664de76b807035ae6427f8e7d5d
eunbin <dmsqlsdl54@gmail.com>
2024-04-11 17:56:00 +0900

    [init] 스타일 반영 완료, 헤더 레이아웃 완성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Header from "./components/Header/Header";
+import "./styles/reset.css";
+import "./styles/global.css";
 
 const App = () => {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,16 +6,33 @@ const Header = () => {
   return (
     <header className="header">
       <div className="inner">
-        <a className="logo flex-center" href="/">
-          <div className="logo-img mobile-hidden">
-            <img src={pandaLogo} alt="판다마켓 로고 이미지" />
-          </div>
-          <span className="logo-text font-rokaf">판다마켓</span>
-        </a>
+        <div className="left flex-center">
+          <a className="logo flex-center" href="/">
+            <div className="logo-img mobile-hidden">
+              <img src={pandaLogo} alt="판다마켓 로고 이미지" />
+            </div>
+            <span className="logo-text font-rokaf">판다마켓</span>
+          </a>
 
-        <a className="button" href="login.html">
-          로그인
-        </a>
+          <ul className="nav-list flex-center">
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                자유게시판
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                중고마켓
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="right">
+          <a className="button" href="login.html">
+            로그인
+          </a>
+        </div>
       </div>
     </header>
   );
