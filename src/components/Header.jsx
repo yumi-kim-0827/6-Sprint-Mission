279920e9c566f631df6c410bb273e3de3690e7f1
WooGi1020 <jungwk1020@gmail.com>
2024-04-11 14:08:47 +0900

    🔨 Header 컴포넌트 및 css 작성

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import longlogo from "../assets/long_logo.png";
+import shortlogo from "../assets/short_logo.png";
+
+const Header = () => {
+  return (
+    <>
+      <header className="Header">
+        <div className="Header-logo">
+          <a href="./">
+            <picture>
+              <source srcset={shortlogo} media="(max-width: 767px)" />
+              <img className="Header-img" src={longlogo} alt="판다마켓 로고" />
+            </picture>
+          </a>
+          <div className="Header-btn">
+            <a href="#">자유게시판</a>
+            <a href="#" className="active">
+              중고마켓
+            </a>
+          </div>
+        </div>
+        <button className="Header-login-btn">로그인</button>
+      </header>
+    </>
+  );
+};
+
+export default Header;
