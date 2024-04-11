dee454a1dd6fe0d2de9709bfa629ab54011bc988
Lin <shl6534@naver.com>
2024-04-11 21:51:59 +0900

    ✨ feat: 라우트 적용

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,21 @@
 import GlobalStyle from '../styles/GlobalStyle';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Home';
+import Items from './Items';
+import Login from './Login';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
-  
   );
 }
 
