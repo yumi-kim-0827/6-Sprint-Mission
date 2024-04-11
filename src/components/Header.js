1e25319d606094b6a13c0dde81481b27d9dac378
chanhee won <wch2208@gmail.com>
2024-04-11 23:15:58 +0900

    feat: Items 페이지 라우터 연결

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header>
+      <div class="header">
+        <div class="logo">
+          <img src="/assets/pandaFace.svg" class="logo_icon" alt="로고" />
+          <a href="/" class="logo_title">
+            판다마켓
+          </a>
+        </div>
+        <a class="login_btn" href="/login.html">
+          로그인
+        </a>
+      </div>
+    </header>
+  );
+}
