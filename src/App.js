8efe293e7fe2d8d6fe0b546ee26a747cd8ae5a44
Daeyang lee <eodid007@gmail.com>
2024-04-11 17:12:47 +0900

    feat: Header Component Add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header.jsx";
 
 function App() {
+  const [navSelected, setNavSelected] = useState("자유게시판");
+
+  const navClickHandler = (text) => {
+    setNavSelected(text);
+  };
+
   return (
     <div className="App">
-      <h1>HELLO WORLD!</h1>
+      <Header navSelected={navSelected} navClickHandler={navClickHandler} />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import logoOnly from "../assets/logo_only.svg";
+import logoText from "../assets/logo_text.svg";
+import Button from "./Button.jsx";
+import "./Header.css";
+
+const Header = ({ navSelected, navClickHandler }) => {
+  return (
+    <header className="Header">
+      <div className="Header__wrapper">
+        <div className="Header__logoWrapper">
+          <img
+            src={logoOnly}
+            className="Header__logo_mark"
+            alt="판다마켓 로고"
+          />
+          <img
+            src={logoText}
+            className="Header__logo_text"
+            alt="판다마켓 로고 텍스트"
+          />
+        </div>
+        <div className="Header__gnb">
+          <button
+            href={"#"}
+            className={navSelected === "자유게시판" ? "selected" : ""}
+            onClick={() => {
+              navClickHandler("자유게시판");
+            }}
+          >
+            자유게시판
+          </button>
+          <button
+            href={"#"}
+            className={navSelected === "중고마켓" ? "selected" : ""}
+            onClick={() => {
+              navClickHandler("중고마켓");
+            }}
+          >
+            중고마켓
+          </button>
+        </div>
+        <div className="Header__buttonWrapper">
+          <Button text="로그인" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
