218b0d98b94150222fe5c182ddae827003331349
eprnf <eprnf21@gmail.com>
2024-04-12 00:31:35 +0900

    feat: font파일 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles/reset.css";
+import "./styles/common.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
