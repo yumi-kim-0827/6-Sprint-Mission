64cde5b6f586e9e6b5e0dcc810d7be6825ea2b4c
chanhee won <wch2208@gmail.com>
2024-04-12 01:48:10 +0900

    feat: nav 태블릿,데스트탑 반응형 디자인 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,26 @@
 import { Link } from "react-router-dom";
 import "./Nav.css";
+import logo from "../assets/logo-panda.svg";
 
 export default function Nav() {
   return (
     <>
       <header>
-        <div className="header-container">
+        <div className="nav-container">
           <div className="logo">
-            <img
-              src="../assets/logo-panda.svg"
-              className="logo-icon"
-              alt="로고"
-            />
+            <img src={logo} className="logo-icon" alt="로고" />
             <Link to="/" className="logo-title">
               판다마켓
             </Link>
           </div>
-          <Link to="/" className="board-btn">
-            자유게시판
-          </Link>
-          <Link to="/items" className="items-btn">
-            중고마켓
-          </Link>
+          <div className="nav-pages">
+            <Link to="/" className="board-btn">
+              자유게시판
+            </Link>
+            <Link to="/items" className="items-btn">
+              중고마켓
+            </Link>
+          </div>
           <Link to="/login" className="btn login-btn">
             로그인
           </Link>
