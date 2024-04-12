c7701840e9e449d296460af34f760576b88af7a9
WooGi1020 <jungwk1020@gmail.com>
2024-04-12 13:15:18 +0900

    🔨 에러처리 코드 추가

--- src/components/api.js
+++ src/components/api.js
@@ -1,15 +1,23 @@
 export async function getProduct(order = "recent") {
-  let query = `orderBy=${order}`;
-  if (order !== "recent" && order !== "favorite") {
-    query = `keyword=${order}`;
+  try {
+    let query = `orderBy=${order}`;
+    if (order !== "recent" && order !== "favorite") {
+      query = `keyword=${order}`;
+    }
+    const response = await fetch(`https://panda-market-api.vercel.app/products/?${query}`);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.error(`${error} : error`);
   }
-  const response = await fetch(`https://panda-market-api.vercel.app/products/?${query}`);
-  const data = response.json();
-  return data;
 }
 
 export async function getBestProduct() {
-  const response = await fetch(`https://panda-market-api.vercel.app/products/?orderBy=favorite`);
-  const data = response.json();
-  return data;
+  try {
+    const response = await fetch(`https://panda-market-api.vercel.app/products/?orderBy=favorite`);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.error(`${error} : error`);
+  }
 }
