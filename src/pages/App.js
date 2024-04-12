aa304da3b3d85d90fa0c4f3295b39dba5e29e532
Lin <shl6534@naver.com>
2024-04-12 20:24:09 +0900

    ✨ feat: 라우터 설정
    - Board.js AddItem.js 추가

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,23 @@
-import GlobalStyle from '../styles/GlobalStyle';
+import GlobalStyle from "../styles/GlobalStyle";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Home';
-import Items from './Items';
-import Login from './Login';
+import Nav from "../components/Nav";
+import Home from "./Home";
+import Board from "./Board";
+import Items from "./Items/Items";
+import AddItem from "./AddItem";
+import Login from "./Login";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
       <Router>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/board" element={<Board />} />
           <Route path="/items" element={<Items />} />
+          <Route path="/additem" element={<AddItem />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
