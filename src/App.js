bf4a469315954ecd10aaae42a069b443099bcb1f
WooGi1020 <jungwk1020@gmail.com>
2024-04-12 12:04:22 +0900

    🔨 App 컴포넌트 및 필요 함수들 작성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Main from "./components/Main";
+import ShowBestProducts from "./components/ShowBestProducts";
+import ShowProducts from "./components/ShowProducts";
+import { getProduct, getBestProduct } from "./components/api";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [bestProducts, setBestProducts] = useState([]);
+  const [order, setOrder] = useState("recent");
+  const sortedBestProducts = products.sort((a, b) => b.favorite - a.favorite);
+
+  const handleLoad = async (orderQuery) => {
+    const { list } = await getProduct(orderQuery);
+    setProducts(list);
+  };
+
+  const handleBestLoad = async () => {
+    const { list } = await getBestProduct();
+    setBestProducts(list);
+  };
+
+  function onChangeSelect(e) {
+    const value = e.target.value;
+    value === "좋아요순" ? setOrder("favorite") : setOrder("recent");
+  }
+
+  useEffect(() => {
+    handleLoad(order);
+  }, [order]);
+
+  useEffect(() => {
+    handleBestLoad();
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Main />
+      <div className="container">
+        <ShowBestProducts products={bestProducts} />
+        <ShowProducts onChange={onChangeSelect} products={products} />
+      </div>
     </div>
   );
 }
