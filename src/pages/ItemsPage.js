3c845ecd47f4af7a13eceae360205c7ef44ab6d3
un0211 <hwryu22@gmail.com>
2024-04-12 20:57:57 +0900

    feat(ItemsPage-Pagination): Implement pagination of items page

--- src/pages/ItemsPage.js
+++ src/pages/ItemsPage.js
@@ -6,17 +6,26 @@ import ItemList from "../components/ItemList";
 import ItemListNav from "../components/ItemListNav";
 import "./ItemsPage.css";
 import ItemListNavMobile from "../components/ItemListNavMobile";
+import Pagination from "../components/Pagination";
 
 function ItemsPage({ device }) {
   const [bestItems, setBestItems] = useState([]);
   const [bestItemsUsing, setBestItemsUsing] = useState([]);
   const [allItems, setAllItems] = useState([]);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
   const [order, setOrder] = useState("recent");
 
   const handleInitialLoad = async () => {
-    const { list: newBestItems } = await getBestItems();
+    const { list: newBestItems, totalCount: newTotalCountString } =
+      await getBestItems();
+    const newTotalCount = Number(newTotalCountString);
+    const newTotalPage = Math.ceil(newTotalCount / NUM_ALL_ITEMS[device]);
+
     setBestItems(newBestItems);
+    setTotalCount(newTotalCount);
+    setTotalPage(newTotalPage);
     setBestItemsUsing(newBestItems.slice(0, NUM_BEST_ITEMS[device]));
 
     handleLoad();
@@ -25,7 +34,7 @@ function ItemsPage({ device }) {
   const handleLoad = async () => {
     const pageSize = NUM_ALL_ITEMS[device];
     const { list: newAllItems } = await getItems({
-      page: pageNumber,
+      page: currentPage,
       pageSize,
       orderBy: order,
     });
@@ -39,11 +48,14 @@ function ItemsPage({ device }) {
   useEffect(() => {
     const newBestItemsUsing = bestItems.slice(0, NUM_BEST_ITEMS[device]);
     setBestItemsUsing(newBestItemsUsing);
+
+    const newTotalPage = Math.ceil(totalCount / NUM_ALL_ITEMS[device]);
+    setTotalPage(newTotalPage);
   }, [device]);
 
   useEffect(() => {
     handleLoad();
-  }, [device, order, pageNumber]);
+  }, [device, order, currentPage]);
 
   return (
     <>
@@ -60,6 +72,7 @@ function ItemsPage({ device }) {
             <ItemListNav order={order} setOrder={setOrder} />
           )}
           <ItemList items={allItems} />
+          <Pagination totalPage={totalPage} setCurrentPage={setCurrentPage} />
         </section>
       </main>
     </>
