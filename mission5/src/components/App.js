1203ceb6e3dccea3916e931a3490d603b7f83b72
AdamSeungheonShin <ssh9809@naver.com>
2024-04-12 22:30:05 +0900

    리액트 인스톨, 신규 페이지 레이아웃 구성

---
+++ mission5/src/components/App.js
@@ -0,0 +1,15 @@
+import NavBar from "../layout/header";
+import Content from "../layout/content";
+import ProductList from "./Test";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Content />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
