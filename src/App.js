c8c934086afec34d933d366d468ad8536816c542
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-04-12 10:46:07 +0900

    feat : 페이지라우팅 되게 설정

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import "./css/style.css";
 import Product from "./components/Products";
+import AddItem from "./components/AddItem";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Product />
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/items" element={<Product />} />
+        <Route path="/additem" element={<AddItem />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/components/AddItem.js
@@ -0,0 +1,5 @@
+const AddItem = () => {
+  return <div>상품추가페이지</div>;
+};
+
+export default AddItem;
