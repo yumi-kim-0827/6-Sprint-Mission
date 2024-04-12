bbe769628514aae9feb1ce8fe61836feee434df2
유주민 <yjm825000@naver.com>
2024-04-12 19:06:33 +0900

    feat: 상단 네비게이션바 css 적용

---
+++ items/src/components/TopNavigation/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import pandoLogo from '../../assets/logos/panda-logo.png';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+const TopNavigation = () => {
+  return (
+    <header>
+      <div className='leftContainer'>
+        <Link to='/'>
+          <img src={pandoLogo} alt='판다마켓 로고' />
+        </Link>
+        <nav>
+          <ul className='navBar'>
+            <li>
+              <Link to='/board'>자유게시판</Link>
+            </li>
+            <li>
+              <Link to='/items'>중고마켓</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Link to='/login'>
+        <button className='login-button'>로그인</button>
+      </Link>
+    </header>
+  );
+};
+
+export default TopNavigation;
---
+++ items/src/pages/ItemsPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNavigation from '../../components/TopNavigation';
+
+const ItemsPage = () => {
+  return (
+    <div>
+      <TopNavigation />
+    </div>
+  );
+};
+
+export default ItemsPage;
