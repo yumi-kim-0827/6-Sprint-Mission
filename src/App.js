3fd18864fe294aff24c24f15bd470aed091a322b
un0211 <hwryu22@gmail.com>
2024-04-12 18:45:15 +0900

    fix(App-init): Change logic of setting inital device value

--- src/App.js
+++ src/App.js
@@ -14,25 +14,28 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   // <Q1>
-  // 처음 접속할 때 괜히 PC 버전으로 데이터가 한 번 로드되는데
-  // 처음엔 -1이나 undefined 등으로 로드되지 않도록 막는 게 좋을까요?
-  const [device, setDevice] = useState(PC);
+  // 초기 디바이스 모드 잘 정하는 방법이 있을까요?
+  // 이번 커밋 이전에는 처음에 무조건 PC버전으로 로드됐는데,
+  // 아래에서 setDevice로 변경해주어도 자동으로 다시 렌더링 되어
+  // 알맞은 버전으로 보여주지 않은 이유가 무엇일까요?
+  const mobileMediaQuery = window.matchMedia("(max-width: 767px)");
+  const tabletMediaQuery = window.matchMedia("(max-width: 1199px)");
+  const getDevice = () => {
+    if (mobileMediaQuery.matches) {
+      return MOBILE;
+    } else if (tabletMediaQuery.matches) {
+      return TABLET;
+    } else {
+      return PC;
+    }
+  };
+
+  const [device, setDevice] = useState(getDevice());
 
   useEffect(() => {
-    const mobileMediaQuery = window.matchMedia("(max-width: 767px)");
-    const tabletMediaQuery = window.matchMedia("(max-width: 1199px)");
-
     const handleResize = () => {
-      if (mobileMediaQuery.matches) {
-        setDevice(MOBILE);
-      } else if (tabletMediaQuery.matches) {
-        setDevice(TABLET);
-      } else {
-        setDevice(PC);
-      }
+      setDevice(getDevice());
     };
-
-    handleResize();
 
     // 미디어 쿼리 변경 사항을 감지하고 상태 업데이트
     mobileMediaQuery.addListener(handleResize);
