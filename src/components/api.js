2745a24725801cf655e159004c7f22b0e49f28d2
WooGi1020 <jungwk1020@gmail.com>
2024-04-12 18:47:33 +0900

    🔨 페이지네이션을 위한 url 경로 수정

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,10 @@
-export async function getProduct(order = "recent") {
+export async function getProduct(order = "recent", page = 1) {
   try {
     let query = `orderBy=${order}`;
     if (order !== "recent" && order !== "favorite") {
       query = `keyword=${order}`;
     }
-    const response = await fetch(`https://panda-market-api.vercel.app/products/?${query}`);
+    const response = await fetch(`https://panda-market-api.vercel.app/products?page=${page}&pageSize=10&${query}`);
     const data = response.json();
     return data;
   } catch (error) {
