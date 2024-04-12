eea16f046ffe8befaf53948b4c3f97954cc8576f
SeungRae Kim <tmdfo50@gmail.com>
2024-04-12 13:06:37 +0900

    Fix : Main.js merge error 수정

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,18 +4,10 @@ import './Main.css';
 
 function Main() {
 	return (
-<<<<<<< HEAD
 		<div className="main">
 			<BestItems />
 			<AllItems />
 		</div>
-
-=======
-		<div className='main'>
-      <BestItems/>
-      <AllItems/>
-    </div>
->>>>>>> 72f21a664dd22b2df77a623517ccf75aa7798bbc
 	);
 }
 
