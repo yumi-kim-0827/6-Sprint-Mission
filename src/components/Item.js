460f2b454306bf7e52447a8f791c489e3940f308
un0211 <hwryu22@gmail.com>
2024-04-12 14:57:28 +0900

    feat(Item): Implement structure of Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,24 @@
-function Item() {
-  return <></>;
+import "./Item.css";
+import favoriteIcon from "../assets/icon/ic_favorite.svg";
+
+function Item({ item }) {
+  const { name, images, price, favoriteCount } = item;
+  const imgSrc = images[0];
+  const priceString = `${price.toLocaleString("ko-KR")}원`;
+
+  return (
+    <article className="item">
+      <img className="item-image" src={imgSrc} alt="상품 이미지" />
+      <div className="item-info">
+        <p className="item-title">{name}</p>
+        <p className="item-price">{priceString}</p>
+        <div className="item-favorite">
+          <img src={favoriteIcon} alt="좋아요 아이콘" />
+          {favoriteCount}
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default Item;
