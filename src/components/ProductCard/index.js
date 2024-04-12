a31169328ee297ff29de454751b6415222c8ee1b
heony704 <heony704@gamil.com>
2024-04-13 00:02:46 +0900

    refact: 상품 카드 컴포넌트 이미지의 alt 추가

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -5,7 +5,11 @@ function ProductCard({ type = "big", product }) {
 
   return (
     <div className={`product-card ${type}`}>
-      <img className={`product-card-image ${type}`} src={images[0]} />
+      <img
+        className={`product-card-image ${type}`}
+        src={images[0]}
+        alt={`${name} 상품`}
+      />
       <div className="product-card-description">
         <h3 className="product-card-name">{name}</h3>
         <h2 className="product-card-price">{price.toLocaleString()}원</h2>
@@ -13,6 +17,7 @@ function ProductCard({ type = "big", product }) {
           <img
             className="product-card-favorite-icon"
             src="images/ic_heart_inactive.svg"
+            alt="빈 하트 아이콘"
           />
           <p className="product-card-favorite-count">{favoriteCount}</p>
         </div>
