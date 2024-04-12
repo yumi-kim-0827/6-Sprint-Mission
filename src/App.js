3b64284ede66b1b11099d80dab718fc5eb650b5c
heony704 <heony704@gamil.com>
2024-04-12 23:59:11 +0900

    feat: 메인페이지 추가 (내용 미구현)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 import ProductPage from "./pages/ProductPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<MainPage />} />
       <Route path="/items" element={<ProductPage />} />
     </Routes>
   );
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
