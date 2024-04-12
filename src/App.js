fa1b9f46940afbd5458c390ddb75751ba0499268
un0211 <hwryu22@gmail.com>
2024-04-12 09:19:42 +0900

    feat(Nav): Implement Nav features

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import ItemsPage from "./pages/ItemsPage";
 import PrivacyPage from "./pages/PrivacyPage";
 import FaqPage from "./pages/FaqPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import "./App.css";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="items" element={<ItemsPage />} />
         <Route path="privacy" element={<PrivacyPage />} />
         <Route path="faq" element={<FaqPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,5 @@
+function NotFoundPage() {
+  return <p>404: page not found</p>;
+}
+
+export default NotFoundPage;
