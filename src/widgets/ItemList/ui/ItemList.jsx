3c39fb52cf708684f5ddb5a7ac83bac2930ef71b
codefug <robot9917@gmail.com>
2024-04-13 14:16:21 +0900

    feat: add mediaquery of ItemList and  style of them

--- src/widgets/ItemList/ui/ItemList.jsx
+++ src/widgets/ItemList/ui/ItemList.jsx
@@ -4,12 +4,15 @@ import { getDatum } from "../../../shared/api/api.jsx";
 import { Button } from "../../../shared/ui/Button.jsx";
 import { ItemCard } from "../../../entities/ItemCard/ItemCard.jsx";
 import { useMediaQuery } from "react-responsive";
+import searchIcon from "../../../shared/asset/searchIcon.png";
+
+import "./ItemList.scss";
 
 export const ItemList = () => {
   const [isError, setIsError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [items, setItems] = useState([]);
-  const [order, setOrder] = useState("recent");
+  const [orderBy, setOrderBy] = useState("recent");
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
 
@@ -17,9 +20,17 @@ export const ItemList = () => {
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1199 });
   const isDesktop = useMediaQuery({ minWidth: 1200 });
 
-  const handleRecentSort = () => setOrder("recent");
+  const handleRecentSort = () => {
+    setOrderBy("recent");
 
-  const handleFavoriteSort = () => setOrder("favorite");
+    console.log(orderBy);
+  };
+
+  const handleFavoriteSort = () => {
+    setOrderBy("favorite");
+
+    console.log(orderBy);
+  };
 
   const handleload = async (options) => {
     try {
@@ -27,9 +38,6 @@ export const ItemList = () => {
       setIsLoading(true);
       const newItems = await getDatum(options);
       setItems(newItems);
-      if (options.offset === 0) {
-        setItems();
-      }
     } catch (error) {
       setIsError(error);
     } finally {
@@ -37,23 +45,41 @@ export const ItemList = () => {
     }
   };
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target["search"].value);
+  };
+
   useEffect(() => {
-    handleload({ page: 1, order, search });
-  }, [order, search]);
+    handleload({
+      page: 1,
+      orderBy,
+      search,
+      pageSize: isMobile ? 4 : isTablet ? 6 : 10,
+    });
+  }, [page, orderBy, search, isMobile, isDesktop, isTablet]);
 
   return (
     <>
-      <h1></h1>
-      <div>
+      <div className="ItemList__header">
+        <span className="ItemList__title">
+          {isDesktop ? "전체상품" : "판매 중인 상품"}
+        </span>
+        <form onSubmit={handleSearchSubmit}>
+          <img src={searchIcon} />
+          <input name="search" placeholder={"검색할 상품을 입력해주세요"} />
+        </form>
+        <a href="./items">
+          <Button
+            classNames={["button--blue", "button--small"]}
+            value={"상품 등록하기"}
+          />
+        </a>
         <ToggleList
           options={[
             { name: "최신순", callback: handleRecentSort },
             { name: "좋아요순", callback: handleFavoriteSort },
           ]}
-        />
-        <Button
-          classNames={["button--blue", "button--small"]}
-          value={"상품 등록하기"}
         />
       </div>
       {isError?.message && <span>{isError.message}</span>}
@@ -63,7 +89,7 @@ export const ItemList = () => {
           items.list.map((item) => {
             return (
               <div key={item.createdAt} className="">
-                <ItemCard item={item} cardType="--big" />
+                <ItemCard item={item} cardType="--small" />
               </div>
             );
           })}
