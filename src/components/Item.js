368f087a2fd239beb804a04a070f3f8d8ccc7ffe
un0211 <hwryu22@gmail.com>
2024-04-15 12:29:12 +0900

    refactore(Item-imgSrc): Use images[0] directly instead of assigning it to variable imgSrc

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,12 +3,11 @@ import favoriteIcon from "../assets/icon/ic_favorite.svg";
 
 function Item({ item }) {
   const { name, images, price, favoriteCount } = item;
-  const imgSrc = images[0];
   const priceString = `${price.toLocaleString("ko-KR")}원`;
 
   return (
     <article className="item">
-      <img className="item-image" src={imgSrc} alt="상품 이미지" />
+      <img className="item-image" src={images[0]} alt="상품 이미지" />
       <div className="item-info">
         <p className="item-title">{name}</p>
         <p className="item-price">{priceString}</p>
