2d056d77817ec705fe18f70c35ed46acaeb461de
김제완 <jeeee_in@naver.com>
2024-04-15 21:58:07 +0900

    feat: 페이지네이션 기능 추가

--- src/components/API.js
+++ src/components/API.js
@@ -1,8 +1,8 @@
 export async function getData({ page = 1, size = 10, order = "recent" }) {
-  const query = `page=${page}&Size=${size}&orderBy=${order}`;
+  const query = `page=${page}&pageSize=${size}&orderBy=${order}`;
   const res = await fetch(
     `https://panda-market-api.vercel.app/products?${query}`
   );
   const body = await res.json();
-  return body.list;
+  return body;
 }
--- src/components/AllMarket.js
+++ src/components/AllMarket.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../stlye/allMarket.css";
-// import { useMediaQuery } from "react-responsive";
 import Button from "../common/Button";
 import HeartIcon from "../assets/icon/ic_heart.svg";
+import Arrow_left from "../assets/icon/ic_arrow_left.svg";
+import Arrow_right from "../assets/icon/ic_arrow_right.svg";
 import { Link } from "react-router-dom";
 import { SearchInput } from "./SearchInput";
 import Commas from "../util/Commas";
@@ -10,67 +11,116 @@ import SelectBtn from "./SelectBtn";
 import { getData } from "./API";
 
 export default function AllMarket() {
-  const [Data, setData] = useState([]);
+  const [data, setData] = useState([]);
   const [order, setOrder] = useState("recent");
-  // const [page, setPage] = useState(1);
-  const page = 1;
-  const size = 10;
-
-  const handleLoad = async () => {
-    try {
-      const result = await getData({ page, size, order });
-      setData(result);
-    } catch (error) {
-      console.error(error);
-    } finally {
-    }
-  };
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const pageSize = 10;
 
   useEffect(() => {
-    handleLoad();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [order]);
+    const fetchData = async () => {
+      try {
+        const { list, totalCount } = await getData({
+          page: currentPage,
+          size: pageSize,
+          order,
+        });
+        setData(list);
+        setTotalPages(Math.ceil(totalCount / pageSize));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [currentPage, order]);
 
   const handleSortOrderChange = (selectedOrder) => {
     setOrder(selectedOrder);
   };
 
-  return (
-    <div className="all-market">
-      <div className="all-header">
-        <div>
-          <h1 className="all-title">전체 상품</h1>
+  const handlePageChange = (pageNumber) => {
+    if (pageNumber >= 1 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+    }
+  };
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <div
+          key={i}
+          onClick={() => handlePageChange(i)}
+          className={"pageBtn " + (currentPage === i ? "active" : "")}
+        >
+          {i}
         </div>
-        <div className="all-search-input">
-          <div className="searchInput">
-            <SearchInput />
+      );
+    }
+    return pageNumbers;
+  };
+
+  return (
+    <>
+      <div className="all-market">
+        <div className="all-header">
+          <div>
+            <h1 className="all-title">전체 상품</h1>
           </div>
-          <div className="button">
-            <Button to="/additem">상품 등록하기</Button>
+          <div className="all-search-input">
+            <div className="searchInput">
+              <SearchInput />
+            </div>
+            <div className="button">
+              <Button to="/additem">상품 등록하기</Button>
+            </div>
+            <div className="selectInput">
+              <SelectBtn onChange={handleSortOrderChange} />
+            </div>
           </div>
-          <div className="selectInput">
-            {/* SelectBtn 컴포넌트에 정렬 기능을 구현하는 함수를 props로 전달 */}
-            <SelectBtn onChange={handleSortOrderChange} />
+        </div>
+        {data.length > 0 && (
+          <div className="cards">
+            {data.map((item) => (
+              <Link to={`/product/${item.id}`} key={item.id}>
+                <div className="card">
+                  <img
+                    className="all-img"
+                    src={item.images[0]}
+                    alt={item.name}
+                  />
+                  <p className="all-name">{item.name}</p>
+                  <p className="all-price">{Commas(item.price)}원</p>
+                  <div className="like">
+                    <img src={HeartIcon} alt="Heart" />
+                    <p>{item.favoriteCount}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
           </div>
+        )}
+      </div>
+      <div className="pagination">
+        <div className="pageBtn">
+          <img
+            src={Arrow_left}
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+            alt="arrow_left"
+          />
+        </div>
+        {renderPageNumbers()}
+        <div className="pageBtn">
+          <img
+            src={Arrow_right}
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+            alt="arrow_right"
+          />
         </div>
       </div>
-      {Data.length > 0 && (
-        <div className="cards">
-          {Data.map((item) => (
-            <Link to={`/product/${item.id}`} key={item.id}>
-              <div className="card">
-                <img className="all-img" src={item.images[0]} alt={item.name} />
-                <p className="all-name">{item.name}</p>
-                <p className="all-price">{Commas(item.price)}원</p>
-                <div className="like">
-                  <img src={HeartIcon} alt="Heart" />
-                  <p>{item.favoriteCount}</p>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      )}
-    </div>
+    </>
   );
 }
