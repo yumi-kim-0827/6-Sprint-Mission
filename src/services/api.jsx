61c1ba8e0bbbd6961bb42318fbceddbe10da5d26
Hyunji <hjcho2303@naver.com>
2024-04-15 19:09:48 +0900

    🚸 Exception handling for data requests

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,9 @@
 export async function getItems({ order = 'createdAt', offset = 0, limit = 10 }) {
   const query = `order=${order}`;
   const response = await fetch(`https://panda-market-api.vercel.app/products?${query}`);
+  if (!response.ok) {
+    throw new Error('정보를 불러오는데 실패했습니다');
+  }
   const body = await response.json();
   return body;
 }
