cf9cd99d53702176f06d3504e83fe8fb1a5afcd2
Bersk3r <kiscwook@gmail.com>
2024-04-15 15:03:23 +0900

    fix: 기본 생성 파일 내용 수정

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header/Header";
+import Items from "./Items/Items";
+
+function App() {
+    return (
+        <>
+          <Header className="header-container"/>
+          {/*<Items />*/}
+        </>
+    );
+}
+
+export default App;
