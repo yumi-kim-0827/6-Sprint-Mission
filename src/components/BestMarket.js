adacdbec0663a62681ddc9fe443075a3898b54f3
김제완 <jeeee_in@naver.com>
2024-04-15 22:17:17 +0900

    refactor: 베스트상품 데이터 부분 수정

--- src/components/BestMarket.js
+++ src/components/BestMarket.js
@@ -1,65 +1,44 @@
-import iPad from "../assets/img/mock/ipad.png";
-import Book from "../assets/img/mock/book.png";
-import WashingMachine from "../assets/img/mock/washing-machine.png";
-import Oven from "../assets/img/mock/oven.png";
-import HeartIcon from "../assets/icon/ic_heart.svg";
-import { useMediaQuery } from "react-responsive";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../stlye/bestMarket.css";
 import Commas from "../util/Commas";
-
-const BEST_DATA = [
-  {
-    id: 1,
-    images: [iPad],
-    name: "아이패드 미니 팝니다",
-    price: 500_000,
-    favoriteCount: 240,
-  },
-  {
-    id: 2,
-    images: [Book],
-    name: "책 팝니다",
-    price: 50_000,
-    favoriteCount: 800,
-  },
-  {
-    id: 3,
-    images: [WashingMachine],
-    name: "세탁기 팝니다",
-    price: 500_000,
-    favoriteCount: 700,
-  },
-  {
-    id: 4,
-    images: [Oven],
-    name: "오븐 팝니다",
-    price: 300_000,
-    favoriteCount: 500,
-  },
-];
+import HeartIcon from "../assets/icon/ic_heart.svg";
+import { getData } from "./API";
+import { useMediaQuery } from "react-responsive";
 
 export default function BestMarket() {
+  const [bestData, setBestData] = useState([]);
   const isMobile = useMediaQuery({
     query: "(max-width: 767px)",
   });
   const isTablet = useMediaQuery({
-    query: "(min-width:768px) and (max-width: 1279px)",
+    query: "(min-width: 768px) and (max-width: 1279px)",
   });
 
-  var showData = [];
+  useEffect(() => {
+    const fetchBestData = async () => {
+      try {
+        let size = 4; // 기본적으로 4개의 데이터를 가져옴
+        if (isMobile) size = 1; // 모바일일 때는 1개의 데이터만 가져옴
+        else if (isTablet) size = 2; // 태블릿일 때는 2개의 데이터를 가져옴
 
-  if (isMobile) showData = BEST_DATA.slice(0, 1);
-  else if (isTablet) showData = BEST_DATA.slice(0, 2);
-  else showData = BEST_DATA;
+        const data = await getData({ page: 1, size, order: "favorite" });
+        setBestData(data.list);
+      } catch (error) {
+        console.error("Error fetching best data:", error);
+      }
+    };
+
+    fetchBestData();
+  }, [isMobile, isTablet]);
 
   return (
     <div className="best-market">
       <h1 className="best-title">베스트 상품</h1>
       <div className="cards">
-        {showData.map((item) => (
-          <Link>
-            <div key={item.id} className="card">
+        {bestData.map((item) => (
+          <Link to={`/product/${item.id}`} key={item.id}>
+            <div className="card">
               <img className="best-img" src={item.images[0]} alt={item.name} />
               <p className="best-name">{item.name}</p>
               <p className="best-price">{Commas(item.price)}원</p>
