3d6a649f027f1367f50ac8985ceba5df12f714cf
eprnf <eprnf21@gmail.com>
2024-04-16 00:44:04 +0900

    refactor(mento): 하트아이콘관련 함수 이름 변경 및 인라인 메서드 정리

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,22 +6,25 @@ function Card({ item }) {
 
   const { name, price, favoriteCount, images } = item;
 
+  const previewImage = images[0];
+  const formattedPrice = `${price.toLocaleString()}원`;
+
   const handleFavoriteClick = () => {
     setIsFavorite(!isFavorite);
   };
 
-  const handleFavoriteClassName = isFavorite
+  const getFavoriteIconClassName = isFavorite
     ? "icon-heart-active"
     : "icon-heart";
 
   return (
     <div className="Card">
-      <img className="Card__img" src={images[0]} alt="상품 이미지" />
+      <img className="Card__img" src={previewImage} alt="상품 이미지" />
       <p className="Card__name">{name}</p>
-      <p className="Card__price">{price.toLocaleString()}원</p>
+      <p className="Card__price">{formattedPrice}</p>
       <span className="Card__favorites">
         <button className="Card__favorites-btn" onClick={handleFavoriteClick}>
-          <i className={handleFavoriteClassName}></i>
+          <i className={getFavoriteIconClassName}></i>
         </button>
         {isFavorite ? favoriteCount + 1 : favoriteCount}
         {/* 서버에는 반영 x */}
