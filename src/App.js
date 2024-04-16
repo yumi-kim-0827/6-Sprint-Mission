e25eda2ccbe4817205e5b97763c305e7d4494672
이지영 <69416798+leejyart@users.noreply.github.com>
2024-04-16 13:54:45 +0900

    Revert "[이승헌] sprint 5"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import ProductPage from "./pages/ProductPage";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/items" element={<ProductPage />} />
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
