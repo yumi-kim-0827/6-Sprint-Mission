88f9750e4a44728aebb63062ae7dd0e32e676253
CHAE <cowhdals1111@gmail.com>
2024-04-16 19:20:00 +0900

    [FIX] 모바일 정렬 아이콘 추가

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <>
       <Header />
-      <>
+      <main>
         <Outlet />
-      </>
+      </main>
     </>
   );
 }
--- src/ItemPage.js
+++ src/ItemPage.js
@@ -5,18 +5,15 @@ import "./css/itemPage.css";
 import useSetNumOfItemsToShow from "./api/hooks/setNumOfItemsToShow";
 
 const ItemPage = () => {
-  const [bestProductsNumOfItemsToShow,ProductsNumOfItemsToShow] = useSetNumOfItemsToShow({ bestProducts:[1,2,4], Products:[4,6,10] })
+  const [bestProductsNumOfItemsToShow, ProductsNumOfItemsToShow] =
+    useSetNumOfItemsToShow({ bestProducts: [1, 2, 4], Products: [4, 6, 10] });
 
-    
   return (
     <>
-      
-      <main>
-        <div className="main-content">
-          <BestProducts numOfItemsToShow={bestProductsNumOfItemsToShow}/>
-          <Products numOfItemsToShow={ProductsNumOfItemsToShow}/>
-        </div>
-      </main>
+      <div className="main-content">
+        <BestProducts numOfItemsToShow={bestProductsNumOfItemsToShow} />
+        <Products numOfItemsToShow={ProductsNumOfItemsToShow} />
+      </div>
     </>
   );
 };
