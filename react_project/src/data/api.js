a512447a4dfd7d6202ff9edd27c3ba7d4d9e0f0c
YeongTaek <yeongtaek2109@gmail.com>
2024-04-16 20:20:59 +0900

    api 작성

--- react_project/src/data/api.js
+++ react_project/src/data/api.js
@@ -1,5 +1,27 @@
-export async function getProducts() {
-  const response = await fetch('https://panda-market-api.vercel.app/products/');
-  const body = await response.json();
-  return body;
+export async function getProducts(order = 'recent', page = 1) {
+  try {
+    let query = `orderBy = ${order}`;
+    if (order !== 'recent' && order !== 'favorite') {
+      query = `keywords = ${order}`;
+    }
+    const response = await fetch(
+      `https://panda-market-api.vercel.app/products?page=${page}&pageSize=10&${query}`
+    );
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(`${error} : error`);
+  }
+}
+
+export async function getBestProduct() {
+  try {
+    const response = await fetch(
+      `https://panda-market-api.vercel.app/products/?orderBy=favorite`
+    );
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(`${error} : error`);
+  }
 }
