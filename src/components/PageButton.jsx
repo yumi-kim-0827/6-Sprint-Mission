5b1389b70e175b8239906a0ad26191948fed6440
WooGi1020 <jungwk1020@gmail.com>
2024-04-17 20:20:27 +0900

    🔨 좌우측 화살표 버튼 로직 구현

--- src/components/PageButton.jsx
+++ src/components/PageButton.jsx
@@ -1,18 +1,32 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import "./PageButton.css";
 
 const PageButton = ({ handlePageNum, handlePage }) => {
   let pageArr = [];
+  const [buttonNum, setButtonNum] = useState();
   for (let i = 1; i <= handlePageNum(); i++) {
     pageArr.push(i);
   }
   const getButtonValue = (e) => {
     const value = e.target.value;
+    setButtonNum(value);
     handlePage(value);
+  };
+  const pageDown = () => {
+    if (buttonNum > 1) {
+      setButtonNum(buttonNum * 1 - 1);
+      handlePage(buttonNum * 1 - 1);
+    }
+  };
+  const pageUp = () => {
+    if (buttonNum < pageArr.length) {
+      setButtonNum(buttonNum * 1 + 1);
+      handlePage(buttonNum * 1 + 1);
+    }
   };
   return (
     <div className="button">
-      <button>&lt;</button>
+      <button onClick={pageDown}>&lt;</button>
       {pageArr.map((num, idx) => {
         return (
           <button key={idx} onClick={getButtonValue} value={num}>
@@ -20,7 +34,7 @@ const PageButton = ({ handlePageNum, handlePage }) => {
           </button>
         );
       })}
-      <button>&gt;</button>
+      <button onClick={pageUp}>&gt;</button>
     </div>
   );
 };
