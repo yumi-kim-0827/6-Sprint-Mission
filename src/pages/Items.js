403c4dca15bb8d674a55d356c4372c3f9f7831d2
WooGi1020 <jungwk1020@gmail.com>
2024-04-17 18:00:49 +0900

    🔨 페이지네이션을 위한 handlePageNum, handlePage 함수 구현

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,14 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./Items.css";
 import ShowBestProducts from "../components/ShowBestProducts";
 import ShowProducts from "../components/ShowProducts";
+import PageButton from "../components/PageButton";
 import { getProduct, getBestProduct } from "../components/api";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [bestProducts, setBestProducts] = useState([]);
-  const [order, setOrder] = useState("recent");
-
+  const [selectValue, setSelectValue] = useState("최신순");
+  const [totalCount, setTotalCount] = useState();
+  const [order, setOrder] = useState({
+    order: "recent",
+    page: 1,
+    pageSize: 10,
+  });
   const handleLoad = async (orderQuery) => {
     const { list } = await getProduct(orderQuery);
     setProducts(list);
@@ -21,16 +27,47 @@ function App() {
 
   function onChangeSelect(e) {
     const value = e.target.value;
-    value === "좋아요순" ? setOrder("favorite") : setOrder("recent");
+    setSelectValue(value);
+    value === "좋아요순"
+      ? setOrder((prevOrder) => ({ ...prevOrder, order: "favorite" }))
+      : setOrder((prevOrder) => ({ ...prevOrder, order: "recent" }));
   }
+
+  const getSelectValue = (value) => {
+    return value;
+  };
 
   function onChangeInput(e) {
     const value = e.target.value;
-    value === "" ? setOrder("recent") : setOrder(value);
+    value === ""
+      ? setOrder((prevOrder) =>
+          selectValue === "좋아요순" ? { ...prevOrder, order: "favorite" } : { ...prevOrder, order: "recent" }
+        )
+      : setOrder((prevOrder) => ({ ...prevOrder, order: value }));
   }
+
+  const getTotalCount = async () => {
+    const { totalCount } = await getBestProduct();
+    setTotalCount(totalCount);
+  };
+
+  const handlePageNum = () => {
+    if (!isNaN(totalCount)) {
+      const pageNum = Math.ceil(totalCount / order.pageSize);
+      return pageNum;
+    }
+  };
+
+  const handlePage = (value) => {
+    setOrder((prevOrder) => ({
+      ...prevOrder,
+      page: value,
+    }));
+  };
 
   useEffect(() => {
     handleLoad(order);
+    getTotalCount();
   }, [order]);
 
   useEffect(() => {
@@ -41,8 +78,14 @@ function App() {
     <div className="App">
       <div className="container">
         <ShowBestProducts products={bestProducts} />
-        <ShowProducts onChangeSelect={onChangeSelect} onChangeInput={onChangeInput} products={products} />
+        <ShowProducts
+          onChangeSelect={onChangeSelect}
+          onChangeInput={onChangeInput}
+          products={products}
+          getSelectValue={getSelectValue}
+        />
       </div>
+      <PageButton handlePageNum={handlePageNum} handlePage={handlePage} />
     </div>
   );
 }
