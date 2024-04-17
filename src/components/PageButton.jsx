2145976b07cef6c65be87c80043663f62e291348
WooGi1020 <jungwk1020@gmail.com>
2024-04-17 17:59:25 +0900

    🔨 페이지네이션을 위한 pageButton 컴포넌트 로직 구현

--- src/components/PageButton.jsx
+++ src/components/PageButton.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./PageButton.css";
 
-const PageButton = () => {
-  return <>{}</>;
+const PageButton = ({ handlePageNum, handlePage }) => {
+  let pageArr = [];
+  for (let i = 1; i <= handlePageNum(); i++) {
+    pageArr.push(i);
+  }
+  const getButtonValue = (e) => {
+    const value = e.target.value;
+    handlePage(value);
+  };
+  return (
+    <div className="button">
+      <button>&lt;</button>
+      {pageArr.map((num, idx) => {
+        return (
+          <button key={idx} onClick={getButtonValue} value={num}>
+            {num}
+          </button>
+        );
+      })}
+      <button>&gt;</button>
+    </div>
+  );
 };
 
 export default PageButton;
