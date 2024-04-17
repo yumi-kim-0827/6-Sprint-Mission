07498e5e1d2c923f08c7be525adfc5a25d4c41ed
chanhee won <wch2208@gmail.com>
2024-04-17 18:01:27 +0900

    style: AddItem 페이지 스타일 작업 일부
    상품 등록 버튼 영역, 상품 이미지 미리보기 영역 작업
    미리보기 이미지는 기본값을 스켈레톤으로하고 preview 상태값에 따라 조건부
    랜더링으로 이미지 추가되도록 구현할 예정

--- src/pages/addItem/AddItem.jsx
+++ src/pages/addItem/AddItem.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import styles from "./AddItem.module.css";
+import skeleton from "../../assets/bg-img-skeleton.svg";
+
 export default function AddItem() {
+  const [preview, setPreview] = useState(skeleton);
+
   return (
-    <div>
-      <h1>Add Item</h1>
+    <div className={styles.container}>
+      <div className={styles.containerSubmit}>
+        <p className={styles.titleSubmit}>상품 등록하기</p>
+        <button className={styles.btn}>등록</button>
+      </div>
+      <div>
+        <p className={styles.titleImg}>상품 이미지</p>
+        <div className={styles.containerImg}>
+          <div className={styles.preview}>
+            <img src={preview} alt="상품 미리보기 이미지" />
+          </div>
+          <div className={styles.preview} />
+        </div>
+      </div>
+      <div>
+        <p>상품 이름</p>
+        <input type="text" placeholder="상품명을 입력해주세요" />
+      </div>
+      <div>
+        <p>상품 소개</p>
+        <textarea placeholder="상품 소개를 입력해주세요" />
+      </div>
+      <div>
+        <p>판매 가격</p>
+        <input placeholder="판매 가격을 입력해주세요" />
+      </div>
+      <div>
+        <p>태그</p>
+        <input placeholder="태그를 입력해주세요" />
+      </div>
+      <div className="container-tags">
+        <div className="tag">
+          <span>티셔츠</span>
+          <img />
+        </div>
+      </div>
     </div>
   );
 }
