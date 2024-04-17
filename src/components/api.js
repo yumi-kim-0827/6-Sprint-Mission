f4ee0bfaedf054a81ca716d11976336ae98438ee
WooGi1020 <jungwk1020@gmail.com>
2024-04-17 18:00:06 +0900

    🔨 BASE_URL 변수 생성

--- src/components/api.js
+++ src/components/api.js
@@ -1,22 +1,22 @@
-export async function getProduct(order = "recent", page = 1) {
+const BASE_URL = `https://panda-market-api.vercel.app/products`;
+
+export async function getProduct({ order = "recent", page = 1, pageSize = 10 }) {
   try {
     let query = `orderBy=${order}`;
     if (order !== "recent" && order !== "favorite") {
       query = `keyword=${order}`;
     }
-    const response = await fetch(`https://panda-market-api.vercel.app/products?page=${page}&pageSize=10&${query}`);
-    const data = response.json();
-    return data;
+    const response = await fetch(`${BASE_URL}?&page=${page}&pageSize=${pageSize}&${query}`);
+    return response.json();
   } catch (error) {
     console.error(`${error} : error`);
   }
 }
 
-export async function getBestProduct() {
+export async function getBestProduct(pageSize = 4) {
   try {
-    const response = await fetch(`https://panda-market-api.vercel.app/products/?orderBy=favorite`);
-    const data = response.json();
-    return data;
+    const response = await fetch(`${BASE_URL}?orderBy=favorite&page=1&pagesize=${pageSize}`);
+    return response.json();
   } catch (error) {
     console.error(`${error} : error`);
   }
