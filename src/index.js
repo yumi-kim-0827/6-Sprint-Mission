d156897f80454bfd0f6d769694ff54c79f745230
eprnf <eprnf21@gmail.com>
2024-04-17 13:30:28 +0900

    refactor: reset.css, common.css ->  global.js, theme.js 변경

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/reset.css";
-import "./styles/common.css";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/global";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,93 @@
-*,
-html {
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+:root {
+  --color-blue: #3692ff;
+  --color-red: #f74747;
+  --color-gray-50: #f7f7f8;
+  --color-gray-100: #e8ebed;
+  --color-gray-200: #e5e7eb;
+  --color-gray-400: #9ea4a8;
+  --color-gray-500: #72787f;
+  --color-gray-600: #454c53;
+  --color-gray-800: #26282b;
+  --color-gray-900: #1b1d1f;
+  --color-cool-gray-100: #f3f4f6;
+  --color-cool-gray-400: #9ca3af;
+  --color-cool-gray-500: #6b7280;
+  --color-cool-gray-600: #4b5563;
+  --color-cool-gray-800: #1f2937;
+}
+
+button i {
+  pointer-events: none;
+}
+
+.hidden {
+  display: none !important;
+}
+
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("../assets/fonts/Pretendard-Thin.woff") format("woff");
+  font-weight: 100;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("../assets/fonts/Pretendard-Light.woff") format("woff");
+  font-weight: 300;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("../assets/fonts/Pretendard-Regular.woff") format("woff");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("../assets/fonts/Pretendard-Medium.woff") format("woff");
+  font-weight: 500;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("../assets/fonts/Pretendard-Bold.woff") format("woff");
+  font-weight: 700;
+  font-style: normal;
+  font-display: swap;
+}
+
+* {
+  margin: 0;
+  padding: 0;  
   box-sizing: border-box;
 }
 
-html,
+html {
+  font-size: 10px;
+}
+
+body {
+  font-family: "Pretendard", -apple-system, BlinkMacSystemFont, system-ui,
+    Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+    "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+    sans-serif ;
+  font-weight: 400;
+  box-sizing: border-box;
+}
+
+html, 
 body,
 div,
 span,
@@ -88,11 +172,11 @@ video {
   padding: 0;
   border: 0;
   font-size: 100%;
-  font: inherit;
   vertical-align: baseline;
   text-decoration: none;
   color: inherit;
 }
+
 /* HTML5 display-role reset for older browsers */
 article,
 aside,
@@ -192,3 +276,7 @@ button {
   border: none;
   cursor: pointer;
 }
+
+`;
+
+export default GlobalStyle;
