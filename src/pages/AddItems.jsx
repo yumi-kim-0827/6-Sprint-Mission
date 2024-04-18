34fc3303a8d66346799cf324e822f3f0e63eaa2b
WooGi1020 <jungwk1020@gmail.com>
2024-04-18 16:34:06 +0900

    🔨 상태관리용 객체 productValues 생성 및 기본값 지정, handleSubmit 함수 작성

--- src/pages/AddItems.jsx
+++ src/pages/AddItems.jsx
@@ -30,31 +30,61 @@ function ProductTag() {
 }
 
 const AddItems = () => {
+  const [productValues, setProductValues] = useState({
+    productName: "",
+    productIntro: "",
+    productPrice: 0,
+    productImg: null,
+    productTag: "",
+  });
+  const buttonRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="product-form">
+    <form className="product-form" onSubmit={handleSubmit}>
       <div className="product-form-top">
         <h2>상품 등록하기</h2>
-        <button id="register-btn" type="submit">
+        <button id="register-btn" ref={buttonRef} type="submit">
           등록
         </button>
       </div>
-      <ProductImg name="productImg" />
+      <ProductImg name="productImg" value={productValues.productImg} />
       <div className="product-name container">
         <h3>상품명</h3>
         <label htmlFor="product-name"></label>
-        <input name="productName" type="text" id="product-name" placeholder="상품명을 입력해주세요" />
+        <input
+          name="productName"
+          type="text"
+          id="product-name"
+          placeholder="상품명을 입력해주세요"
+          value={productValues.productName}
+        />
       </div>
       <div className="product-intro container">
         <h3>상품 소개</h3>
         <label htmlFor="product-intro"></label>
-        <textarea name="productIntro" id="product-intro" placeholder="상품 소개를 입력해주세요"></textarea>
+        <textarea
+          name="productIntro"
+          id="product-intro"
+          placeholder="상품 소개를 입력해주세요"
+          value={productValues.productIntro}
+        ></textarea>
       </div>
       <div className="product-price container">
         <h3>판매가격</h3>
         <label htmlFor="product-price"></label>
-        <input name="productPrice" type="number" id="product-price" placeholder="판매 가격을 입력해주세요" />
+        <input
+          name="productPrice"
+          type="number"
+          id="product-price"
+          placeholder="판매 가격을 입력해주세요"
+          value={productValues.productPrice}
+        />
       </div>
-      <ProductTag name="productTag" />
+      <ProductTag name="productTag" value={productValues.productIag} />
     </form>
   );
 };
