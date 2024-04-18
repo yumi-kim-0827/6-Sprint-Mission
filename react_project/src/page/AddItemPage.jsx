bcda1f2271a6c455dddb6c06cdbe2b0797de542c
YeongTaek <yeongtaek2109@gmail.com>
2024-04-19 08:41:56 +0900

    페이지 구조 작성

---
+++ react_project/src/page/AddItemPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddItemPage() {
+  return (
+    <div>AddItem</div>
+  )
+}
+
+export default AddItemPage
---
+++ react_project/src/page/MarketPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BestProducts from '../components/BestProducts';
+import AllProducts from '../components/AllProducts';
+
+function MarketPage() {
+  return (
+    <div>
+      <BestProducts />
+      <AllProducts />
+    </div>
+  )
+}
+
+export default MarketPage
