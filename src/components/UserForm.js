53f32c447c369334d268b93cf53838204064e27a
eprnf <eprnf21@gmail.com>
2024-04-18 12:14:29 +0900

    chore: UserForm 내부에서 Input 컴포넌트를 사용하도록 변경

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,18 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
+import { Input } from "./Input";
 import Button from "./Button";
 
 import eyeIcon from "../assets/icon/eye.svg";
 import eyeOffIcon from "../assets/icon/eye-off.svg";
 
-const UserFormInput = ({ label, placeholder, className }) => {
-  const labelName = {
+const UserFormGroup = ({ label, placeholder, className }) => {
+  const labelTable = {
     email: "이메일",
     username: "닉네임",
     password: "비밀번호",
     passwordCheck: "비밀번호 확인",
   };
+
+  const labelName = labelTable[label];
 
   let type;
   switch (label) {
@@ -28,17 +31,10 @@ const UserFormInput = ({ label, placeholder, className }) => {
   }
 
   return (
-    <div className={`${className} user-form__group`}>
-      <label for={label} className="user-form__label">
-        {labelName[label]}
-      </label>
-      <div className="user-form__input">
-        <input
-          type={type}
-          id={label}
-          className="user-form__input-field"
-          placeholder={placeholder}
-        />
+    <StyledUserFormGroup className={className}>
+      <label htmlFor={label}>{labelName}</label>
+      <div className="user-form__input-box">
+        <Input label={label} type={type} placeholder={placeholder} />
         {label.includes("password") && (
           <button type="button" className="toggle-password-btn">
             <i className="icon-eye hidden"></i>
@@ -47,38 +43,24 @@ const UserFormInput = ({ label, placeholder, className }) => {
         )}
       </div>
       <p id={`${label}-error`} className="hidden"></p>
-    </div>
+    </StyledUserFormGroup>
   );
 };
 
-const StyledUserFormInput = styled(UserFormInput)`
-  input &::placeholder {
-    color: var(--color-cool-gray-400);
-  }
-  &:focus {
-    outline: #3692ff solid 1.5px;
-  }
+const StyledUserFormGroup = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-bottom: 8px;
 
-  .user-form__input {
-    position: relative;
-  }
-
-  .user-form__label {
-    display: block;
-    margin-bottom: 16px;
+  label {
     font-weight: 700;
-    font-size: 18px;
-    line-height: 21.48px;
-    color: var(--color-cool-gray-800);
+    font-size: 14px;
   }
 
-  .user-form__input-field {
+  .user-form__input-box {
     position: relative;
-    width: 640px;
-    height: 56px;
-    padding: 16px;
-    border-radius: 12px;
-    background-color: var(--color-cool-gray-100);
   }
 
   .toggle-password-btn {
@@ -109,42 +91,13 @@ const StyledUserFormInput = styled(UserFormInput)`
     background-image: url(${eyeOffIcon});
   }
 
-  @media screen and (max-width: 767px) {
-    .user-form__input-field,
-    .user-form__btn {
-      width: 100%;
+  @media screen and (min-width: 768px) {
+    margin-bottom: 16px;
+
+    label {
+      font-size: 18px;
+      line-height: 21.48px;
     }
-
-    .user-form__label {
-      font-size: 14px;
-      margin-bottom: 8px;
-    }
-
-    .user-form__input {
-      margin-bottom: 16px;
-    }
-  }
-
-  /* input 자동완성 시 자동 스타일 적용 초기화 */
-
-  input:-webkit-autofill,
-  input:-webkit-autofill:hover,
-  input:-webkit-autofill:focus,
-  input:-webkit-autofill:active {
-    -webkit-text-fill-color: #000;
-    -webkit-box-shadow: 0 0 0px 1000px var(--color-cool-gray-100) inset;
-    box-shadow: 0 0 0px 1000px var(--color-cool-gray-100) inset;
-    transition: background-color 5000s ease-in-out 0s;
-  }
-
-  input:autofill,
-  input:autofill:hover,
-  input:autofill:focus,
-  input:autofill:active {
-    -webkit-text-fill-color: #000;
-    -webkit-box-shadow: 0 0 0px 1000px var(--color-cool-gray-100) inset;
-    box-shadow: 0 0 0px 1000px var(--color-cool-gray-100) inset;
-    transition: background-color 5000s ease-in-out 0s;
   }
 `;
 
@@ -155,22 +108,13 @@ const UserForm = ({ type, isValidation = false }) => {
   return (
     <>
       <StyledUserForm action={formAction} className="user-form">
-        <StyledUserFormInput
-          label="email"
-          placeholder="이메일을 입력해주세요"
-        />
+        <UserFormGroup label="email" placeholder="이메일을 입력해주세요" />
         {type === "signup" && (
-          <StyledUserFormInput
-            label="username"
-            placeholder="닉네임을 입력해주세요"
-          />
+          <UserFormGroup label="username" placeholder="닉네임을 입력해주세요" />
         )}
-        <StyledUserFormInput
-          label="password"
-          placeholder="비밀번호를 입력해주세요"
-        />
+        <UserFormGroup label="password" placeholder="비밀번호를 입력해주세요" />
         {type === "signup" && (
-          <StyledUserFormInput
+          <UserFormGroup
             label="passwordCheck"
             placeholder="비밀번호를 다시 한 번 입력해주세요"
           />
@@ -189,13 +133,14 @@ const UserForm = ({ type, isValidation = false }) => {
 };
 
 const StyledUserForm = styled.form`
-  .user-form__group {
-    max-width: 640px;
-    margin-bottom: 24px;
-  }
-
   .user-form__btn {
     width: 640px;
+  }
+
+  @media screen and (max-width: 767px) {
+    .user-form__btn {
+      width: 100%;
+    }
   }
 `;
 
