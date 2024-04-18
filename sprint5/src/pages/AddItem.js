1a31609e8e3324a2e1dd801c202452dcbbc6c737
CelesteCharm <beauciel00@gmail.com>
2024-04-19 01:34:38 +0900

    feat: 폼 생성 및 인풋에 값 입력 시 버튼 활성화

--- sprint5/src/pages/AddItem.js
+++ sprint5/src/pages/AddItem.js
@@ -1,13 +1,45 @@
 import React, { useState } from "react";
+import "./AddItem.css";
 
 function AddItem() {
+  const [productName, setProductName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [tag, setTag] = useState("");
+
+  const isButtonActive = productName && description && price && tag;
+
   return (
-    <main>
-      <div>
-        <h1></h1>
-        <button></button>
+    <form className="addItemForm">
+      <div className="formTitle">
+        <h1>상품 등록하기</h1>
+        <button disabled={!isButtonActive}>등록</button>
       </div>
-    </main>
+      <div className="formInput">
+        <label>상품 이미지</label>
+        <input type="file"></input>
+        <label>상품명</label>
+        <input
+          placeholder="상품명을 입력해주세요"
+          onChange={(e) => setProductName(e.target.value)}
+        />
+        <label>상품 소개</label>
+        <textarea
+          placeholder="상품 소개를 입력해주세요"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label>판매가격</label>
+        <input
+          placeholder="판매 가격을 입력해주세요"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <label>태그</label>
+        <input
+          placeholder="태그를 입력해주세요"
+          onChange={(e) => setTag(e.target.value)}
+        />
+      </div>
+    </form>
   );
 }
 
