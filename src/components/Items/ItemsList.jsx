ef0bb48bb7ccd91c34eba86d1051e2f7bb72b193
Daeyang lee <eodid007@gmail.com>
2024-04-18 15:22:06 +0900

    feat: Items 반응형 적용 (mission 5)

--- src/components/Items/ItemsList.jsx
+++ src/components/Items/ItemsList.jsx
@@ -5,35 +5,65 @@ import SearchBox from "./SearchBox.jsx";
 import Button from "../Button.jsx";
 import DropdownOrder from "./DropdownOrder.jsx";
 import Item from "./Item.jsx";
-import { getData } from "../../apis/apis.js";
+import { getData, getTabletData, getMobileData } from "../../apis/apis.js";
 import Pagenation from "../Pagenation.jsx";
 import { formatCurrency } from "../../utils/utils.js";
-
-const ITEMS_PER_PAGE = 10;
 
 const ItemsList = () => {
   const [data, setData] = useState([]);
   const [order, setOrder] = useState("최신순");
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [displaySize, setDisplaySize] = useState("desktop");
+  const [itemsPerPage, setItemsPerPage] = useState(10);
 
   // 데이터 로드
   useEffect(() => {
     const loadData = async () => {
       const newData = await getData();
       setData(newData.list);
-      setTotalPages(Math.ceil(newData.totalCount / ITEMS_PER_PAGE));
+      setTotalPages(Math.ceil(newData.totalCount / itemsPerPage));
     };
     loadData();
+
+    // window.innerWidth에 따라서 displaySize, itemsPerPage state 변경
+    const resizeHandler = () => {
+      if (window.innerWidth < 1199 && window.innerWidth > 768) {
+        setDisplaySize("tablet");
+        setItemsPerPage(6);
+      } else if (window.innerWidth < 767) {
+        setDisplaySize("mobile");
+        setItemsPerPage(4);
+      } else {
+        setDisplaySize("desktop");
+        setItemsPerPage(10);
+      }
+    };
+    window.addEventListener("resize", resizeHandler);
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
   }, []);
+
+  // DisplaySize State가 변할 때 마다 itemsPerPage를 변경해서 API 재호출
+  useEffect(() => {
+    const orderText = order === "최신순" ? "recent" : "favorite";
+    setCurrentPage(1);
+    const loadData = async () => {
+      const newData = await getData(orderText, currentPage, itemsPerPage);
+      setData(newData.list);
+      setTotalPages(Math.ceil(newData.totalCount / itemsPerPage));
+    };
+    loadData();
+  }, [displaySize]);
 
   // 페이지 및 정렬 변경 시 데이터 로드
   useEffect(() => {
     const orderText = order === "최신순" ? "recent" : "favorite";
     const loadData = async () => {
-      const newData = await getData(orderText, currentPage);
+      const newData = await getData(orderText, currentPage, itemsPerPage);
       setData(newData.list);
-      setTotalPages(Math.ceil(newData.totalCount / ITEMS_PER_PAGE));
+      setTotalPages(Math.ceil(newData.totalCount / itemsPerPage));
     };
     loadData();
   }, [order, currentPage]);
@@ -41,6 +71,7 @@ const ItemsList = () => {
   // 최신순, 좋아요순 정렬
   const orderChangeHandler = (orderText) => {
     setOrder(orderText);
+    // 정렬이 바뀌면 현재 페이지를 1로 바꿈
     setCurrentPage(1);
   };
 
