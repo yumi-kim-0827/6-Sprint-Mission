a7cf4424e46c421023c8ff12e698096a05527b26
eprnf <eprnf21@gmail.com>
2024-04-18 10:36:03 +0900

    chore: /additem 일때도 Nav의 중고마켓이 활성화 되도록 변경

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,34 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import styled from "styled-components";
 
 import Button from "./Button";
 
-import styled from "styled-components";
 import logo from "../assets/img/panda-logo.svg";
 import logoTypo from "../assets/img/panda-logo-typo.svg";
 
 function Navbar() {
+  const { pathname } = useLocation();
+
   return (
     <StyledNavbar className="Navbar">
       <NavLink to="/" className="Navbar__logo-section">
         <img src={logo} className="Navbar__logo" alt="판다마켓" />
         <img src={logoTypo} className="Navbar__logo-typo" alt="판다마켓" />
       </NavLink>
-      <NavLink
+      <StyledNavLink
         to="/community"
-        className={({ isActive }) =>
-          isActive ? "active Navbar__link" : "Navbar__link"
-        }
+        className={({ isActive }) => isActive && "active"}
       >
         자유게시판
-      </NavLink>
-      <NavLink
-        to={["/items", "/additem"]}
-        className={({ isActive }) =>
-          isActive ? "active Navbar__link" : "Navbar__link"
+      </StyledNavLink>
+      <StyledNavLink
+        to="/items"
+        className={
+          pathname === "/items" || pathname === "/additem" ? "active" : ""
         }
       >
         중고마켓
-      </NavLink>
+      </StyledNavLink>
       <NavLink to="/login" className="Navbar__login">
         <Button size="small" className="Navbar__button">
           로그인
@@ -51,22 +51,12 @@ const StyledNavbar = styled.nav`
   background-color: #fff;
   z-index: 2;
 
-  .active.Navbar__link {
-    color: var(--color-blue);
-  }
-
   .Navbar__logo {
     display: none;
   }
 
   .Navbar__logo-typo {
     margin-right: 8px;
-  }
-
-  .Navbar__link {
-    font-weight: 700;
-    font-size: 16px;
-    color: var(--color-cool-gray-600);
   }
 
   .Navbar__login {
@@ -78,13 +68,9 @@ const StyledNavbar = styled.nav`
     height: 42px;
   }
 
-  /* tablet */
-
   @media screen and (min-width: 768px) {
-    .Navbar {
-      gap: 0px;
-      padding: 0 24px;
-    }
+    gap: 0px;
+    padding: 0 24px;
 
     .Navbar__logo {
       display: block;
@@ -94,21 +80,27 @@ const StyledNavbar = styled.nav`
     .Navbar__logo-typo {
       display: none;
     }
-
-    .Navbar__link {
-      width: 109px;
-      font-size: 18px;
-      text-align: center;
-    }
   }
 
-  /* desktop */
-
   @media screen and (min-width: 1200px) {
-    .Navbar {
-      gap: 0px;
-      padding: 0 200px;
-    }
+    gap: 0px;
+    padding: 0 200px;
+  }
+`;
+
+const StyledNavLink = styled(NavLink)`
+  font-weight: 700;
+  font-size: 16px;
+  color: var(--color-cool-gray-600);
+
+  &.active {
+    color: var(--color-blue);
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 109px;
+    font-size: 18px;
+    text-align: center;
   }
 `;
 
