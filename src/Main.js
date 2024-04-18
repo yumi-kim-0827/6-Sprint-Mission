f26fbe0e3e9544544f8071068decab0800cf09a5
haze <skdml20022@gmail.com>
2024-04-18 18:50:00 +0900

    feat: page, router 추가

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./components/App";
+import HomePage from './pages/HomePage'
+import ItemPage from "./pages/ItemPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const Main = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="/items" element={<ItemPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
