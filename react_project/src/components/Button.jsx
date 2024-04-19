3d0fced5d91561a99407a723a9a8c27c1db5a21c
YeongTaek <yeongtaek2109@gmail.com>
2024-04-19 23:00:52 +0900

    Button 컴포넌트 조건 추가

--- react_project/src/components/Button.jsx
+++ react_project/src/components/Button.jsx
@@ -7,11 +7,15 @@ function Button({children}) {
   let design;
   if (children === '로그인') {
     location = "/comunity";
-    design = styles.button;
+    design = styles.button_basic;
   }
   if (children === '상품 등록하기') {
     location = "/additem";
-    design = styles.button;
+    design = styles.button_basic;
+  }
+  if (children === '등록') {
+    location = "/items";
+    design = styles.button_registration;
   }
   
   return (
