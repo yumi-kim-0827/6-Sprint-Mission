5c12f74a3987804a3c862f2d4682a1710ec03150
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-04-19 23:15:19 +0900

    ADD : 라우터 구조 작업 및 페이지 작성 작업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import MarketPage from './pages/MarketPage/MarketPage';
+import AddItemPage from './pages/AddItemPage/AddItemPage';
+import CommunityFeedPage from './pages/CommunityFeedPage/CommunityFeedPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Routes></Routes>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="items" element={<MarketPage />} />
+          <Route path="additem" element={<AddItemPage />} />
+          <Route path="community" element={<CommunityFeedPage />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
