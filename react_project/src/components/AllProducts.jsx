69062a3b2e8f07506f50c6b5fe2f50c25d609b95
YeongTaek <yeongtaek2109@gmail.com>
2024-04-19 10:05:19 +0900

    모든 상품 리스트

--- react_project/src/components/AllProducts.jsx
+++ react_project/src/components/AllProducts.jsx
@@ -1,27 +1,78 @@
-import React, {  } from 'react'
-import Button from './Button'
-import Products from './Products'
-import styles from '../styles/AllProducts.module.css'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import searchIcon from '../assets/ic_search.svg';
+import sortIcon from '../assets/ic_sort.svg'
+import DropdownList from './DropdownList';
+import ProductCard from './ProductCard';
+import { getProducts } from '../data/api';
 
-export default function AllProducts({ products, handleNewstClick, handleFavoriteClick }) {
+const getPageSize = () => {
+  const width = window.innerWidth;
+  if (width < 768) return 4;
+  else if (width < 1280) return 6;
+  else return 10;
+}
+
+function AllProducts() {
+  // 데이터 받아오기
+  const [orderBy, setOrderBy] = useState('recent');
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(1);
+  const [productList, setProductList] = useState([]);
+  
+  const fetchSortedData = async ({orderBy, page, pageSize}) => {
+    const products = await getProducts({ orderBy, page, pageSize});
+    setProductList(products.list);
+  }
+
+  const handleSortSelect = (sortOption) => {
+    setOrderBy(sortOption);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setPageSize(getPageSize());
+    };
+
+    window.addEventListener('resize', handleResize);
+    fetchSortedData({ orderBy, page, pageSize });
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [orderBy, page, pageSize]);
+
+  // 정렬 기준
+
+  // pagination
   return (
-    <div className={styles.wrap}>
-      <div className={styles.products_nav}>
-        <h3>전체 상품</h3>
-        <input type="text" placeholder='검색할 상품을 등록해주세요'/>
-        <Button>상품 등록하기</Button>
-        <select name="" id="">
-          <option value="">최신순</option>
-          <option value="">좋아요순</option>
-        </select>
+    <div>
+      <div>
+        <h1>판매 중인 상품</h1>
+        <Link to="/additem">상품 등록하기</Link>
       </div>
-      <div className={styles.productList_container}>
-        {products.map((product) => {
-          return (
-            <div key={product.id}><Products product={product} /></div>
-          )
-        })}
+  
+      <div>
+        <div>
+          <img src={searchIcon} alt="검색 아이콘" />
+          <input type="text" placeholder='검색할 상품을 입력해 주세요' />
+        </div>
+        <div>
+          <button>
+            <img src={sortIcon} alt="정렬 버튼" />
+          </button>
+
+        </div>
+      </div>
+
+      <div>
+        {productList.map((item) => (
+          <ProductCard item={item} key={`merket_item_${item.id}`} />
+        ))}
       </div>
     </div>
   )
 }
+
+export default AllProducts
