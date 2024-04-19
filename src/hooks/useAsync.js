efbaa90b34a10e93e928d2a8a17d9a77c40a033a
Lin <shl6534@naver.com>
2024-04-19 21:19:52 +0900

    ✨ feat: 에러 상태 처리를 위한 useAsync 훅 생성

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function useAsync (asyncFunction) {
+  const [error, setError] = useState(null);
+
+  const wrappedFunction = async (...args) => {
+    try {
+      setError(null);
+      return await asyncFunction(...args);
+    } catch (error) {
+      setError(error);
+      return;
+    }
+  }
+  return [error, wrappedFunction];
+}
+
+export default useAsync;
