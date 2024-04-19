e8e1c40db5433ae6e406b8af83a6c9a569d1655d
YeongTaek <yeongtaek2109@gmail.com>
2024-04-19 20:50:11 +0900

    MarketPage CSS 작업

--- react_project/src/page/MarketPage.jsx
+++ react_project/src/page/MarketPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import BestProducts from '../components/BestProducts';
 import AllProducts from '../components/AllProducts';
+import styles from '../styles/MarketPage.module.css';
 
 function MarketPage() {
   return (
-    <div>
+    <div className={styles.marketPage_wrap}>
       <BestProducts />
       <AllProducts />
     </div>
