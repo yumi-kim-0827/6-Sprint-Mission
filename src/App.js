25cb1ab822a01435b1b00ce6822a53855aebbf69
chanho <lch0309k@gmail.com>
2024-04-19 22:46:10 +0900

    feat: 서버에서 데이터가져오기 -실패

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Items from "./components/Items";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Items />} />
+      </Routes>
     </BrowserRouter>
   );
 }
