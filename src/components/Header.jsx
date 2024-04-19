47f3864972295624c0336edc1f902a98a0435770
Hyunji <hjcho2303@naver.com>
2024-04-19 12:59:33 +0900

    ♻️ Refactor: NavLink 색상 변환 문제 해결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import './style/Header.css';
 
-function getLinkStyle({ isActive }) {
-  return {
-    color: isActive ? '#3692FF' : '#4B5563',
-  };
-}
-
 export default function Header() {
   return (
     <div className='Header'>
@@ -16,11 +10,19 @@ export default function Header() {
           <source srcSet='/imgs/img_logo.webp' width={153} type='image/webp' />
           <img src='/imgs/img_logo.webp' alt='판다마켓로고' />
         </picture>
-        <NavLink to='/freeboard' style={getLinkStyle}>
-          <button className='Header__free-board'>자유게시판</button>
+        <NavLink to='/freeboard'>
+          {({ isActive }) => (
+            <button className='Header__free-board' style={{ color: isActive ? '#3692FF' : '#4B5563' }}>
+              자유게시판
+            </button>
+          )}
         </NavLink>
-        <NavLink to='/items' style={getLinkStyle}>
-          <button className='Header__flea-market'>중고마켓</button>
+        <NavLink to='/items'>
+          {({ isActive }) => (
+            <button className='Header__flea-market' style={{ color: isActive ? '#3692FF' : '#4B5563' }}>
+              중고마켓
+            </button>
+          )}
         </NavLink>
       </div>
       <button className='Header__login-btn'>로그인</button>
