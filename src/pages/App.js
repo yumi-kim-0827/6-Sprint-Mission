aadbcfa7ac4a620bda57be4a87feeeb9ee843d5a
Lin <shl6534@naver.com>
2024-04-19 16:22:49 +0900

    💄 design: App 컴포넌트 반응형 디자인 전역 설정

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,28 +1,33 @@
-import GlobalStyle from "../styles/GlobalStyle";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PaginationProvider } from "../context/PaginationContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "../Layout";
 import HomePage from "./HomePage";
 import BoardPage from "./BoardPage";
 import AddItemPage from "./AddItem/AddItemPage";
 import FleaMarketPage from "./FleaMarket/FleaMarketPage";
 import LoginPage from "./LoginPage";
-import Layout from "../Layout";
+import { ThemeProvider } from 'styled-components'
+import theme from "../styles/theme";
+import GlobalStyle from "../styles/GlobalStyle";
+
 
 function App() {
   return (
     <PaginationProvider>
-      <GlobalStyle />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="board" element={<BoardPage />} />
-            <Route path="fleamarket" element={<FleaMarketPage />} />
-            <Route path="additem" element={<AddItemPage />} />
-            <Route path="login" element={<LoginPage />} />
-          </Route>
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="board" element={<BoardPage />} />
+              <Route path="fleamarket" element={<FleaMarketPage />} />
+              <Route path="additem" element={<AddItemPage />} />
+              <Route path="login" element={<LoginPage />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </PaginationProvider>
   );
 }
