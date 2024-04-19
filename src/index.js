29ddc77a6ceef96b471045f075433ad2198dff48
CHAE <cowhdals1111@gmail.com>
2024-04-19 20:43:19 +0900

    vercel 배포

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
+//vercel 배포
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
