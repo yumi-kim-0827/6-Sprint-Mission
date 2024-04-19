ccd3e38c04796d9b354d1a6263faaebc171e5e2d
oh-chaeyeon <chaebread555@gmail.com>
2024-04-19 16:52:43 +0900

    feat : 중고마켓 페이지 하던 데까지 커밋

--- src/pages/MarketPage/Market.js
+++ src/pages/MarketPage/Market.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import ItemCard from './components/TestItemCard';
 
 function MarketPage() {
   return (
     <>
       <Header />
-      <main>여기는 MarketPage입니다!</main>
+      <main className="Market-container">
+        <div>
+          <h1>베스트 상품</h1>
+          <ItemCard />
+        </div>
+        <div>
+          <div>
+            <h2>전체 상품</h2>
+
+          </div>
+          <div>
+
+          </div>
+
+        </div>
+      </main>
     </>
   );
 }
