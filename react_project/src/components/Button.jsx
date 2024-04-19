351a23aea8bb54323e4031c47777cf96eccd7ad1
YeongTaek <yeongtaek2109@gmail.com>
2024-04-19 20:51:29 +0900

    children 별 CSS 적용 구현 및 변경

--- react_project/src/components/Button.jsx
+++ react_project/src/components/Button.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from '../styles/Button.module.css';
 
 function Button({children}) {
   let location;
-  if (children === '로그인') location = "/comunity";
-  if (children === '상품 등록하기') location = "/additem";
-
+  let design;
+  if (children === '로그인') {
+    location = "/comunity";
+    design = styles.button;
+  }
+  if (children === '상품 등록하기') {
+    location = "/additem";
+    design = styles.button;
+  }
+  
   return (
-    <button>
-      <Link to={location}>{children}</Link>
-    </button>
+    <Link to={location}>
+      <button className={design}>{children}</button>
+    </Link>
   )
 }
 
