ec86fcf2bbf604ba85846e459a19a65303013763
heony704 <heony704@gamil.com>
2024-04-19 20:12:43 +0900

    feat: 버튼 컴포넌트에 링크 값이 안들어오면 button 요소로 동작

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 import "./index.css";
 
 function Button({ to, children }) {
-  return (
-    <Link className="button" to={to}>
-      {children}
-    </Link>
-  );
+  if (to) {
+    return (
+      <Link className="button" to={to}>
+        {children}
+      </Link>
+    );
+  }
+  return <button className="button">{children}</button>;
 }
 
 export default Button;
