e67d2a09e8c646b657cf4b297c859ed9591bfe63
Hyunji <hjcho2303@naver.com>
2024-04-19 13:29:03 +0900

    ⚰️ Refactor: 사용하지 않는 코드 삭제

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import BestItems from '../components/BestItems';
 import ItemsForSale from '../components/ItemsForSale';
 import { getItems } from '../services/api';
