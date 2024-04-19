1b6255c73c1ed7ba6e1cf6a16d4a2d130917eff1
이정윤 <gramy159@naver.com>
2024-04-19 23:44:29 +0900

    메롱

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import "./css/style.css";
+import Product from "./components/Products";
+import AddItem from "./components/AddItem";
+import GlobalStyle from "./css/GlobalStyle";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/items" element={<Product />} />
+        <Route path="/additem" element={<AddItem />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  )
+};
