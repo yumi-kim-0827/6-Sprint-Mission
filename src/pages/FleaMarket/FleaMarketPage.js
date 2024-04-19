bd3a7eb3187e98863e5f56400bc0faa0c8a067aa
Lin <shl6534@naver.com>
2024-04-19 21:24:14 +0900

    refactor(mentor), wip:  FleaMarketPage 컴포넌트 업데이트 진행중
    - useAsync 및 usePagination 커스텀 훅을 활용하여 FleaMarketPage 컴포넌트 리팩토링 진행중

--- src/pages/FleaMarket/FleaMarketPage.js
+++ src/pages/FleaMarket/FleaMarketPage.js
@@ -1,85 +1,99 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { styled } from "styled-components";
 import BestProducts from "./BestProducts";
 import AllProducts from "./AllProducts";
 import { PAGESIZE } from "../../utils/constant";
 import { getBestProducts, getItems } from "../../api/api";
-import leftButton from "../../assets/btnLeft.svg";
-import rightButton from "../../assets/btnRight.svg";
-import pageNum from "../../assets/pageNumber.svg";
+import Pagination from "./Pagination";
+import useAsync from "../../hooks/useAsync";
+import usePagination from "../../hooks/usePagination";
 
 function FleaMarketPage() {
   const [products, setProducts] = useState([]);
-  const [bestProducts, setBestProducts] = useState([]);
-  const [page, setPage] = useState(1);
   const [orderBy, setOrderBy] = useState("recent");
-  const [loadingError, setLoadingError] = useState(null);
-  
+  const [allProductsError, getAllProductsAsync] = useAsync(getItems);
+  const [bestProductsError, getBestProductsAsync] = useAsync(getBestProducts);
+  const [bestProducts, setBestProducts] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+
+  // Pagination Custom Hook
+  const {
+    page,
+    setPage,
+    totalPage,
+    pageNumbers,
+    handleNextPage,
+    handlePrevPage,
+    handleClickPageNum,
+  } = usePagination(1, totalCount, PAGESIZE);
+
   // 베스트 상품
-  const handleLoadBestProducts = async () => {
-    let bestResult;
-    try {
-      setLoadingError(null);
-      bestResult = await getBestProducts();
-    } catch (error) {
-      setLoadingError(error);
-      return;
-    }
-
-    const { list } = bestResult;
-    const sliceList = list.slice(0, 4);
-    setBestProducts(sliceList);
-  }
-
-  // 전체 상품
-  const handleLoadAllProducts = async (options) => {
-    let result;
-    try {
-      setLoadingError(null);
-      result = await getItems(options);
-    } catch (error) {
-      setLoadingError(error);
-      return;
-    }
+  const handleLoadBestProducts = useCallback(async () => {
+    const result = await getBestProductsAsync();
+    if (!result) return;
 
     const { list } = result;
+    const sliceList = list.slice(0, 4);
+    setBestProducts(sliceList);
+  }, [getBestProducts]);
+
+  // 전체 상품
+  const handleLoadAllProducts = useCallback(async (options) => {
+    const result = await getAllProductsAsync(options);
+    if (!result) return;
+
+    const { list, totalCount } = result;
+    setTotalCount(totalCount);
+
     if (options.page === 1) {
       setProducts(list);
     } else {
       setProducts([...products, ...list]);
     }
-    setPage(options.page + 1);
-  };
-  
-  const handleLoadMore = async () => {
-    setPage((prev) => prev + 1);
-    await handleLoadAllProducts({ page, pageSize: PAGESIZE, orderBy });
+  },[getItems]);
+
+  const handleLoadNext = async (options) => {
+    await handleLoadAllProducts({ page: 1, pageSize: PAGESIZE, orderBy });
   };
 
+  const handleLoadPrev = async (options) => {
+    await handleLoadAllProducts({ page: 1, pageSize: PAGESIZE, orderBy });
+  };
+
+  // const handleLoad = async (number) => {
+  //   setPage(number);
+  //   await handleLoadAllProducts({ page: number, pageSize: PAGESIZE, orderBy });
+  // };
+
   useEffect(() => {
-    const initialPage = 1;
-    setPage(initialPage);
-    handleLoadAllProducts({ page: initialPage, pageSize: PAGESIZE, orderBy });
+    handleLoadAllProducts({ page: 1, pageSize: PAGESIZE, orderBy });
     handleLoadBestProducts();
-  }, [orderBy]);
+  }, [orderBy, handleLoadBestProducts, handleLoadAllProducts]);
 
   return (
     <>
-      {loadingError?.message ? (
-        <ErrorContainer>{loadingError.message}</ErrorContainer>
+      {(bestProductsError?.message || allProductsError?.message) ? (
+        <ErrorContainer>
+          {bestProductsError?.message || allProductsError?.message}
+        </ErrorContainer>
       ) : (
         <FleaMarketContainer>
           <BestProducts bestProducts={bestProducts} />
           <AllProducts products={products} setOrderBy={setOrderBy} />
-          <PageNationIconContainer>
-            <img src={leftButton} alt="왼쪽 버튼" />
-            <img src={pageNum} alt="페이지 번호" />
-            <img src={rightButton} alt="오른쪽 버튼" onClick={handleLoadMore} />
-          </PageNationIconContainer>
+          <Pagination
+            page={page}
+            setPage={setPage}
+            handleLoadPrev={handleLoadPrev}
+            handleLoadNext={handleLoadNext}
+            handleClickPageNum={handleClickPageNum}
+            totalPage={totalCount}
+            pageNumbers={pageNumbers}
+          />
         </FleaMarketContainer>
       )}
     </>
   );
+  
 }
 
 const FleaMarketContainer = styled.div`
@@ -89,16 +103,11 @@ const FleaMarketContainer = styled.div`
   margin-top: 24px;
 `;
 
-const PageNationIconContainer = styled.div`
-  display: flex;
-  gap: 4px;
-`;
-
 const ErrorContainer = styled.h1`
   display: flex;
   justify-content: center;
   font-size: 30px;
   margin-top: 250px;
-`
+`;
 
 export default FleaMarketPage;
