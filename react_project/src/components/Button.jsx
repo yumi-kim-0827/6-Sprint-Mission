d81d7d48d4b1ec8bd8775a1db1e74f35580a6a24
YeongTaek <yeongtaek2109@gmail.com>
2024-04-19 15:30:54 +0900

    Button 재사용성 증가

--- react_project/src/components/Button.jsx
+++ react_project/src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Button({ children }) {
+function Button({children}) {
+  let location;
+  if (children === '로그인') location = "/comunity";
+  if (children === '상품 등록하기') location = "/additem";
+
   return (
-    <div>{children}</div>
+    <button>
+      <Link to={location}>{children}</Link>
+    </button>
   )
 }
+
+export default Button
