b13f9e6863cf1343bf38df53564d2c47f14796cc
chanho <lch0309k@gmail.com>
2024-04-19 14:48:23 +0900

    feat: Navbar 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
 }
 
 export default App;
