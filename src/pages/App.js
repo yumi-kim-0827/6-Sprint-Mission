fd1923b17bf398f2b359e4158d2dc75ca96c4358
Lin <shl6534@naver.com>
2024-04-19 21:17:20 +0900

    🔥remove: PaginationContext 삭제

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,3 @@
-import { PaginationProvider } from "../context/PaginationContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "../Layout";
 import HomePage from "./HomePage";
@@ -6,29 +5,26 @@ import BoardPage from "./BoardPage";
 import AddItemPage from "./AddItem/AddItemPage";
 import FleaMarketPage from "./FleaMarket/FleaMarketPage";
 import LoginPage from "./LoginPage";
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
 import GlobalStyle from "../styles/GlobalStyle";
 
-
 function App() {
   return (
-    <PaginationProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="board" element={<BoardPage />} />
-              <Route path="fleamarket" element={<FleaMarketPage />} />
-              <Route path="additem" element={<AddItemPage />} />
-              <Route path="login" element={<LoginPage />} />
-            </Route>
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </PaginationProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="board" element={<BoardPage />} />
+            <Route path="fleamarket" element={<FleaMarketPage />} />
+            <Route path="additem" element={<AddItemPage />} />
+            <Route path="login" element={<LoginPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
