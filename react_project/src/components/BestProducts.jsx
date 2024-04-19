f3a04834310add51618797fc0b836e070fc64f56
YeongTaek <yeongtaek2109@gmail.com>
2024-04-19 09:12:28 +0900

    BestProduct 기능 구현

---
+++ react_project/src/components/BestProducts.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import { getProducts } from '../data/api';
+import ProductCard from './ProductCard';
+
+const getPageSize = () => {
+  const width = window.innerWidth;
+  if (width < 768) return 1;
+  else if (width < 1280) return 2;
+  else return 4;
+};
+
+function BestProducts() {
+  const [itemList, setItemList] = useState([]);
+  const [pageSize, setPageSize] = useState(getPageSize());
+
+  const fetchSortedData = async ({ orderBy, pageSize }) => {
+    const porducts = await getProducts({ orderBy, pageSize });
+    setItemList(porducts.list);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setPageSize(getPageSize());
+    };
+
+    window.addEventListener('resize', handleResize);
+    fetchSortedData({ orderBy: "favorite", pageSize });
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [pageSize]);
+
+  return (
+    <div>
+      <h1>베스트 상품</h1>
+      <div>
+        {itemList.map((item) => (
+          <ProductCard item={item} key={`bestItem_${item.id}`} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BestProducts
