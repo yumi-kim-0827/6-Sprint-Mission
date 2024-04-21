8e65f1e2592c98737fc0e5de700287a7ffd338e0
YoungJun Bae <dudwns0213@gmail.com>
2024-04-22 02:22:54 +0900

    style: 글로벌 css 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import ReactDOM from "react-dom";
-import App from "./component/App";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./css/globalStyle.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(<App />);
