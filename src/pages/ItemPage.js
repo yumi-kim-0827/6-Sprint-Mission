8ecf0b0b600a4d048e12dae94632a0af042c334d
haze <skdml20022@gmail.com>
2024-04-21 14:54:37 +0900

    style: select-box css 작성

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -4,10 +4,18 @@ import Container from "../components/Container";
 import Button from "../components/Button";
 import CardList from "../components/CardList";
 import Input from "../components/Input";
+import { useState } from "react";
 
 const cn = classNames.bind(style);
 
 const ItemPage = () => {
+  const [order, setOrder] = useState();
+
+  // TODO
+  function handleOrderClick() {
+    setOrder();
+  }
+
   return (
     <Container className={cn("container")}>
       <section className={cn("section")}>
@@ -16,23 +24,37 @@ const ItemPage = () => {
       </section>
       <section className={cn("section")}>
         <h2 className={cn("section-title")}>전체 상품</h2>
-        <Input
-          iconClass={"search-icon"}
-          inputClass={"search-input"}
-          type={"text"}
-          name={"search"}
-          placeholder={"검색할 상품을 입력해주세요"}
-        />
-        <Button
-          isLink={true}
-          text="상품 등록하기"
-          isActive="on"
-          href="/additem"
-        />
-        <select>
-          <option>최신순</option>
-          <option>좋아요순</option>
-        </select>
+        <div className={cn("section-menu")}>
+          <Input
+            iconClass={"search-icon"}
+            inputClass={"search-input"}
+            type={"text"}
+            name={"search"}
+            placeholder={"검색할 상품을 입력해주세요"}
+          />
+          <Button
+            isLink={true}
+            text="상품 등록하기"
+            isActive="on"
+            href="/additem"
+          />
+          {/* TODO */}
+          <div className={cn("select-order")}>
+            <button className={cn("select-btn")}>최신순</button>
+            <ul className={cn("order-list")}>
+              <li className={cn("order-item")}>
+                <button type="button" onClick={handleOrderClick}>
+                  최신순
+                </button>
+              </li>
+              <li className={cn("order-item")}>
+                <button type="button" onClick={handleOrderClick}>
+                  좋아요순
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
         <CardList />
       </section>
     </Container>
