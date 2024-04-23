2a03e729ccb5424e4d313c749032ea930cd4189f
Hyunji <hjcho2303@naver.com>
2024-04-23 19:41:36 +0900

    ✨ Feat: 아이템 아이디로 상세페이지 링크 연결

--- src/components/ItemForSale.jsx
+++ src/components/ItemForSale.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './style/ItemForSale.css';
+import { useNavigate } from 'react-router-dom';
 
-export default function ItemForSale({ price, images, favoriteCount, name }) {
+export default function ItemForSale({ price, images, favoriteCount, name, id }) {
   const formattedPrice = price.toLocaleString();
+  const nav = useNavigate();
 
   return (
     <div className='item'>
-      <picture className='item-for-sale__img-wrap'>
+      <div onClick={() => nav(`/items/${id}`)} className='item-for-sale__img-wrap'>
         <img className='item-for-sale__img' src={images} alt={name} />
-      </picture>
+      </div>
       <div className='item-for-sale__description'>
-        <p className='item-for-sale__title'>{name}</p>
+        <p className='item-for-sale__title' onClick={() => nav(`/items/${id}`)}>
+          {name}
+        </p>
         <p className='item-for-sale__price'>{formattedPrice}원</p>
         <div className='item-for-sale__heart'>
           <img className='item-for-sale__heart__img' src='/imgs/small_grayHeart.webp' alt='' />
