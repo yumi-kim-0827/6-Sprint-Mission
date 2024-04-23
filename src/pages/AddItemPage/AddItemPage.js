7a3322707c08e00a78cde5978004ab7c6c7a543a
JustDevRae <ksrae165@gmail.com>
2024-04-23 17:06:02 +0900

    add : 이미지 파일 업로드 기능 구현

--- src/pages/AddItemPage/AddItemPage.js
+++ src/pages/AddItemPage/AddItemPage.js
@@ -4,27 +4,52 @@ import "./AddItemPage.css";
 
 function AddItemPage() {
 
+  const [values, setValues] = useState({
+    title: '',
+    description: '',
+    price: '',
+    tag: '',
+    imgFile: null,
+  });
+
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+    
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
   return (
     <div className="add-item-section">
-      <div className="add-item-section-header">
-        <h1>상품 등록하기</h1>
-        <button type="submit" className="add-item-button">등록</button>
-      </div>
+      <form onSubmit={handleSubmit} className="add-item-form">
+        <div className="add-item-section-header">
+          <h1>상품 등록하기</h1>
+          <button type="submit" className="add-item-button">등록</button>
+        </div>
 
-      <form className="add-item-form">
-        
-        <FileInput/>
+        <FileInput name='imgFile' value={values.imgFile} onChange={handleChange} />
         <label htmlFor="img">상품명</label>
-        <input id="img" placeholder="상품명을 입력해주세요" />
+        <input name="title" id="img" value={values.title} placeholder="상품명을 입력해주세요" onChange={handleInputChange} />
 
         <label htmlFor="img">상품 소개</label>
-        <textarea id="img" placeholder="상품 소개를 입력해주세요" className="item-description" />
+        <textarea name="description" id="img" value={values.description} placeholder="상품 소개를 입력해주세요" onChange={handleInputChange} className="item-description" />
 
         <label htmlFor="img">판매 가격</label>
-        <input id="img" placeholder="판매 가격을 입력해주세요" />
+        <input name="price" id="img" value={values.price} placeholder="판매 가격을 입력해주세요" onChange={handleInputChange} />
 
         <label htmlFor="img">태그</label>
-        <input id="img" placeholder="태그를 입력해주세요" />
+        <input name="tag" id="img" value={values.tag} placeholder="태그를 입력해주세요" onChange={handleInputChange} />
       </form>
     </div>
 
--- src/pages/AddItemPage/component/FileInput.js
+++ src/pages/AddItemPage/component/FileInput.js
@@ -1,10 +1,18 @@
-import uploadIcon from "../../../assets/plus.svg";
 
-function FileInput() {
+
+function FileInput({ name, value, onChange }) {
+	
+
+	const handleChange = (e) => {
+		const nextValue = e.target.files[0];
+		onChange(name, nextValue);
+	}
+
 	return (
 		<>
 			<label htmlFor="img">상품 이미지</label>
-			<input type="file" id="img" placeholder="이미지 등록" className="add-item-img" />
+			<input type="file" id="img" placeholder="이미지 등록" onChange={handleChange} className="add-item-img" />
+			
 		</>
 	);
 }
