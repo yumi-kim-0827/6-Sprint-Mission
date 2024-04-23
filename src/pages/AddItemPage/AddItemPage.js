fd26f1488428bcd863a1aa5fe8ea088f5ea73186
JustDevRae <ksrae165@gmail.com>
2024-04-23 14:42:25 +0900

    fix : addItem 페이지 js, css 파일 수정

--- src/pages/AddItemPage/AddItemPage.js
+++ src/pages/AddItemPage/AddItemPage.js
@@ -1,51 +1,33 @@
 import { useState } from "react";
-import FileInput from "../AddItemPage/component/FileInput";
+import FileInput from "./component/FileInput";
 import "./AddItemPage.css";
 
 function AddItemPage() {
-  const [inputValue, setInputValue] = useState("");
-  const [isButtonActive, setIsButtonActive] = useState(false);
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-    setIsButtonActive(e.target.value !== "");
-  };
-
-  
 
   return (
-    <form className="addItemSection">
-      <div className="add-section">
+    <div className="add-item-section">
+      <div className="add-item-section-header">
         <h1>상품 등록하기</h1>
-        <button className="item-add-button">등록</button>
+        <button type="submit" className="add-item-button">등록</button>
       </div>
 
-      <div>
-        <h2>상품 이미지</h2>
-        <FileInput />
-      </div>
-      <div>
-        <h2>상품명</h2>
-        <input placeholder="상품명을 입력해주세요" />
-      </div>
-      <div>
-        <h2>상품소개</h2>
-        <input
-          placeholder="상품 소개를 입력해주세요"
-          className="input-description"
-        />
-      </div>
-      <div>
-        <h2>판매가격</h2>
-        <input placeholder="판매 가격을 입력해주세요" />
-      </div>
-      <div>
-        <h2>태그</h2>
-        <input placeholder="태그를 입력해주세요" className="tag-input" />
-      </div>
+      <form className="add-item-form">
+        
+        <FileInput/>
+        <label htmlFor="img">상품명</label>
+        <input id="img" placeholder="상품명을 입력해주세요" />
 
-      <div></div>
-    </form>
+        <label htmlFor="img">상품 소개</label>
+        <textarea id="img" placeholder="상품 소개를 입력해주세요" className="item-description" />
+
+        <label htmlFor="img">판매 가격</label>
+        <input id="img" placeholder="판매 가격을 입력해주세요" />
+
+        <label htmlFor="img">태그</label>
+        <input id="img" placeholder="태그를 입력해주세요" />
+      </form>
+    </div>
+
   );
 }
 
--- src/pages/AddItemPage/component/FileInput.js
+++ src/pages/AddItemPage/component/FileInput.js
@@ -1,31 +1,13 @@
-import { useState } from "react";
-import plus from "../../../assets/plus.svg";
+import uploadIcon from "../../../assets/plus.svg";
 
 function FileInput() {
-  const [value, setValue] = useState();
-
-  const handleChange = (e) => {
-    const nextValue = e.target.files[0];
-    setValue(nextValue);
-  };
-
-  return (
-    <div className="img-add-button">
-      <label htmlFor="file-upload">
-        <div className="img-add-button-align">
-          <img src={plus} alt="+이미지" />
-          <span>이미지 등록</span>
-        </div>
-      </label>
-
-      <input
-        id="file-upload"
-        type="file"
-        onChange={handleChange}
-        className="img-add-button-hidden"
-      />
-    </div>
-  );
+	return (
+		<>
+			<label htmlFor="img">상품 이미지</label>
+			<input type="file" id="img" placeholder="이미지 등록" className="add-item-img" />
+		</>
+	);
 }
+
 
 export default FileInput;
