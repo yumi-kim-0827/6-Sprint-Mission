db86ebe141dd8e530aed20ee553d4c01aeba2034
WooGi1020 <jungwk1020@gmail.com>
2024-04-23 16:14:07 +0900

    ✨ feat : 동적 경로 추가에 따른 boolean 할당문 수정

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import "./Header.css";
 import longlogo from "../assets/long_logo.png";
 import shortlogo from "../assets/short_logo.png";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, useParams } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
 
-  const isItemPage = location.pathname === "/Items" || location.pathname === "/additems";
+  const isItemPage = location.pathname.startsWith("/Items") || location.pathname === "/additems";
 
   return (
     <>
