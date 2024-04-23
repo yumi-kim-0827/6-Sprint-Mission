f2dcb48135a86abf97afebd8c3acc6cd9796d570
임진조 <dlawlswh123@gmail.com>
2024-04-23 15:44:26 +0900

    feat: 이미지 삽입 취소 버튼을 이미지에 맞게 고정

--- pandamarket/src/components/additem/FileInput.js
+++ pandamarket/src/components/additem/FileInput.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import fileUpLoad from "../../assets/file_upload.svg";
 import styled from "styled-components";
-
+import cancelButton from "../../assets/file_cancel.svg";
 const FileInputHide = styled.input`
   display: none;
 `;
@@ -14,10 +14,33 @@ const FileInputLabel = styled.label`
   margin: 12px 0 24px;
 `;
 
+const ImgDiv = styled.div`
+  position: relative;
+  width: 282px;
+  height: 282px;
+`;
+
 const style = {
   width: "282px",
   height: "282px",
 };
+
+const Img = styled.img`
+  background-color: #f3f4f6;
+  border-radius: 12px;
+`;
+
+const FileCancelButton = styled.button`
+  position: absolute;
+  right: 12px;
+  top: 12px;
+  width: 20px;
+  height: 20px;
+  background-image: url(${cancelButton});
+  border: none;
+  border-radius: 999px;
+  background-position: center;
+`;
 
 function FileInput({ name, value, onChange }) {
   const inputRef = useRef();
@@ -43,7 +66,13 @@ function FileInput({ name, value, onChange }) {
   }, [value]);
   return (
     <>
-      {value && <img style={style} src={preview} alt="이미지 미리보기" />}
+      {value && (
+        <ImgDiv>
+          <Img style={style} src={preview} alt="이미지 미리보기" />
+          {value && <FileCancelButton onClick={handleClearClick} />}
+        </ImgDiv>
+      )}
+
       {/* <img src={fileUpLoad} style={style} alt="이미지 등록" /> */}
       <FileInputHide
         type="file"
@@ -52,7 +81,6 @@ function FileInput({ name, value, onChange }) {
         id="imgFile"
       />
       {!value && <FileInputLabel htmlFor="imgFile" />}
-      {value && <button onClick={handleClearClick}>X</button>}
     </>
   );
 }
