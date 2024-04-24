602d7fa261e43a60ab2606e11942785c517ef5d0
Hyunji <hjcho2303@naver.com>
2024-04-24 18:50:29 +0900

    💄 Comment component ui 수정

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,57 @@
-import React from 'react';
 import Profile from './Profile';
+import styled from 'styled-components';
+import ic_kebab from '../assets/ic_kebab.png';
 
-export default function Comment({ text, time }) {
+export default function Comment({ content, createdAt, ...comment }) {
+  const { nickname, image } = comment.writer;
   return (
-    <div>
-      <p>댓글 {text}</p>
-      <img src='' alt='드롭다운 메뉴' />
-      <Profile>{time}</Profile>
-      <div>horizontal line</div>
-    </div>
+    <CommentContainer>
+      <p>{content}</p>
+      <Kebab>
+        <img src={ic_kebab} alt='' />
+      </Kebab>
+      <Profile image={image} nickname={nickname}>
+        <CreatedAt>{createdAt}</CreatedAt>
+      </Profile>
+      <HorizontalLine></HorizontalLine>
+    </CommentContainer>
   );
 }
+const CreatedAt = styled.span`
+  color: #9ca3af;
+  font-size: 12px;
+`;
+
+const CommentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  font-size: 16px;
+  color: #1f2937;
+  position: relative;
+`;
+
+const Kebab = styled.div`
+  position: absolute;
+  right: 0;
+  width: 24px;
+  height: 24px;
+
+  & img {
+    width: 100%;
+  }
+`;
+
+const HorizontalLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #e5e7eb;
+  margin-bottom: 16px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 16px;
+  }
+
+  @media (min-width: 1199px) {
+  }
+`;
