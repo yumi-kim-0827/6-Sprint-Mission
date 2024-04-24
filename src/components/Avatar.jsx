fc9cad5ede757d010dc1bb58f06322726e808bb7
Hyunji <hjcho2303@naver.com>
2024-04-24 18:50:01 +0900

    💄 Avatar component ui 수정

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Avatar({ img }) {
   return (
-    <div>
+    <AvatarImg>
       <img src={img} alt='프로필 이미지' />
-    </div>
+    </AvatarImg>
   );
 }
+
+const AvatarImg = styled.div`
+  width: 40px;
+  height: 40px;
+
+  & img {
+    width: 100%;
+  }
+`;
