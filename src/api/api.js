7b52401fd50eb137d7698ebd973fe940ec902d3a
Lin <shl6534@naver.com>
2024-04-24 19:31:36 +0900

    refactor(mentor): axios interceptor을 이용해 에러 처리 모듈화

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,24 @@
+import axiosInstance from "../utils/axiosInstance";
+
 export async function getItems({
   page = 1,
   pageSize = 10,
   orderBy = 'recent',
 }) {
-  const query=`page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`;
-  const response = await fetch(`https://panda-market-api.vercel.app/products?${query}`);
-  if(!response.ok) {
-    throw new Error('데이터를 불러오는데 실패했습니다.');
+  try {
+    const query=`page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`;
+    const { data } = await axiosInstance.get(`products?${query}`);
+    return data;
+  } catch (error) {
+    throw error;
   }
-  const data = await response.json();
-  return data;
 } 
 
 export async function getBestProducts() {
-  const response = await fetch('https://panda-market-api.vercel.app/products?orderBy=favorite');
-  if (!response.ok) {
-      throw new Error('데이터를 불러오는데 실패했습니다.');
+  try {
+    const { data } = await axiosInstance.get('/products?orderBy=favorite');
+    return data;
+  } catch (error) {
+    throw error;
   }
-  const data = await response.json();
-  return data;
 }
