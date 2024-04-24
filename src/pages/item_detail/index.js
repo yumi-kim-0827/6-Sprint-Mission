cd9750d632311c77b53c28ebd1fd002530f05b80
CHAE <cowhdals1111@gmail.com>
2024-04-24 21:57:37 +0900

    useNavigate 사용해서 목록으로 돌아가기 구현

--- src/pages/item_detail/index.js
+++ src/pages/item_detail/index.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from "react";
-import {
-  Main,
-  BackToListButton,
-  CommentSection,
-} from "./style";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Main, BackToListButton, CommentSection } from "./style";
 import ic_back from "../../image/ic_back.svg";
 import ProductInfo from "./ProductInfo";
 import Inquiry from "./Inquiry";
 
 const ItemDetail = () => {
 
+  const navigate = useNavigate();
 
   return (
     <Main>
-      <ProductInfo/>
-      <Inquiry/>
+      <ProductInfo />
+      <Inquiry />
       <CommentSection></CommentSection>
-      <BackToListButton>
+      <BackToListButton onClick={()=>navigate('/items')}>
         <span>목록으로 돌아가기</span>
         <img src={ic_back} alt="목록으로 돌아가기" />
       </BackToListButton>
