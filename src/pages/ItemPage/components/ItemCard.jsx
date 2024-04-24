02cd56fea2ba907c8bf81a80487ade9932b6839c
wayandway <wayandway@kakao.com>
2024-04-24 15:46:31 +0900

    feat: 상품 상세 페이지 라우팅 추가

--- src/pages/ItemPage/components/ItemCard.jsx
+++ src/pages/ItemPage/components/ItemCard.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
 import HeartIconUrl from "../../../assets/icons/icon-heart.svg";
 
 function ItemCard({ item }) {
   return (
     <div className="item-card">
-      <img src={item.images[0]} alt={item.name} className="item-card-img" />
+      <Link to={`/items/${item.id}`}>
+        <img src={item.images[0]} alt={item.name} className="item-card-img" />
+      </Link>
       <div className="item-card-description">
-        <h2 className="item-name">{item.name}</h2>
+        <Link to={`/items/${item.id}`}>
+          <h2 className="item-name">{item.name}</h2>
+        </Link>
         <p className="item-price">{item.price.toLocaleString()}원</p>
         <div className="item-favorite">
           <img src={HeartIconUrl} />
