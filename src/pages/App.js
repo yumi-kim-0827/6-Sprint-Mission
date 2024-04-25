b3a4fd6822c6e5e713346faeb7e6acf55af17d68
Lin <shl6534@naver.com>
2024-04-25 23:47:51 +0900

    feat: 상세 제품 페이지 Route 설정

--- src/pages/App.js
+++ src/pages/App.js
@@ -9,6 +9,7 @@ import NotFoundPage from "./NotFoundPage";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
 import GlobalStyle from "../styles/GlobalStyle";
+import ProductDetailPage from "./FleaMarket/ProductDetail/ProductDetailPage";
 
 function App() {
   return (
@@ -19,7 +20,10 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="board" element={<BoardPage />} />
-            <Route path="fleamarket" element={<FleaMarketPage />} />
+            <Route path="items">
+              <Route index element={<FleaMarketPage />} />
+              <Route path=":productId" element={<ProductDetailPage />} />
+            </Route>
             <Route path="additem" element={<AddItemPage />} />
             <Route path="login" element={<LoginPage />} />
             <Route path="*" element={<NotFoundPage />} />
