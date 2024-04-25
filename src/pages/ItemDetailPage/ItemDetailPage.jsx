c9a8df2fe73690b3d096212b0bb18623b2039b37
wayandway <wayandway@kakao.com>
2024-04-25 21:56:01 +0900

    feat: 목록으로 돌아가기 버튼 추가

--- src/pages/ItemDetailPage/ItemDetailPage.jsx
+++ src/pages/ItemDetailPage/ItemDetailPage.jsx
@@ -1,11 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import ItemDetailSection from "./components/ItemDetailSection";
 import CommentSection from "./components/Comment/CommentSection";
+import BackIconUrl from "../../assets/icons/icon-back-page.svg";
+import "./ItemDetailPage.scss";
 
 function ItemDetailPage() {
+  const navigate = useNavigate();
+  const goBackPage = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="page-wrapper">
       <ItemDetailSection />
       <CommentSection />
+      <button onClick={goBackPage} className="btn-primary back-icon">
+        목록으로 돌아가기
+        <img src={BackIconUrl} alt="목록으로 돌아가기 아이콘" />
+      </button>
     </div>
   );
 }
