7498c555e717a64eb2f81cc512c3dca372e8fee2
Hyunji <hjcho2303@naver.com>
2024-04-25 12:58:48 +0900

    ✨ Feat: 댓글 작성 시간 "몇 시간 전" 으로 보이는것 구현

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,20 +3,43 @@ import styled from 'styled-components';
 import ic_kebab from '../assets/ic_kebab.png';
 
 export default function Comment({ content, createdAt, ...comment }) {
+  const detailDate = a => {
+    const milliSeconds = new Date() - a;
+    const seconds = milliSeconds / 1000;
+    if (seconds < 60) return `방금 전`;
+    const minutes = seconds / 60;
+    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+    const weeks = days / 7;
+    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+    const months = days / 30;
+    if (months < 12) return `${Math.floor(months)}개월 전`;
+    const years = days / 365;
+    return `${Math.floor(years)}년 전`;
+  };
+
+  const nowDate = detailDate(new Date(createdAt));
+
+  console.log(createdAt);
+  console.log(nowDate);
   const { nickname, image } = comment.writer;
   return (
     <CommentContainer>
       <p>{content}</p>
       <Kebab>
-        <img src={ic_kebab} alt='' />
+        <img src={ic_kebab} alt='메뉴바' />
       </Kebab>
       <Profile image={image} nickname={nickname}>
-        <CreatedAt>{createdAt}</CreatedAt>
+        <CreatedAt>{nowDate}</CreatedAt>
       </Profile>
       <HorizontalLine></HorizontalLine>
     </CommentContainer>
   );
 }
+
 const CreatedAt = styled.span`
   color: #9ca3af;
   font-size: 12px;
