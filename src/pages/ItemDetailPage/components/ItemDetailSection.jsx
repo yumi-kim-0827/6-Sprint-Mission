2a12b27cacbfb2e75e465cf27d59c7f1fb1e9681
wayandway <wayandway@kakao.com>
2024-04-25 21:55:22 +0900

    style: 상품 상세 정보 섹션 스타일 적용

--- src/pages/ItemDetailPage/components/ItemDetailSection.jsx
+++ src/pages/ItemDetailPage/components/ItemDetailSection.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getProductDetail } from "../../../services/api";
+import HeartIconUrl from "../../../assets/icons/icon-heart.svg";
 
 function ItemDetailSection() {
   const [itemDetail, setItemDetail] = useState({});
@@ -16,13 +17,42 @@ function ItemDetailSection() {
   }, []);
 
   return (
-    <div>
-      <img src={itemDetail.images} alt={itemDetail.name} />
-      {itemDetail.name}
-      {itemDetail.price}
-      {itemDetail.description}
-      {itemDetail.tags}
-    </div>
+    <>
+      <div className="item-detail-card">
+        <img
+          src={itemDetail.images}
+          alt={itemDetail.name}
+          className="item-detail-img"
+        />
+        <div className="item-details">
+          <div>
+            <h1 className="item-detail-name">{itemDetail.name}</h1>
+            <div className="item-detail-price">
+              {Number(itemDetail.price).toLocaleString()}원
+            </div>
+            <hr />
+            <h2>상품 소개</h2>
+            <div className="item-detail-description">
+              {itemDetail.description}
+            </div>
+            <h2>상품 태그</h2>
+            <div className="item-detail-tags">
+              {itemDetail.tags?.map((tag) => (
+                <nav key={itemDetail.id} className="item-detail-tag">
+                  #{tag}
+                </nav>
+              ))}
+            </div>
+          </div>
+
+          <nav className="item-detail-favorite">
+            <img src={HeartIconUrl} />
+            {itemDetail.favoriteCount}
+          </nav>
+        </div>
+      </div>
+      <hr />
+    </>
   );
 }
 
