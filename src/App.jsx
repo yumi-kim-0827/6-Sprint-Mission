5dce85c4dee49127122b207113f7ae21c5b5263a
Hyunji <hjcho2303@naver.com>
2024-04-25 09:19:24 +0900

    🔥 Feat: 데이터 불러오는 방법 변경하여 안쓰는 코드 및 파일 삭제
    useContext 사용했었는데 api로 아이디 불러와서 사용하여 파일 정리했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useReducer, useRef, createContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import useFetchItems from './hooks/useFetchItems';
 import './styles/App.css';
 
 import Main from './Main';
@@ -10,80 +8,19 @@ import AddItem from './pages/AddItem';
 import ItemDetails from './pages/ItemDetails';
 import NotFound from './pages/NotFound';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'SET_ITEMS':
-      return action.data;
-    case 'CREATE':
-      return [action.data, ...state];
-    case 'UPDATE':
-      return state.map(item => (String(item.id) === String(action.data.id) ? action.data : item));
-    case 'DELETE':
-      return state.filter(item => String(item.id) !== String(action.id));
-    default:
-      return state;
-  }
-}
-
-export const ItemStateContext = createContext();
-export const ItemDispatchContext = createContext();
-
 export default function App() {
-  const { items, loading, error } = useFetchItems({ offset: 0, limit: 10 });
-  const [data, dispatch] = useReducer(reducer, []);
-
-  useEffect(() => {
-    dispatch({ type: 'SET_ITEMS', data: items });
-  }, [items]);
-  const idRef = useRef(10);
-
-  if (loading) return <div>Loading...🐼🎋</div>;
-  if (error) return <div>Error 발생!</div>;
-
-  const onCreate = (createdDate, description) => {
-    dispatch({
-      type: 'CREATE',
-      data: {
-        createdDate,
-        id: idRef.current++,
-        description,
-      },
-    });
-  };
-
-  const onUpdate = (id, description) => {
-    dispatch({
-      type: 'UPDATE',
-      data: {
-        id,
-        description,
-      },
-    });
-  };
-
-  const onDelete = id => {
-    dispatch({
-      type: 'DELETE',
-      id,
-    });
-  };
-
   return (
     <>
-      <ItemStateContext.Provider value={data}>
-        <ItemDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
-          <Routes>
-            <Route path='/' element={<Main />}>
-              <Route index element={<Items />} />
-              <Route path='/items' element={<Items />} />
-              <Route path='/items/:id' element={<ItemDetails />} />
-              <Route path='/additem' element={<AddItem />} />
-              <Route path='/freeboard' element={<FreeBoard />} />
-            </Route>
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </ItemDispatchContext.Provider>
-      </ItemStateContext.Provider>
+      <Routes>
+        <Route path='/' element={<Main />}>
+          <Route index element={<Items />} />
+          <Route path='/items' element={<Items />} />
+          <Route path='/items/:id' element={<ItemDetails />} />
+          <Route path='/additem' element={<AddItem />} />
+          <Route path='/freeboard' element={<FreeBoard />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   );
 }
