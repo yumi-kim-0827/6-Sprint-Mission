c260808768c0113e025bd000e24b9d01cb9cadf9
wayandway <wayandway@kakao.com>
2024-04-25 23:26:39 +0900

    feat: 댓글 리스트 스타일 적용

--- src/pages/ItemDetailPage/components/Comment/CommentList.jsx
+++ src/pages/ItemDetailPage/components/Comment/CommentList.jsx
@@ -1,9 +1,23 @@
 function CommentList({ comment }) {
+  const dateString = comment.createdAt.split("T")[0];
+
   return (
-    <div>
-      <img src={comment.writer.image} alt={comment.writer} />
-      {comment.nickname}
-      {comment.content}
+    <div className="comment">
+      <div className="comment-content">{comment.content}</div>
+      <div className="comment-writer">
+        <img
+          src={comment.writer.image}
+          alt={comment.writer.nickname}
+          className="comment-writer-avatar"
+        />
+        <div>
+          <div className="comment-writer-nickname">
+            {comment.writer.nickname}
+          </div>
+          <div className="comment-writer-createdAt">{dateString}</div>
+        </div>
+      </div>
+      <hr />
     </div>
   );
 }
