73afc58fe70002456ab2074ac42d53d748147286
Hyunji <hjcho2303@naver.com>
2024-04-26 08:40:43 +0900

    ✨ Feat: 코멘트에 기존 시간 구하는 함수 지우고 모먼트 라이브러리 사용

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,31 +1,14 @@
+import moment from 'moment';
+import 'moment/locale/ko';
+
 import Profile from './Profile';
 import styled from 'styled-components';
 import ic_kebab from '../assets/ic_kebab.png';
 
 export default function Comment({ content, createdAt, ...comment }) {
-  const detailDate = a => {
-    const milliSeconds = new Date() - a;
-    const seconds = milliSeconds / 1000;
-    if (seconds < 60) return `방금 전`;
-    const minutes = seconds / 60;
-    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-    const hours = minutes / 60;
-    if (hours < 24) return `${Math.floor(hours)}시간 전`;
-    const days = hours / 24;
-    if (days < 7) return `${Math.floor(days)}일 전`;
-    const weeks = days / 7;
-    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
-    const months = days / 30;
-    if (months < 12) return `${Math.floor(months)}개월 전`;
-    const years = days / 365;
-    return `${Math.floor(years)}년 전`;
-  };
-
-  const nowDate = detailDate(new Date(createdAt));
-
-  console.log(createdAt);
-  console.log(nowDate);
   const { nickname, image } = comment.writer;
+  let nowDate = moment(createdAt).startOf('hour').fromNow();
+
   return (
     <CommentContainer>
       <p>{content}</p>
