076407abe1de744dae9f57b1f9425630fd7f2f19
JHmeatschool <ceojr1234@gmail.com>
2024-04-26 23:11:59 +0900

    Add: 문의하기 함수 추가
    -src/components/ProductDetail.js에서 문의하기 등록 버튼 기능 추가
    -문의하기 글 작성 후 등록하면 밑에 추가되도록 함수 추가

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -10,6 +10,20 @@ const ProductDetail = () => {
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
 
+  const handleCommentSubmit = () => {
+    const newCommentData = {
+      content: newComment,
+      writer: {
+        image: "/images/home/commentpanda.png",
+        nickname: "User",
+      },
+      createdAt: new Date().toISOString(),
+    };
+
+    setComments([...comments, newCommentData]);
+    setNewComment("");
+  };
+
   const isFormValid = () => {
     return newComment.trim() !== "";
   };
@@ -80,7 +94,9 @@ const ProductDetail = () => {
           placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
         ></StyledTextarea>
       </ProductDetailCommentBox>
-      <CommentButton disabled={!isFormValid()}>등록</CommentButton>
+      <CommentButton onClick={handleCommentSubmit} disabled={!isFormValid()}>
+        등록
+      </CommentButton>
       <ProductDetailCommentBoxBottom>
         {comments.length === 0 ? (
           <>
