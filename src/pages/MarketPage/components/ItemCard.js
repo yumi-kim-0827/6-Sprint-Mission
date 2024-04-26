f70282a8424c1aa819f000773ef18915515a1f7b
oh-chaeyeon <chaebread555@gmail.com>
2024-04-26 23:39:50 +0900

    feat : 베스트 상품 컨포넌트 완료 백엔드 연결 실패

--- src/pages/MarketPage/components/ItemCard.js
+++ src/pages/MarketPage/components/ItemCard.js
@@ -3,12 +3,10 @@ import React from 'react';
 const ItemCard = ({ product }) => {
   return (
     <div className="item-card">
-      <img src={product.image} alt={product.name} className="product-image" />
+      <img src={product.images[0]} alt={product.name} className="product-image" />
       <div className="product-description">
         <h3>{product.name}</h3>
-      </div>
-      <div className="product-details">
-        <span className="price">{product.price}</span>
+        <h2 className="price">{product.price.toLocaleString()}원</h2>
         <div className="like">
           <img src="assets/ic_heart.svg" alt="heart" className="heart-icon" />
           <span className="like-count">{product.likes}</span>
