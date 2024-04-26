1f6b225376f91dafe42883aa254eceed90fd4180
doh-yo <doyo159090@gmail.com>
2024-04-26 22:39:09 +0900

    Feat: 등록 버튼 색상 변경

--- src/pages/ItemDetail/AddComments.jsx
+++ src/pages/ItemDetail/AddComments.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
 function AddComments() {
-  const [addComments, setAddCommnets] = useState();
+  const [addComments, setAddCommnets] = useState("");
+  const [isContentEntered, setIsContentEntered] = useState(false);
 
   const handleChange = (e) => {
     setAddCommnets(e.target.value);
+    setIsContentEntered(e.target.value.trim().length > 0); // 입력된 내용이 있는지 검사
   };
 
   const handleSubmit = (e) => {
@@ -21,13 +23,20 @@ function AddComments() {
         <textarea
           className="addComment-input"
           name="addComments"
+          value={addComments}
           onChange={handleChange}
           placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
         />
+        <div className="addComment-button">
+          <button
+            type="submit"
+            style={{ backgroundColor: isContentEntered ? "#3692FF" : "" }}
+            disabled={!isContentEntered}
+          >
+            등록
+          </button>
+        </div>
       </form>
-      <div className="addComment-button">
-        <button type="submit">등록</button>
-      </div>
     </div>
   );
 }
