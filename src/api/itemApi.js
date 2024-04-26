d05c7fb523eae73e679f46bb127e77e58c201e0e
YoungJun Bae <dudwns0213@gmail.com>
2024-04-26 23:58:04 +0900

    feat: api 작성, 제품 상세 페이지 적용(베스트 상품에만)

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,10 +1,26 @@
 export async function getProducts(params = {}) {
   // URLSearchParams을 이용하면 파라미터 값을 자동으로 쉽게 인코딩할 수 있어요.
   const query = new URLSearchParams(params).toString();
-
+  console.log(query);
   try {
     const response = await fetch(
       `https://panda-market-api.vercel.app/products?${query}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error("Failed to fetch products:", error);
+    throw error;
+  }
+}
+
+export async function getProductById(productId) {
+  try {
+    const response = await fetch(
+      `https://panda-market-api.vercel.app/products/${productId}`
     );
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
