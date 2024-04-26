ec1c0837292fd09af527f522c4bb5c04396a53c5
JaesungKo <jaesungko.dev@gmail.com>
2024-04-26 20:17:29 +0900

    refactor: 네비게이션 색상변경 코드 간소화

--- panda/src/components/Header.jsx
+++ panda/src/components/Header.jsx
@@ -3,30 +3,22 @@ import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.png";
 
 export default function Header() {
+  const getNavStyle = ({ isActive }) => {
+    return isActive
+      ? "text-lg text-primaryColor mr-4 font-bold"
+      : "text-lg text-coolGray600 mr-4 font-bold";
+  };
+
   return (
     <header className="flex justify-between items-center px-20 py-2 gap-10 border-b-2">
       <Link to="/">
         <img src={Logo} alt="logo" className="w-32 h-12" />
       </Link>
       <div className="grow">
-        <NavLink
-          to="/community"
-          className={({ isActive }) =>
-            isActive
-              ? "text-lg text-primaryColor mr-4 font-bold"
-              : "text-lg mr-4 text-coolGray600 font-bold"
-          }
-        >
+        <NavLink to="/community" className={getNavStyle}>
           자유게시판
         </NavLink>
-        <NavLink
-          to="/items"
-          className={({ isActive }) =>
-            isActive
-              ? "text-lg text-primaryColor font-bold"
-              : "text-lg text-coolGray600 font-bold"
-          }
-        >
+        <NavLink to="/items" className={getNavStyle}>
           중고마켓
         </NavLink>
       </div>
