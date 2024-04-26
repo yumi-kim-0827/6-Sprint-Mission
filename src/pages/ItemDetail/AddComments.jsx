88d0311a5518ea0b2b4e7da2809c7c0659681d0d
doh-yo <doyo159090@gmail.com>
2024-04-26 22:12:14 +0900

    Feat: 스타일 적용

--- src/pages/ItemDetail/AddComments.jsx
+++ src/pages/ItemDetail/AddComments.jsx
@@ -15,17 +15,19 @@ function AddComments() {
   };
 
   return (
-    <div>
-      <div>문의하기</div>
+    <div className="addComment">
+      <h2>문의하기</h2>
       <form onSubmit={handleSubmit}>
         <textarea
-          className="comment-input"
+          className="addComment-input"
           name="addComments"
           onChange={handleChange}
           placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
         />
       </form>
-      <button type="submit">등록</button>
+      <div className="addComment-button">
+        <button type="submit">등록</button>
+      </div>
     </div>
   );
 }
--- src/pages/ItemDetail/ItemDescription.jsx
+++ src/pages/ItemDetail/ItemDescription.jsx
@@ -2,50 +2,61 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getProductById } from "../../itemApi";
 import { ReactComponent as HeartIcon } from "../../assets/heart.svg";
+import "./ItemDetail.css";
 
 const ItemDescription = () => {
   const { productId } = useParams();
-  const [product, setProduct] = useState({});
-
-  const fetchProduct = async () => {
-    const productData = await getProductById(productId);
-    setProduct(productData);
-  };
+  const [product, setProduct] = useState(null);
 
   useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const productData = await getProductById(productId);
+        setProduct(productData);
+      } catch (error) {
+        console.error("Failed to fetch product:", error);
+      }
+    };
+
     fetchProduct();
   }, [productId]);
 
   return (
     <div>
       {product ? (
-        <div className="Product">
-          <div className="Product-wrapper">
-            <div className="Product-image">
-              <img src={product.images} alt="Product" />
+        <div className="product">
+          <div className="product-wrapper">
+            <div className="product-image">
+              <img src={product.images} alt="ProductImg" />
             </div>
-            <div className="Product-detail">
-              <h2>{product.name}</h2>
-              <p>{Number(product.price).toLocaleString()}원</p>
-            </div>
-            <div className="Product-description">
-              <h3>상품 소개</h3>
-              <p>{product.description}</p>
-            </div>
-            <div className="Product-tag">
-              <h3>상품 태그</h3>
-              <p>
-                {product.tags?.map((tag) => (
-                  <p key={`${product.id}`}>#{tag}</p>
-                ))}
-              </p>
-            </div>
-            <div className="Product-favoriteCount">
-              <p>
-                <HeartIcon /> {product.favoriteCount}
-              </p>
+            <div className="product-detail-wrapper">
+              <div className="product-detail">
+                <h1>{product.name}</h1>
+                <p>{Number(product.price).toLocaleString()}원</p>
+              </div>
+              <hr />
+              <div className="product-description">
+                <h3>상품 소개</h3>
+                <p>{product.description}</p>
+              </div>
+              <div className="product-tag">
+                <h3>상품 태그</h3>
+                <div className="product-tag-wrapper">
+                  {product.tags?.map((tag) => (
+                    <p className="product-tag-detail" key={`${product.id}`}>
+                      #{tag}
+                    </p>
+                  ))}
+                </div>
+              </div>
+              <div className="product-favoriteCount">
+                <p>
+                  <HeartIcon /> {product.favoriteCount}
+                </p>
+              </div>
             </div>
           </div>
+          <hr />
         </div>
       ) : (
         <p>Loading...</p>
