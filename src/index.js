caefdcad15f7b92bb416a0791bba38a2f985a329
CHAE <cowhdals1111@gmail.com>
2024-04-26 15:57:04 +0900

    vercel 배포

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-//vercel 배포
+//vercel sprint7 배포
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
