9a5d3dbf098d4e12acb8055d4c36c279bc052cab
doh-yo <doyo159090@gmail.com>
2024-04-26 22:30:59 +0900

    Feat: 댓글 데이터 가져오기 및 레이아웃 구성

--- src/pages/ItemDetail/ItemDetail.jsx
+++ src/pages/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ItemDescription from "./ItemDescription";
 import AddComments from "./AddComments";
+import CommentsSection from "./CommentsSection";
 
 const ItemDetail = () => {
   return (
     <div>
       <ItemDescription />
       <AddComments />
+      <CommentsSection />
     </div>
   );
 };
