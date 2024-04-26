1c378c3aee080a857fbfb81c38f8f353d46318cb
greenyun2 <cys4875@naver.com>
2024-04-26 22:32:24 +0900

    스프린트 미션6

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,8 +1,93 @@
+import { useState } from "react";
+import InputTitle from "../components/InputTitle";
+import "./AddItem.css";
+import FileInput from "../components/FileInput";
+
+const INITIAL_VALUES = {
+  imgFile: null,
+  name: "",
+  desc: "",
+  price: "",
+  tag: "",
+};
+
 function AddItem() {
+  const [values, setValues] = useState(INITIAL_VALUES);
+
+  const handleChange = (name, value) => {
+    setValues((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+    console.log(name, value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValues(INITIAL_VALUES);
+    console.log(values);
+  };
+
   return (
-    <div className='AddItem'>
-      <h1>상품등록 페이지</h1>
-    </div>
+    <form onSubmit={handleSubmit} className='AddItem-form'>
+      <div className='AddItem-title'>
+        <h1>상품 등록하기</h1>
+        <button type='submit'>등록</button>
+      </div>
+      <div className='AddItem-file'>
+        <InputTitle name='상품 이미지' />
+        <FileInput
+          name='imgFile'
+          value={values.imgFile}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='AddItem-name'>
+        <InputTitle name='상품명' />
+        <input
+          value={values.name}
+          name='name'
+          type='text'
+          placeholder='상품명을 입력해주세요'
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className='AddItem-desc'>
+        <InputTitle name='상품 소개' />
+        <textarea
+          value={values.desc}
+          name='desc'
+          type='text'
+          placeholder='상품 소개를 입력해주세요'
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className='AddItem-price'>
+        <InputTitle name='판매가격' />
+        <input
+          value={values.price}
+          name='price'
+          type='text'
+          placeholder='판매 가격을 입력해주세요'
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className='AddItem-tag'>
+        <InputTitle name='태그' />
+        <input
+          value={values.tag}
+          name='tag'
+          type='text'
+          placeholder='태그를 입력해주세요'
+          onChange={handleInputChange}
+        />
+      </div>
+    </form>
   );
 }
 
