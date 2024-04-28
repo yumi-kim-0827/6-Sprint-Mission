6db84de6b132dd96ab54326ecdfe1f6095f616e8
codefug <86544979+codefug@users.noreply.github.com>
2024-04-28 15:45:53 +0900

    fix: some file format jsx into js

---
+++ src/shared/util/util.js
@@ -0,0 +1,13 @@
+export const formatPrice = (price) => {
+  const newPrice = new Intl.NumberFormat().format(price);
+  return `${newPrice}원`;
+};
+
+export const getTimeDiffer = (date) => {
+  const getTimeDiffer = Math.floor(
+    (Date.now() - new Date(date)) / 1_000 / (60 * 60)
+  );
+  return Math.floor(getTimeDiffer / 24) !== 0
+    ? Math.floor(getTimeDiffer / 24) + "일 전"
+    : Math.floor(getTimeDiffer) + "시간 전";
+};
