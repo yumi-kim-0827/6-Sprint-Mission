f0dce005b47c0f44eb130d07a9be7996dd1b2248
WooGi1020 <jungwk1020@gmail.com>
2024-05-21 13:43:55 +0900

    ♻️ refactor : SearchInput 컴포넌트 분리

--- src/components/ShowProducts.jsx
+++ src/components/ShowProducts.jsx
@@ -23,7 +23,7 @@ function Product({ name, price, favoriteCount, images }) {
   );
 }
 
-const ShowProducts = ({ onChangeSelect, onChangeInput, products }) => {
+const SearchInput = ({ onChangeSelect, onChangeInput }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [dropdownMenu, setDropdownMenu] = useState("최신순");
   const recentRef = useRef();
@@ -51,36 +51,42 @@ const ShowProducts = ({ onChangeSelect, onChangeInput, products }) => {
   };
 
   return (
-    <>
-      <div className="Search-Group">
-        <h2>전체 상품</h2>
-        <div>
-          <label htmlFor="search"></label>
-          <img id="search-icon" src={searchicon} alt="검색 아이콘" />
-          <input onChange={onChangeInput} id="search" type="text" placeholder="검색할 상품을 입력해주세요" />
-        </div>
-        <Link className="link" to="/additems">
-          <button className="Product-Resister-Btn">상품 등록하기</button>
-        </Link>
-        <div className="select-category">
-          <div onClick={handleDropdown} className="dropdown-menu">
-            <p>{dropdownMenu}</p>
-            <img src={dropdownMobile} className="mobileIcon" alt="제품 페이지 드롭다운 메뉴 모바일 아이콘" />
-            <img src={dropdownArrow} className="dropdownIcon" alt="제품 페이지 드롭다운 메뉴 아이콘" />
-          </div>
-          {isDropdownOpen && (
-            <div className="dropdown-option">
-              <span ref={recentRef} onClick={handleChangeRecent}>
-                최신순
-              </span>
-              <div className="line"></div>
-              <span ref={favoriteRef} onClick={handleChangeFavorite}>
-                좋아요순
-              </span>
-            </div>
-          )}
-        </div>
+    <div className="Search-Group">
+      <h2>전체 상품</h2>
+      <div>
+        <label htmlFor="search"></label>
+        <img id="search-icon" src={searchicon} alt="검색 아이콘" />
+        <input onChange={onChangeInput} id="search" type="text" placeholder="검색할 상품을 입력해주세요" />
       </div>
+      <Link className="Product-Resister-Btn" to="/additems">
+        상품 등록하기
+      </Link>
+      <div className="select-category">
+        <div onClick={handleDropdown} className="dropdown-menu">
+          <p>{dropdownMenu}</p>
+          <img src={dropdownMobile} className="mobileIcon" alt="제품 페이지 드롭다운 메뉴 모바일 아이콘" />
+          <img src={dropdownArrow} className="dropdownIcon" alt="제품 페이지 드롭다운 메뉴 아이콘" />
+        </div>
+        {isDropdownOpen && (
+          <div className="dropdown-option">
+            <span ref={recentRef} onClick={handleChangeRecent}>
+              최신순
+            </span>
+            <div className="line"></div>
+            <span ref={favoriteRef} onClick={handleChangeFavorite}>
+              좋아요순
+            </span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const ShowProducts = ({ onChangeSelect, onChangeInput, products }) => {
+  return (
+    <>
+      <SearchInput onChangeSelect={onChangeSelect} onChangeInput={onChangeInput} />
       <ul className="ProductsList">
         {products.map((item) => {
           return (
