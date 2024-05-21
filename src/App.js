4fe10c46afd6ab6a72858a56756c3252115fe9ae
JHmeatschool <ceojr1234@gmail.com>
2024-05-22 00:11:14 +0900

    add: 홈페이지 추가
    -src/components/HomePage.js 파일을 추가함

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import HomePage from "./components/HomePage";
 import Product from "./components/Products";
 import AddItem from "./components/AddItems";
 import ProductDetail from "./components/ProductDetail";
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/items" element={<Product />} />
           <Route path="/additem" element={<AddItem />} />
           <Route path="/items/:productId" element={<ProductDetail />} />
