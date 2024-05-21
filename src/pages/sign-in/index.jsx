24d54bb0231a0115a66bf5a78f3af36d426659c0
WooGi1020 <jungwk1020@gmail.com>
2024-05-22 01:39:26 +0900

    ✨ feat : signup 페이지 생성 및 html 태그 부분 작성

---
+++ src/pages/sign-in/index.jsx
@@ -0,0 +1,122 @@
+import "./SignIn.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import logoPng from "../../assets/long_logo.png";
+import showIcon from "../../assets/sign-up/show.svg";
+import nonShowIcon from "../../assets/sign-up/non-show.svg";
+import kakaoLogo from "../../assets/sign-up/kakao-icon.svg";
+import googleLogo from "../../assets/sign-up/google-icon.png";
+
+const SignIn = () => {
+  const [isSignInBtnDisabled, setIsSignInBtnDisabled] = useState(true);
+  const [isPwShow, setIsPwShow] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isValidPw, setIsValidPw] = useState(true);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validOption = {
+    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    pw: /.{8,}/,
+  };
+
+  useEffect(() => {
+    if (isValidEmail && isValidPw && email && password) {
+      setIsSignInBtnDisabled(false);
+    } else {
+      setIsSignInBtnDisabled(true);
+    }
+  }, [isValidEmail, isPwShow, email, password]);
+
+  const checkEmailValidity = (e) => {
+    const { value } = e.target;
+    setEmail(value);
+    const isValid = validOption.email.test(value);
+    setIsValidEmail(isValid ? true : false);
+  };
+
+  const checkPwValidity = (e) => {
+    const { value } = e.target;
+    setPassword(value);
+    const isValid = validOption.pw.test(value);
+    setIsValidPw(isValid ? true : false);
+  };
+
+  const handlePwShow = () => {
+    setIsPwShow(!isPwShow);
+  };
+
+  return (
+    <div className="sign-in-container">
+      <div className="sign-in-container__logo">
+        <Link to="/">
+          <img className="logo" src={logoPng} alt="판다마켓 로고" />
+        </Link>
+      </div>
+
+      <form action="#" method="post">
+        <div className="email con">
+          <label htmlFor="email">이메일</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            placeholder="이메일을 입력해주세요"
+            autoFocus
+            onChange={checkEmailValidity}
+            className={isValidEmail ? "" : "wrong"}
+          />
+          {isValidEmail || (
+            <span className="wrong-email">{email === "" ? "이메일을 입력해주세요" : "잘못된 이메일 형식입니다"}</span>
+          )}
+        </div>
+
+        <div className="pw con">
+          <label htmlFor="pw">비밀번호</label>
+          <input
+            type={isPwShow ? "text" : "password"}
+            id="pw"
+            placeholder="비밀번호를 입력해주세요"
+            onChange={checkPwValidity}
+            className={isValidPw ? "" : "wrong"}
+          />
+          {isPwShow ? (
+            <img className="pw-icon" src={showIcon} onClick={handlePwShow} alt="비밀번호 보이기" />
+          ) : (
+            <img className="pw-icon" src={nonShowIcon} onClick={handlePwShow} alt="비밀번호 보이기" />
+          )}
+          {isValidPw || (
+            <span className="wrong-pw">
+              {password === "" ? "비밀번호를 입력해주세요" : "비밀번호를 8자 이상 입력해주세요"}
+            </span>
+          )}
+        </div>
+
+        <button disabled={isSignInBtnDisabled} className="login-btn">
+          로그인
+        </button>
+
+        <div className="social-login">
+          <p>간편 로그인하기</p>
+          <div className="social-login-logo">
+            <Link to="https://accounts.google.com/signin/v2/usernamerecovery?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&hl=ko&theme=glif&ddm=0&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
+              <img src={googleLogo} alt="구글 로고" />
+            </Link>
+            <Link to="https://accounts.kakao.com/login/?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fproxy%3DeasyXDM_Kakao_9iyy5od3l29_provider%26ka%3Dsdk%252F1.43.2%2520os%252Fjavascript%2520sdk_type%252Fjavascript%2520lang%252Fko-KR%2520device%252FWin32%2520origin%252Fhttps%25253A%25252F%25252Fwww.epis.or.kr%26origin%3Dhttps%253A%252F%252Fwww.epis.or.kr%26response_type%3Dcode%26redirect_uri%3Dkakaojs%26state%3Dlgo6gi4qr2byw694fiizh%26through_account%3Dtrue%26client_id%3D950f3ae1f2f550e5ba7ff7d9fffc7781&talk_login=hidden#login">
+              <img src={kakaoLogo} alt="카카오 로고" />
+            </Link>
+          </div>
+        </div>
+
+        <div className="go-signup-container">
+          <p>판다마켓이 처음이신가요?</p>
+          <Link to="/sign-up">회원가입</Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
