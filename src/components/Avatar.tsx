f18bead9e6abac4d21d0a02b6c5af87fb42c80d5
cindyCho <hjcho2303@naver.com>
2024-05-21 14:18:59 +0900

    jsx to tsx

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Avatar({ img }) {
+interface Props {
+  img: string;
+}
+
+export default function Avatar({ img }: Props) {
   return (
     <AvatarImg>
       <img src={img} alt='프로필 이미지' />
