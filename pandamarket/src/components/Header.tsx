43e5ed1cb81a214869f0bd72bb19587273eb59dc
김보미 <kimbomi2172@naver.com>
2024-05-22 16:31:26 +0900

    🐛 fix: 헤더 srcSet 수정

--- pandamarket/src/components/Header.tsx
+++ pandamarket/src/components/Header.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import "../styles/style.css";
-import styles from "../styles/main.module.css";
-import { useNavigate, useLocation } from "react-router-dom";
+import React from 'react'
+import '../styles/style.css'
+import styles from '../styles/main.module.css'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 import logo_text from '../assets/logo_text.png'
 import logo from '../assets/logo.png'
@@ -9,40 +9,43 @@ import logo from '../assets/logo.png'
 function Header() {
   const location = useLocation()
 
-  const isItemsPage = location.pathname === "/items" || location.pathname === "/additem"
+  const isItemsPage =
+    location.pathname === '/items' || location.pathname === '/additem'
 
   const navigate = useNavigate()
 
   const goToItems = () => {
-    navigate("/items")
+    navigate('/items')
   }
 
   const goToMain = () => {
-    navigate("/")
+    navigate('/')
   }
 
   const goToSignin = () => {
-    navigate("/signin")
+    navigate('/signin')
   }
 
   return (
     <header>
       <picture onClick={goToMain}>
-        <source
-          srcset={logo_text}
-          media="all and (max-width: 768px)"
-        />
+        <source srcSet={logo_text} media="all and (max-width: 768px)" />
         <img src={logo} className={styles.logo} alt="로고" />
       </picture>
       <nav className={styles['header-nav']}>
         <p>자유게시판</p>
-        <p onClick={goToItems} style={{color: isItemsPage ? '#3692FF': 'inherit'}}>중고마켓</p>
+        <p
+          onClick={goToItems}
+          style={{ color: isItemsPage ? '#3692FF' : 'inherit' }}
+        >
+          중고마켓
+        </p>
       </nav>
       <button id="btn_small" onClick={goToSignin}>
         로그인
       </button>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
