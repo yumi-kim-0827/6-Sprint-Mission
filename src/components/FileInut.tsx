ff4d69c4f73bed3f9a261d799c989d24adee2a2c
JustDevRae <ksrae165@gmail.com>
2024-05-23 21:21:41 +0900

    feat: AddItemPage 미리보기 이미지 업로드 기능 구현

--- src/components/FileInut.tsx
+++ src/components/FileInut.tsx
@@ -1,13 +1,66 @@
-export default function FileInput() {
+import { useEffect, useRef, useState } from "react";
+
+import Close from "../assets/icons/ic_close.png";
+
+interface Props {
+  // 매개변수로 받은 props 정의
+  name: string;
+  value: any;
+  onChange: (name: string, value: any) => void;
+}
+
+export default function FileInput(props: Props) {
+  const [previewImg, setPreviewImg] = useState<string>(""); // 파일 미리보기 주소를 문자열로 저장
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // HTMLInputElement의 files 속성은 FileList | null 타입
+    // Optional chaining을 사용하여 files가 null이 아닌 경우에만 접근합니다.
+    const nextValue = e.target.files?.[0];
+
+    props.onChange(props.name, nextValue);
+  };
+
+  const handleDelete = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    props.onChange(props.name, null);
+  };
+
+  useEffect(() => {
+    if (!props.value) return;
+
+    const nextPreviewImg = URL.createObjectURL(props.value);
+    setPreviewImg(nextPreviewImg);
+
+    return () => {
+      setPreviewImg(""); // preview State값 초기화
+      URL.revokeObjectURL(nextPreviewImg); // 앞에서 만든 ObjectURL을 해제
+    };
+  }, [props.value]); // 파일을 선택할 때마다 미리보기 주소 변경
+
   return (
-    <div>
+    <div className="img_add_wrapper">
       <input
         type="file"
         accept="images/png, images/jpeg"
         placeholder="이미지 등록"
+        onChange={handleChange}
+        ref={inputRef}
         className="img_add_input"
       />
-      <div></div>
+      <div className="preview_img_wrapper">
+        {props.value && (
+          <img src={previewImg} alt="미리보기 이미지" className="preview_img" />
+        )}
+        {props.value && (
+          <button onClick={handleDelete} className="img_delete_button">
+            <img src={Close} alt="닫기" />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/AddItemPage.tsx
+++ src/pages/AddItemPage.tsx
@@ -1,13 +1,55 @@
+import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-
-import Logo from "../assets/logo.png";
-
 import FileInput from "../components/FileInut";
-
+import Logo from "../assets/logo.png";
 import "../styles/AddItemPage.css";
+
+// input 태그로 입력받는 태그들의 값들을 interface로 정의해서 정리
+interface InputValue {
+  title: string;
+  description: string;
+  price: string;
+  tag: string;
+  imgFile: any; // 객체로 전달해야 되는데 일단 걍 any로 타입 지정
+}
 
 export default function AddItemPage() {
   const navigate = useNavigate();
+  const [disabled, setDisabled] = useState(true);
+  const [values, setValues] = useState<InputValue>({
+    title: "",
+    description: "",
+    price: "",
+    tag: "",
+    imgFile: null,
+  });
+
+  const handleChange = (name: string, value: any): void => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  // input 입력 이벤트를 매개변수로 받는다.
+  // e의 타입은 input과 textarea에서 발생하므로 유니언 타입으로 해야 한다. 
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    // 입력한 e를 name과 value로 구조분해 할당
+    const { name, value } = e.target;
+    handleChange(name, value);
+  };
+
+  useEffect(() => {
+    const { title, description, price, tag, imgFile } = values;
+    if (title !== "" && description !== "" && price !== "" && tag !== "" && imgFile !== null) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [values]);
+
   return (
     <div>
       <header>
@@ -31,28 +73,53 @@ export default function AddItemPage() {
       <form action="" className="add_form_wrapper">
         <div className="add_form_wrapper_header">
           <h1>상품 등록하기</h1>
-          <button>등록</button>
+          <button disabled={disabled}>등록</button>
         </div>
 
         <label htmlFor="">상품 이미지</label>
-        <FileInput/>
+        <FileInput
+          name="imgFile"
+          value={values.imgFile}
+          onChange={handleChange}
+        />
 
         <label htmlFor="">상품명</label>
-        <input type="text" placeholder="상품명을 입력해주세요" />
+        <input
+          type="text"
+          name="title"
+          value={values.title}
+          onChange={handleInputChange}
+          placeholder="상품명을 입력해주세요"
+        />
 
         <label htmlFor="">상품소개</label>
-        <textarea placeholder="상품 소개를 입력해주세요" />
+        <textarea
+          name="description"
+          value={values.description}
+          onChange={handleInputChange}
+          placeholder="상품 소개를 입력해주세요"
+        />
 
         <label htmlFor="">판매가격</label>
-        <input type="text" placeholder="판매 가격을 입력해주세요" />
+        <input
+          type="text"
+          name="price"
+          value={values.price}
+          onChange={handleInputChange}
+          placeholder="판매 가격을 입력해주세요"
+        />
 
         <label htmlFor="">태그</label>
-        <input type="text" placeholder="태그를 입력해주세요" />
+        <input
+          type="text"
+          name="tag"
+          value={values.tag}
+          onChange={handleInputChange}
+          placeholder="태그를 입력해주세요"
+        />
 
         <div className="tag_wrapper">
-          <ul>
-
-          </ul>
+          <ul></ul>
         </div>
       </form>
     </div>
