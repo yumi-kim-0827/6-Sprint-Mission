8dd683eec2b2801e03990fc51c74ac79b81cd00f
seo02wow <rlatjdud9188@naver.com>
2024-05-24 02:12:59 +0900

    :recycle:refactor: 리스트 페이지, 관련 컴포넌트  TS 마이그레이션

--- src/components/ItemListPage/BestItemList.tsx
+++ src/components/ItemListPage/BestItemList.tsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
 import "../styles/BestItemList.css";
 import { AllItem } from "./AllItemList";
+import Item from "constants/Item";
 
-function BestItemList({ items }) {
+interface Props {
+  items: Item[];
+}
+
+function BestItemList({ items }: Props) {
   return (
     <section className="best-item-list">
       <h2 className="category-title">베스트 상품</h2>
---
+++ src/constants/Item.ts
@@ -0,0 +1,9 @@
+export default interface Item {
+  name: string;
+  price: number;
+  description: string;
+  tags: string[];
+  images: string;
+  favoriteCount: number;
+  id: number;
+}
