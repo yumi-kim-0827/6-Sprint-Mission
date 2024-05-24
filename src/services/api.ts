1fc3e0dc66f0112abecb585032c2e2a3f56b4a83
Jiyun Kim <wayandway@kakao.com>
2024-05-24 12:08:48 +0900

    Refactor: get API 레이어 & 파라미터 타입 추가

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,50 +1,32 @@
 const BASE_URL = "https://panda-market-api.vercel.app";
 
+// GET API Layer
+async function GET(URL: string) {
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error("Failed to get data : ", error);
+    throw error;
+  }
+}
+
 // 상품 리스트 조회
 export async function getProducts(params = {}) {
   const query = new URLSearchParams(params).toString();
-
-  try {
-    const response = await fetch(`${BASE_URL}/products?${query}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-    const body = await response.json();
-    return body;
-  } catch (error) {
-    console.error("상품 데이터를 불러오는데 실패했습니다: ", error);
-    throw error;
-  }
+  return await GET(`${BASE_URL}/products?${query}`);
 }
 
 // 상품 상세 정보 조회
-export async function getProductDetail(productId) {
-  try {
-    const response = await fetch(`${BASE_URL}/products/${productId}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-    const body = await response.json();
-    return body;
-  } catch (error) {
-    console.error("상품의 상세 정보를 불러오는데 실패했습니다: ", error);
-    throw error;
-  }
+export async function getProductDetail(productId: string | undefined) {
+  return await GET(`${BASE_URL}/products/${productId}`);
 }
 
 // 상품별 댓글 조회
-export async function getComments(productId) {
-  try {
-    const response = await fetch(
-      `${BASE_URL}/products/${productId}/comments?limit=3`
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-    const body = await response.json();
-    return body;
-  } catch (error) {
-    console.error("상품의 댓글 정보를 불러오는데 실패했습니다: ", error);
-    throw error;
-  }
+export async function getComments(productId: string | undefined) {
+  return await GET(`${BASE_URL}/products/${productId}/comments?limit=3`);
 }
