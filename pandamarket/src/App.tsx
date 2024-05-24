6af04b86c42e6b0139608dd080597fb5f15d2e4d
김보미 <kimbomi2172@naver.com>
2024-05-24 13:31:38 +0900

    ♻️ refactor: outlet 설정

--- pandamarket/src/App.tsx
+++ pandamarket/src/App.tsx
@@ -1,19 +1,30 @@
-import { Header, Footer } from './components'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { AddItem, Items, ItemsDetail, Main } from './pages'
+import {
+  AddItem,
+  Items,
+  ItemsDetail,
+  Main,
+  Signin,
+  Signup,
+} from './pages/index.ts'
+import Layout from './pages/Layout.tsx'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
+      <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/items" element={<Items />} />
-          <Route path="/items/:id" element={<ItemsDetail />} />
-          <Route path="/additem" element={<AddItem />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Main />} />
+            <Route path="items">
+              <Route index element={<Items />} />
+              <Route path=":id" element={<ItemsDetail />} />
+            </Route>
+            <Route path="additem" element={<AddItem />} />
+          </Route>
+          <Route path="signin" element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </>
   )
