97cb6c37e889d5a3f2cdb648e70332d61555e14b
Darkthos <kiki7951@naver.com>
2024-05-25 16:37:13 +0900

    Delete all file and make initial setting

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as SignLogo } from "./SignLogo";
-export { default as HeadNav } from "./HeadNav";
-export { default as HeadLogo } from "./HeadLogo";
-export { default as Button } from "./Button";
-export { default as Dropdown } from "./Dropdown";
-export { default as Favorite } from "./Favorite";
--- src/utils/themes.js
+++
@@ -1,2 +0,0 @@
-export const PC_SIZE = "@media screen and (min-width: 1201px)";
-export const TABLET_SIZE = "@media screen and (min-width: 744px) and (max-width: 1200px)";
