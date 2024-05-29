16d79553919578f094e707499dc6ae166c398df5
AdamSeungheonShin <ssh9809@naver.com>
2024-05-29 16:55:04 +0900

    프로젝트 세팅 및 기본 ui(헤더, 메인) 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from "@/components/Header/Header";
+import "./globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className="w-[1200px] mx-auto mt-20 border-[1px] border-dashed border-pink-100">
+      <Component {...pageProps} />
+    </main>
+  );
 }
