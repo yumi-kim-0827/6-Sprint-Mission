3476eaaef530a6f524a9feb135190189ef174737
easyhyun00 <leejihyun0324@gmail.com>
2024-05-29 15:38:37 +0900

    🎉 Init: next.js 스프린트 미션 초기 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/normalize.css';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>판다마켓</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="./favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ pages/boards/index.tsx
@@ -0,0 +1,10 @@
+const Boards = () => {
+  return (
+    <div>
+      안녕
+      <p>안녕</p>
+    </div>
+  );
+};
+
+export default Boards;
