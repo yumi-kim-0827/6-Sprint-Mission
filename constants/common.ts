f485ad153166f7d57b4b52c7830e91b02d384537
eprnf <eprnf21@gmail.com>
2024-05-29 16:16:50 +0900

    add: constants 추가

---
+++ constants/common.ts
@@ -0,0 +1,1 @@
+export const APP_BASE_URL = "https://panda-market-api.vercel.app/";
---
+++ constants/deviceSize.ts
@@ -0,0 +1,3 @@
+export const SMALL_DEVICE_WIDTH = 375;
+export const MEDIUM_DEVICE_WIDTH = 768;
+export const LARGE_DEVICE_WIDTH = 1200;
