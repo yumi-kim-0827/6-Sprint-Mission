708ee27b6c052aaaf29b198e303c24ee0a47b06c
eprnf <eprnf21@gmail.com>
2024-05-31 01:44:14 +0900

    design: 그림자 위치 조절

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 export default function NotFoundPage() {
   return (
     <div className="relative mx-auto mt-20 max-w-screen-xl h-[calc(100vh-230px)]">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-4xl text-center tracking-widest">
-        <div className="relative mb-6 text-[102px] whitespace-nowrap">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-[32px] text-center tracking-widest">
+        <div className="relative mb-6 text-[102px] leading-[102px] whitespace-nowrap">
           {/* shadow */}
           <div className="absolute bottom-0 left-1/2 w-[82px] h-[4px] bg-black bg-opacity-20 rounded-[50%] shadow-[0_0_5px_5px_rgba(0,0,0,0.2)] transform -translate-x-1/2"></div>
           {/* shadow */}4
