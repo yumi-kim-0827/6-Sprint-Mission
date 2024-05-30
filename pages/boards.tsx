c5c8644dd4ba28dba04f209013e013d4c36da094
김제완 <gimjewan@CarpeDiem-MacBookAir.local>
2024-05-31 07:46:59 +0900

    efactor: 모든 게시글 프리렌더링으로 변경

--- pages/boards.tsx
+++ pages/boards.tsx
@@ -1,11 +1,12 @@
+import { GetServerSideProps } from "next";
 import BestPost from "@/components/bestPost";
 import Posts from "@/components/posts";
 import SearchInput from "@/components/search";
 import styles from "@/styles/Board.module.css";
 import LinkButton from "@/utils/Button";
+import Dropdown from "@/components/dropdown";
 import { useEffect, useState } from "react";
 import axios from "@/utils/axios";
-import Dropdown from "@/components/dropdown";
 
 interface List {
   id: number;
@@ -23,16 +24,35 @@ interface Writer {
   nickname: string;
 }
 
-export default function Board() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [posts, setPosts] = useState<List[]>([]);
-  const [order, setOrder] = useState<"recent" | "like">("recent");
+interface Props {
+  PostsData: List[];
+}
 
-  async function getPosts() {
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
     const res = await axios.get("/articles");
-    const posts = res.data.list ?? [];
-    setPosts(posts);
+    const PostsData: List[] = res.data.list ?? [];
+
+    return {
+      props: {
+        PostsData,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        PostsData: [],
+      },
+    };
   }
+};
+
+export default function Board({ PostsData }: Props) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [posts, setPosts] = useState<List[]>(PostsData);
+  const [order, setOrder] = useState<"recent" | "like">("recent");
 
   const handleSortOrderChange = (selectedOrder: "recent" | "like"): void => {
     setOrder(selectedOrder);
@@ -57,47 +77,39 @@ export default function Board() {
     setSearchTerm(event.target.value);
   };
 
-  useEffect(() => {
-    try {
-      getPosts();
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
-  useEffect(() => {
-    const sortedPosts = sortData(posts, order);
-    setPosts(sortedPosts);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [order]);
-
   const filteredData = posts.filter((item) =>
     item.title.toLowerCase().trim().includes(searchTerm.toLowerCase().trim())
   );
 
+  useEffect(() => {
+    const sortedPosts = sortData(PostsData, order);
+    setPosts(sortedPosts);
+  }, [order, PostsData]);
+
   return (
-      <div className={styles.BoardContainer}>
-        <div className={styles.bestContainer}>
-          <span className={styles.bestTopText}>베스트 게시글</span>
-          <div className={styles.bestPosts}>
-            <BestPost />
-          </div>
-        </div>
-        <div className={styles.posts}>
-          <div className={styles.postsTop}>
-            <span className={styles.bestTopText}>게시글</span>
-            <LinkButton href="/">글쓰기</LinkButton>
-          </div>
-          <div className={styles.postsMiddle}>
-            <SearchInput value={searchTerm} onChange={handleSearchChange} />
-            <Dropdown onChange={handleSortOrderChange} />
-          </div>
-          <div className={styles.postsContainer}>
-            {filteredData.map((post) => (
-              <Posts key={post.id} posts={post} />
-            ))}
-          </div>
+    <div className={styles.BoardContainer}>
+      <div className={styles.bestContainer}>
+        <span className={styles.bestTopText}>베스트 게시글</span>
+        <div className={styles.bestPosts}>
+          <BestPost />
         </div>
       </div>
+      <div className={styles.posts}>
+        <div className={styles.postsTop}>
+          <span className={styles.bestTopText}>게시글</span>
+          <LinkButton href="/">글쓰기</LinkButton>
+        </div>
+        <div className={styles.postsMiddle}>
+          <SearchInput value={searchTerm} onChange={handleSearchChange} />
+          <Dropdown onChange={handleSortOrderChange} />
+        </div>
+        <div className={styles.postsContainer}>
+          {filteredData.map((post) => (
+            <Posts key={post.id} posts={post} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
+
