6f4cd5cb11621c4a59235136aac208c41545e162
김보미 <kimbomi2172@naver.com>
2024-05-30 18:29:18 +0900

    🚚 chore: 헤더 전역적 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import Header from '../components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +12,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
     </>
   )
 }
