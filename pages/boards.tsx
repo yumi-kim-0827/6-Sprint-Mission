5fa187d443c156a3994dfe0705aeb4bf9e9c3c7c
김보미 <kimbomi2172@naver.com>
2024-05-30 19:09:03 +0900

    💄 ui: boards페이지 UI 구현

---
+++ pages/boards.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../styles/boards.module.css'
+import BestPosts from '@/components/BestPosts'
+
+export default function boards() {
+  return (
+    <div className={styles.container}>
+      <h3>베스트 게시글</h3>
+      <BestPosts />
+      <div className={styles.postsNav}>
+        <h3>게시글</h3>
+        <button>글쓰기</button>
+      </div>
+    </div>
+  )
+}
