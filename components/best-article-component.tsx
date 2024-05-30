8d51c0f973a3dfd781d1168a7144ccffa1421e0f
cindyCho <hjcho2303@naver.com>
2024-05-30 09:11:23 +0900

    Feat: bestArticles 데이터 연결

--- components/best-article-component.tsx
+++ components/best-article-component.tsx
@@ -1,27 +1,49 @@
 import Image from 'next/image';
 import image_badge from '@/public/images/img_badge.svg';
 import ic_heart from '@/public/images/ic_heart.png';
-import mockimg from '@/public/images/mockimg.png';
+interface ListProps {
+  updatedAt: string;
+  createdAt: string;
+  likeCount: number;
+  writer: Writer;
+  image: string;
+  content: string;
+  title: string;
+  id: number;
+}
+interface Writer {
+  nickname: string;
+  id: number;
+}
 
-export default function BestArticleComponent() {
+export default function BestArticleComponent({ createdAt, likeCount, image, title, writer }: ListProps) {
+  const date = new Date(createdAt).getDate();
+  const month = new Date(createdAt).getMonth() + 1;
+  const year = new Date(createdAt).getFullYear();
+  const createdDate = `${year}. ${month}. ${date}`;
+
   return (
     <div className='h-[169px] px-6 pb-6 bg-cool-gray50 rounded-lg sm:min-w-[343px] md:min-w-[340px] lg:min-w-[384px]'>
       <Image src={image_badge} alt='베스트 뱃지' />
       <div className='flex gap-2 mt-4 mb-4 h-[72px]'>
-        <div className=' text-cool-gray800 text-[18px] leading-5 sm:min-w[212px] lg:w-[256px] lg:text-xl'>
-          맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?
+        <div className=' text-cool-gray800 text-[18px] leading-5 sm:min-w[212px] w-[212px] lg:w-[256px] lg:text-xl'>
+          {title}
         </div>
-        <Image src={mockimg} alt='메롱'></Image>
+        {image && (
+          <div className='flex justify-center items-center bg-white border border-solid rounded-md border-cool-gray200 w-[72px] h-[72px]'>
+            <Image src={image} alt='메롱' width={48} height={45} />
+          </div>
+        )}
       </div>
-      <div className='flex justify-between text-cool-gray400'>
+      <div className='flex justify-between'>
         <div className='flex items-center gap-2'>
-          <span>푸바오</span>
+          <span className='text-cool-gray600'>{writer.nickname}</span>
           <div className='flex items-center gap-1'>
             <Image src={ic_heart} alt='좋아요 하트' width={16}></Image>
-            <span>10</span>
+            <span className='text-cool-gray500'>{likeCount}</span>
           </div>
         </div>
-        <div>2024. 04. 16</div>
+        <div className='text-cool-gray400'>{createdDate}</div>
       </div>
     </div>
   );
--- components/best-articles.tsx
+++ components/best-articles.tsx
@@ -1,11 +1,50 @@
 import BestArticleComponent from './best-article-component';
+import { useEffect, useState } from 'react';
+import { getArticles } from '@/lib/getArticles';
+
+interface ArticlesListProps {
+  totalCount?: number;
+  list: ArticleProps[];
+}
+interface ArticleProps {
+  updatedAt: string;
+  createdAt: string;
+  likeCount: number;
+  writer: Writer;
+  image: string;
+  content: string;
+  title: string;
+  id: number;
+}
+interface Writer {
+  nickname: string;
+  id: number;
+}
+
+const PAGE_NUM = 1;
+const PAGE_SIZE = 3;
+const ORDERBY = 'like';
 
 export default function BestArticles() {
+  const [bestArticles, setBestArticles] = useState<ArticleProps[]>([]);
+
+  useEffect(() => {
+    const fetchBestArticles = async () => {
+      try {
+        const data = await getArticles(PAGE_NUM, PAGE_SIZE, ORDERBY);
+        setBestArticles(data);
+      } catch (error) {
+        console.error('Failed to fetch items:', error);
+      }
+    };
+    fetchBestArticles();
+  }, []);
+
   return (
     <div className='flex gap-6 md:gap-4'>
-      <BestArticleComponent />
-      <BestArticleComponent />
-      <BestArticleComponent />
+      {bestArticles.map(article => (
+        <BestArticleComponent key={article.id} {...article} />
+      ))}
     </div>
   );
 }
