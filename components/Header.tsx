7b214c9c956b11f060020855d384064949636a97
yehaYoo <beauciel00@gmail.com>
2024-05-31 03:35:49 +0900

    feat: 불필요한 버튼 삭제 및 css 초기화

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,59 +4,52 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import styles from "./Header.module.css";
 
-interface UserStateButtonProps {
-  isAddItemPage: boolean;
-}
-
-function UserStateButton({ isAddItemPage }: UserStateButtonProps) {
-  return isAddItemPage ? (
-    <button className={styles.profileButton}>
-      <Image src="/images/profile.svg" alt="프로필 사진" />
-    </button>
-  ) : (
-    <Link href="/signin">
-      <button className={styles.loginButton}>로그인</button>
-    </Link>
-  );
-}
-
 function Navbar() {
   const router = useRouter();
   const isAddItemPage = router.pathname === "/additem";
   const isFreeBoardPage = router.pathname === "/items";
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navMenu}>
-        <Link href="/" className={styles.logo}>
-          <Image
-            src="/images/logo.svg"
-            className={styles.logoImg}
-            width={153}
-            height={51}
-            alt="panda-market"
-          />
-        </Link>
-        <Link
-          href="/items"
-          className={`${styles.navbarButton} ${
-            isFreeBoardPage ? "styles.activeLink" : ""
-          }`}
-        >
-          자유게시판
-        </Link>
-        <Link
-          href="/additem"
-          className={`${styles.navbarButton} ${
-            isAddItemPage ? styles.activeLink : ""
-          }`}
-        >
-          중고마켓
-        </Link>
-      </div>
-      <div>
-        <UserStateButton isAddItemPage={isAddItemPage} />
-      </div>
-    </nav>
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.navMenu}>
+          <Link href="/" className={styles.logo}>
+            <Image
+              src="/images/logo.svg"
+              className={styles.logoImg}
+              width={153}
+              height={51}
+              alt="panda-market"
+            />
+          </Link>
+          <Link
+            href="/items"
+            className={`${styles.navbarButton} ${
+              isFreeBoardPage ? "styles.activeLink" : ""
+            }`}
+          >
+            자유게시판
+          </Link>
+          <Link
+            href="/additem"
+            className={`${styles.navbarButton} ${
+              isAddItemPage ? styles.activeLink : ""
+            }`}
+          >
+            중고마켓
+          </Link>
+        </div>
+        <div>
+          <button className={styles.profileButton}>
+            <Image
+              src="/images/profile.svg"
+              alt="프로필 사진"
+              width={153}
+              height={51}
+            />
+          </button>
+        </div>
+      </nav>
+    </>
   );
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import React from "react";
+import "../styles/reset.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
