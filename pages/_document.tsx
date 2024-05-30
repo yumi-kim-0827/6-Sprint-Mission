f6993f3b823ac5b18b057261c61b235482457b07
cindyCho <hjcho2303@naver.com>
2024-05-31 08:26:03 +0900

    Test: 배포 오류 수정 테스트

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='ko'>
-      <Head>
-        <title>판다마켓</title>
-        <meta name='description' content='The best flea market in the world - Home' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
