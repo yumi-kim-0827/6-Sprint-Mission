d0b7ccd871c74ede8b755c27b8d05d88da81d39d
cindyCho <hjcho2303@naver.com>
2024-05-30 09:09:09 +0900

    Feat: axios 및 api 추가
    Articles 받아오는 함수 생성

---
+++ lib/getArticles.ts
@@ -0,0 +1,33 @@
+import instance from './axios';
+
+interface ListProps {
+  updatedAt: string;
+  createdAt: string;
+  likeCount: number;
+  writer: Writer;
+  image: string;
+  content: string;
+  title: string;
+  id: number;
+}
+
+interface Writer {
+  nickname: string;
+  id: number;
+}
+
+export const getArticles = async (
+  page: number,
+  pageSize: number,
+  orderBy: string,
+  keyword?: string
+): Promise<ListProps[]> => {
+  const query = `page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`;
+  try {
+    const response = await instance.get(`/articles?${query}`);
+    return response.data.list ?? [];
+  } catch (error) {
+    console.error(`Failed to fetch items: ${error}`);
+    throw new Error('정보를 불러오는데 실패했습니다');
+  }
+};
