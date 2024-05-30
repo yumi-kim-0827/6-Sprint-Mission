2384650a324d4bb6de7b9a8ba6471da2fe412b55
eunbinnie <dmsqlsdl54@naver.com>
2024-05-30 18:46:14 +0900

    feat: 헤더 컴포넌트 pc 완성

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import pandaLogo from "@/src/assets/icons/panda_face.svg";
+import Image from "next/image";
+import LinkButton from "./LinkButton";
+
+const Header = () => {
+  return (
+    <header className="flex sticky top-0 py-[15px] pl-6 pr-[34px] bg-white border-b border-b-[#dfdfdf] z-10">
+      <div className="w-full flex justify-between items-center flex-wrap max-w-[1520px] mx-auto">
+        <div className="flex justify-center items-center">
+          <Link href="/" className="flex justify-center items-center gap-2">
+            <div className="hidden w-10 h-10 sm:block">
+              <Image src={pandaLogo} alt="판다마켓 로고 이미지" />
+            </div>
+            <span className="font-ROKAFSans logo-text">판다마켓</span>
+          </Link>
+          <ul className="flex justify-center items-center ml-4 sm:ml-5 md:ml-8 gap-2">
+            <li className="md:min-w-[110px] text-center">
+              <Link href="/free" className="text-gray-600 font-bold text-lg">
+                자유게시판
+              </Link>
+            </li>
+            <li className="md:min-w-[110px] text-center">
+              <Link href="/items" className="text-gray-600 font-bold text-lg">
+                중고마켓
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <LinkButton href={"/signin"}>로그인</LinkButton>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+import type { UrlObject } from "url";
+import type { ChildrenProps } from "@/types/common.interface";
+
+interface LinkButtonProps extends ChildrenProps {
+  href: string | UrlObject;
+}
+
+const LinkButton = ({ children, href }: LinkButtonProps) => {
+  return (
+    <Link
+      href={href}
+      className="py-3 px-[23px] rounded-lg font-semibold bg-blue text-white flex justify-center items-center leading-11"
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default LinkButton;
