180968f73c39b0798ae93059107cdb87cea065d7
cindyCho <hjcho2303@naver.com>
2024-05-30 14:49:20 +0900

    Feat: 데이터 연결 및 검색기능 구현

--- components/articles.tsx
+++ components/articles.tsx
@@ -1,19 +1,55 @@
-import AricleComponent from './article-component';
+import Image from 'next/image';
+import ArticleComponent from './article-component';
+import ic_search from '../public/images/ic_search.png';
 import SelectBox from './select-box';
+import { getArticles, ListProps } from '@/lib/getArticles';
+import { ChangeEvent, useState, useEffect } from 'react';
+
+const PAGE_NUM = 1;
+const PAGE_SIZE = 10;
 
 export default function Articles() {
+  const [orderby, setOrderby] = useState('like');
+  const [keyword, setKeyword] = useState('');
+  const [articles, setArticles] = useState<ListProps[]>([]);
+
+  const handleOrderClick = (sortType: string): void => {
+    setOrderby(sortType);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setKeyword(value);
+  };
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const data = await getArticles(PAGE_NUM, PAGE_SIZE, orderby, keyword);
+        setArticles(data);
+      } catch (error) {
+        console.error('Failed to fetch items:', error);
+      }
+    };
+    fetchArticles();
+  }, [orderby, keyword]);
+
   return (
     <div>
-      <div className='flex justify-between gap-2 mb-6'>
-        <input type='text' className='w-[293px] h-[42px] bg-cool-gray100 rounded-xl md:w-[560px] lg:w-[1054px]' />
-        <SelectBox />
+      <div className='relative flex justify-between gap-2 mb-6'>
+        <Image src={ic_search} width={24} alt='돋보기' className='absolute z-10 top-2 left-3' />
+        <input
+          onChange={handleChange}
+          type='text'
+          placeholder='검색할 상품을 입력해주세요'
+          className='placeholder-cool-gray400 w-[293px] h-[42px] pl-9 bg-cool-gray100 rounded-xl md:w-[560px] lg:w-[1054px] relative left-[1px] focus:border'
+        />
+        <SelectBox handleOrder={handleOrderClick} />
       </div>
       <div className='flex flex-col gap-6'>
-        <AricleComponent />
-        <AricleComponent />
-        <AricleComponent />
-        <AricleComponent />
-        <AricleComponent />
+        {articles.map(article => (
+          <ArticleComponent key={article.id} {...article} />
+        ))}
       </div>
     </div>
   );
