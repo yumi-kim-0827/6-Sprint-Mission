52c8231d1a05021b317b0c154f5778baca61d1bc
WooGi1020 <jungwk1020@gmail.com>
2024-05-30 19:42:19 +0900

    ✨ feat : Head 태그 및 타이틀 지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 import styles from "@/styles/Home.module.css";
+import Head from "next/head";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Head>
+        <title>판다마켓</title>
+      </Head>
+    </>
+  );
 }
