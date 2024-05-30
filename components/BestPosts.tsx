e25f7151a8bd573a9acb9d6f5e15164d04fe42a2
김보미 <kimbomi2172@naver.com>
2024-05-30 20:56:37 +0900

    ✨ feat: 날짜 함수 생성

--- components/BestPosts.tsx
+++ components/BestPosts.tsx
@@ -1,11 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { getBestPosts } from '@/api/api'
 import React, { useEffect, useState } from 'react'
+import icon_medal from '@/public/assets/icon_medal.png'
+import icon_favorite from '@/public/assets/icon_favorite.png'
+import Image from 'next/image'
+import styles from '@/styles/posts.module.css'
+import formatDate from '@/utils/formatDate'
 
 type bestPostsData = {
   id: number
   title: string
   content: string
-  image: string
+  image?: string | null
   likeCount: number
   createdAt: string
   updatedAt: string
@@ -32,18 +38,38 @@ export default function BestPosts() {
   }, [])
 
   return (
-    <div>
-      <div>
-        {bestPosts.map((post) => {
-          return (
-            <>
-              <div key={post.id}>
-                <p>{post.title}</p>
+    <div className={styles.bestPostBox}>
+      {bestPosts.map((post) => {
+        return (
+          <>
+            <div key={post.id}>
+              <div className={styles.medal}>
+                <Image src={icon_medal} alt="메달" width={16} height={16} />
+                <p>Best</p>
               </div>
-            </>
-          )
-        })}
-      </div>
+              <div className={styles.titleImage}>
+                <p>{post.title}</p>
+                {post.image && (
+                  <div className={styles.postImg}>
+                    <Image
+                      src={post.image}
+                      alt="포스트 이미지"
+                      width={48}
+                      height={48}
+                    />
+                  </div>
+                )}
+              </div>
+              <div>
+                <p>{post.writer.nickname}</p>
+                <Image src={icon_favorite} alt="하트" width={16} height={16} />
+                <p>{post.likeCount}</p>
+                <p>{formatDate(post.createdAt)}</p>
+              </div>
+            </div>
+          </>
+        )
+      })}
     </div>
   )
 }
