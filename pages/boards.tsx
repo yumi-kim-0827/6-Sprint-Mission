eb445b07636abfe3c98db815c42d2fd10fdd5de6
gohansaem92 <gohansaem@gmail.com>
2024-05-31 22:01:51 +0900

    fix: 네트리파이 빌드오류

--- pages/boards.tsx
+++ pages/boards.tsx
@@ -14,42 +14,49 @@ export default function BoardPage() {
     const res = await axios.get(
       `/articles?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`
     );
-    const nextActicles = res.data.list;
+    const nextActicles = res.data.list ?? [];
     setBoardList(nextActicles);
   }
   async function getBestList(page = 1, pageSize = 3) {
     const res = await axios.get(
       `/articles?page=${page}&pageSize=${pageSize}&orderBy=like`
     );
-    const nextBestList = res.data.list;
+    const nextBestList = res.data.list ?? [];
     setBestList(nextBestList);
   }
 
   useEffect(() => {
     getBoardList();
-    getBestList();
   }, [keyword, orderBy]);
+
+  useEffect(() => {
+    getBestList();
+  }, []);
 
   return (
     <>
       <SearchForm keyword={keyword} onChangeKeyword={setKeyword} />
       <DropDown order={orderBy} onChangeOrder={setOrderBy} />
-      {bestList.map((article) => {
-        return (
-          <li id={article.id}>
-            <BoardItem article={article} />
-            best like article
-          </li>
-        );
-      })}
-      {boardList.map((article) => {
-        return (
-          <li id={article.id}>
-            <BoardItem article={article} />
-            ------------------------------------
-          </li>
-        );
-      })}
+      <ul>
+        {bestList.map((article) => {
+          return (
+            <li key={article.id}>
+              <BoardItem article={article} />
+              best like article
+            </li>
+          );
+        })}
+      </ul>
+      <ul>
+        {boardList.map((article) => {
+          return (
+            <li key={article.id}>
+              <BoardItem article={article} />
+              ------------------------------------
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
