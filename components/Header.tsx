b690f65cf1c72cb664915a0be8502222e838327c
gohansaem92 <gohansaem@gmail.com>
2024-05-31 19:51:42 +0900

    feat:설정페이지 테마기능 구현

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
-      <div>로고</div>
-      <div>자유게시판</div>
-      <div>중고마켓</div>
-      <div>로그인 프로필</div>
+      <Link href="/">
+        <div>로고</div>
+      </Link>
+      <Link href="/boards">
+        <div>자유게시판</div>
+      </Link>
+      <Link href="/market">
+        <div>중고마켓</div>
+      </Link>
+      <Link href="/join">
+        <div>로그인</div>
+      </Link>
+      <Link href="/setting">
+        <div>설정</div>
+      </Link>
     </header>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import Container from "@/components/Container";
 import Header from "@/components/Header";
+import ThemeProvider from "@/lib/ThemeContext";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider>
       <Header />
       <Container>
         <Component {...pageProps} />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
