39f71fcaa2d778a067c91f95c538ddb9cc91db95
김보미 <kimbomi2172@naver.com>
2024-05-31 19:11:33 +0900

    ✨ feat: 베스트 게시글 pageSize에 따른 개수 반영 구현

--- api/api.ts
+++ api/api.ts
@@ -3,11 +3,12 @@ import instance from '@/lib/axios'
 export async function getPosts({ orderBy = '', keyword = '' }) {
   const query = `orderBy=${orderBy}&keyword=${keyword}`
   const response = await instance.get(`/articles?${query}`)
-
   return response.data.list
 }
 
-export async function getBestPosts() {
-  const response = await instance.get('/articles?orderBy=like')
+export async function getBestPosts({ pageSize = 3 }) {
+  const response = await instance.get(
+    `/articles?&pageSize=${pageSize}&orderBy=like`
+  )
   return response.data.list
 }
--- components/BestPosts.tsx
+++ components/BestPosts.tsx
@@ -23,62 +23,107 @@ type bestPostsData = {
 
 export default function BestPosts() {
   const [bestPosts, setBestPosts] = useState<bestPostsData[]>([])
+  const [pageSize, setPageSize] = useState(3)
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchBestPosts = async () => {
       try {
-        const data = await getBestPosts()
+        const data = await getBestPosts({ pageSize: pageSize })
         setBestPosts(data)
       } catch (error) {
         console.error('posts 가져오는데 문제 발생', error)
       }
     }
 
-    fetchData()
+    fetchBestPosts()
+  }, [pageSize])
+
+  function getBestPostsPerPage(screenSize: string) {
+    switch (screenSize) {
+      case 'desktop':
+        return 3
+      case 'tablet':
+        return 2
+      case 'mobile':
+        return 1
+      default:
+        return 3
+    }
+  }
+
+  function handleMediaQueryChange() {
+    if (typeof window !== 'undefined') {
+      const mqlDesktop = window.matchMedia('(min-width: 1200px)')
+      const mqlTablet = window.matchMedia(
+        '(min-width: 768px) and (max-width: 1199px)'
+      )
+
+      const screenSize = mqlDesktop.matches
+        ? 'desktop'
+        : mqlTablet.matches
+        ? 'tablet'
+        : 'mobile'
+      const bestPostsPerPage = getBestPostsPerPage(screenSize)
+      setPageSize(bestPostsPerPage)
+
+      return screenSize
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const mqlDesktop = window.matchMedia('(min-width: 1200px)')
+      const mqlTablet = window.matchMedia(
+        '(min-width: 768px) and (max-width: 1199px)'
+      )
+
+      const mediaQueryListener = () => handleMediaQueryChange()
+
+      mqlDesktop.addEventListener('change', mediaQueryListener)
+      mqlTablet.addEventListener('change', mediaQueryListener)
+
+      handleMediaQueryChange()
+
+      return () => {
+        mqlDesktop.removeEventListener('change', mediaQueryListener)
+        mqlTablet.removeEventListener('change', mediaQueryListener)
+      }
+    }
   }, [])
 
   return (
-    <>
-      {bestPosts.map((post) => {
-        return (
-          <>
-            <div className={styles.bestPostBox}>
-              <div key={post.id}>
-                <div className={styles.medal}>
-                  <Image src={icon_medal} alt="메달" width={16} height={16} />
-                  <p>Best</p>
-                </div>
-                <div className={styles.titleImage}>
-                  <h3 className={styles.titleP}>{post.title}</h3>
-                  {post.image && (
-                    <div className={styles.postImg}>
-                      <Image
-                        src={post.image}
-                        alt="포스트 이미지"
-                        width={48}
-                        height={48}
-                      />
-                    </div>
-                  )}
-                </div>
-                <div className={styles.bestPostFooter}>
-                  <div className={styles.writerContent}>
-                    <p className={styles.writer}>{post.writer.nickname}</p>
-                    <Image
-                      src={icon_favorite}
-                      alt="하트"
-                      width={16}
-                      height={16}
-                    />
-                    <p className={styles.writer}>{post.likeCount}</p>
-                  </div>
-                  <p className={styles.date}>{formatDate(post.createdAt)}</p>
-                </div>
-              </div>
+    <div className={styles.container}>
+      {bestPosts.map((post) => (
+        <div className={styles.bestPostBox} key={post.id}>
+          <div>
+            <div className={styles.medal}>
+              <Image src={icon_medal} alt="메달" width={16} height={16} />
+              <p>Best</p>
             </div>
-          </>
-        )
-      })}
-    </>
+            <div className={styles.titleImage}>
+              <h3 className={styles.titleP}>{post.title}</h3>
+              {post.image && (
+                <div className={styles.postImg}>
+                  <Image
+                    src={post.image}
+                    alt="포스트 이미지"
+                    width={48}
+                    height={48}
+                  />
+                </div>
+              )}
+            </div>
+            <div className={styles.bestPostFooter}>
+              <div className={styles.writerContent}>
+                <p className={styles.writer}>{post.writer.nickname}</p>
+                <Image src={icon_favorite} alt="하트" width={16} height={16} />
+                <p className={styles.writer}>{post.likeCount}</p>
+              </div>
+              <p className={styles.date}>{formatDate(post.createdAt)}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
