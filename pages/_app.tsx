4b9230e14d6d182c2e28d8d3b6b9fb52780b5506
songa <thddk0121@gmail.com>
2024-05-31 21:12:30 +0900

    style: styled-components 설치 후 GlobalStyles.ts 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/src/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { GlobalStyle } from '@/src/styles/shared/Globalstyles';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
