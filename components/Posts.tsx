916584cfbc73a74a5d89990b010fedc08fb59e56
김보미 <kimbomi2172@naver.com>
2024-05-31 12:46:11 +0900

    💄 ui: 검색, 드롭박스 UI 구현

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -5,6 +5,9 @@ import formatDate from '@/utils/formatDate'
 import Image from 'next/image'
 import icon_favorite from '@/public/assets/icon_favorite.png'
 import img_profile from '@/public/assets/img_profile.png'
+import icon_search from '@/public/assets/icon_search.png'
+import icon_order from '@/public/assets/icon_order.png'
+import icon_dropdown from '@/public/assets/icon_dropdown.png'
 
 type PostsData = {
   id: number
@@ -21,7 +24,15 @@ type PostsData = {
 }
 
 export default function Posts() {
+  const selectOptions = [
+    { value: 'createdAt', label: '최신순' },
+    { value: 'favoriteCount', label: '좋아요순' },
+  ]
+
   const [posts, setPosts] = useState<PostsData[]>([])
+  const [keyword, setKeyword] = useState('')
+  const [isDropdownView, setDropdownView] = useState(false)
+  const [order, setOrder] = useState(selectOptions[0].value)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,9 +50,59 @@ export default function Posts() {
     }
 
     fetchData()
-  }, [])
+  }, [order, keyword])
+
+  const handleKeywordSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value)
+  }
+
+  const toggleDropdown = () => {
+    setDropdownView(!isDropdownView)
+  }
+
+  const selectOption = (value: string) => {
+    setOrder(value)
+    setDropdownView(false)
+  }
+
   return (
     <div>
+      <div className={styles.navSearch}>
+        <div className={styles.search}>
+          <div className={styles.searchIcon}>
+            <Image src={icon_search} alt="검색 아이콘" width={24} height={24} />
+          </div>
+          <input
+            className={styles.searchInput}
+            placeholder="검색할 상품을 입력해주세요"
+            onChange={handleKeywordSearch}
+          />
+        </div>
+        <div className={styles.dropdown} onClick={toggleDropdown}>
+          <picture>
+            <source
+              srcSet={icon_order.src}
+              media="all and (max-width: 768px)"
+            />
+            <span className={styles.valueName}>
+              {selectOptions.find((option) => option.value === order)?.label}
+            </span>
+            <Image src={icon_dropdown} alt="드롭다운" width={24} height={24} />
+          </picture>
+          {isDropdownView && (
+            <ul className={styles.dropdownMenu}>
+              {selectOptions.map((option) => (
+                <li
+                  key={option.value}
+                  onClick={() => selectOption(option.value)}
+                >
+                  {option.label}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
       <div>
         {posts.map((post) => {
           return (
