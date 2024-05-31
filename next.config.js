ce7967d3150e9f0bdfef6f328516afae20514345
Lin <shl6534@naver.com>
2024-05-31 19:11:30 +0900

    chore: 패키지 설치 및 초기 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/Sprint_Mission/**',
+      },
+    ],
+  },
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+module.exports = nextConfig;
