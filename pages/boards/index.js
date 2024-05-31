bb8e6b619e8281eaf8bff5e8eac50d8c89579959
songa <thddk0121@gmail.com>
2024-05-31 11:31:15 +0900

    feat: BoardPage 컴포넌트 추가

---
+++ pages/boards/index.js
@@ -0,0 +1,12 @@
+export default function BoardPage() {
+  return (
+    <>
+      <section>
+        <h2>베스트 게시글</h2>
+      </section>
+      <section>
+        <h2>게시글</h2>
+      </section>
+    </>
+  );
+}
