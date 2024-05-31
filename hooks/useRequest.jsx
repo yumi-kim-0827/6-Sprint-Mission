b23698446eecfa0abd911cd60fb6b3ab813f8b15
Lin <shl6534@naver.com>
2024-05-31 22:24:31 +0900

    feat: useRequest 훅 생성

---
+++ hooks/useRequest.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { dispatcher } from "lib/axios";
+
+export default function useRequest({ options, skip = false, deps = [] }) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const requestFunc = async (...args) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await dispatcher({ ...options, ...args });
+
+      setData(() => response);
+      return response;
+    } catch (err) {
+      setError(() => err);
+      return err;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (skip) return;
+    requestFunc();
+  }, deps);
+
+  return { data, isLoading, error, requestFunc };
+}
