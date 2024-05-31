46fc08528a2b5ce6df10987bcc054e94dd31fc93
codefug <robot9917@naver.com>
2024-05-31 22:57:40 +0900

    feat: search 기능

--- pages/boards.tsx
+++ pages/boards.tsx
@@ -10,7 +10,7 @@ import {
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 export const getStaticProps = (async () => {
   const likeRes = await fetch(`${BASE_URL}/articles?pageSize=3&&orderBy=like`);
@@ -28,8 +28,18 @@ export default function BoardsPage({
   const [sort, setSort] = useState<string>("recent");
   const [isLoading, setLoading] = useState<boolean>(true);
   const [keyword, setKeyword] = useState<string>("");
+  const [value, setValue] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setKeyword(value);
+  };
 
   const handleClick = (e: ChangeEvent<HTMLDivElement>) => {};
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(() => e.target.value);
+  };
 
   useEffect(() => {
     fetch(`${BASE_URL}/articles?orderBy=${sort}&&keyword=${keyword}`)
@@ -48,28 +58,33 @@ export default function BoardsPage({
       </Head>
       <article className="flex flex-col gap-4 mt-4 md:mt-6">
         <header className="text-xl font-bold">베스트 게시글</header>
-        <section className="flex gap-0 md:gap-4 lg:gap-6">
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0 md:gap-4 lg:gap-6">
           <UpperMobileComponent>{likeList[0].content}</UpperMobileComponent>
           <UpperTabletComponent>{likeList[1].content}</UpperTabletComponent>
           <UpperDesktopComponent>{likeList[2].content}</UpperDesktopComponent>
         </section>
       </article>
       <article className="mt-10">
-        <form>
-          <header className="text-xl font-bold flex justify-between items-center">
-            게시글
-            <div className="text-base">
-              <Button>글쓰기</Button>
-            </div>
-          </header>
-          <section className="flex items-center gap-2 md:gap-4">
+        <header className="text-xl font-bold flex justify-between items-center">
+          게시글
+          <div className="text-base">
+            <Button>글쓰기</Button>
+          </div>
+        </header>
+        <form onSubmit={handleSubmit}>
+          <section className="flex items-center gap-2 md:gap-4 mt-4 md:mt-6 mb-6">
             <input
               type="text"
               name="search"
               placeholder="검색할 상품을 입력해주세요."
-              className="h-[42px] rounded-xl flex-grow py-2 pl-11 bg-[#f3f4f6] bg-input-placeholder bg-no-repeat bg-left bg-[16px]"
+              onChange={handleChange}
+              value={value}
+              className="h-[42px] rounded-xl flex-grow py-2 pl-11 bg-[#f3f4f6] bg-input-placeholder bg-no-repeat bg-[16px]"
             />
-            <button type="button">
+            <button
+              type="button"
+              className="flex items-center border-[#e5e7eb] border rounded-xl p-[9px] md:px-5 md:py-3 text-base h-[42px]"
+            >
               <OnlyMobileComponent>
                 <Image
                   className="md:hidden"
@@ -80,14 +95,16 @@ export default function BoardsPage({
                 />
               </OnlyMobileComponent>
               <UpperTabletComponent>
-                {sort === "recent" ? "최신 순" : "좋아요 순"}
-                <Image
-                  className="hidden md:inline"
-                  width={24}
-                  height={24}
-                  src="/icons/arrowDown.png"
-                  alt="sort button"
-                />
+                <div className="flex gap-[14px]">
+                  {sort === "recent" ? "최신 순" : "좋아요 순"}
+                  <Image
+                    className="hidden md:inline"
+                    width={24}
+                    height={24}
+                    src="/icons/arrowDown.png"
+                    alt="sort button"
+                  />
+                </div>
               </UpperTabletComponent>
             </button>
           </section>
--- shared/ui/button.tsx
+++ shared/ui/button.tsx
@@ -4,7 +4,7 @@ export function Button({ children }: PropsWithChildren) {
   return (
     <button
       type="button"
-      className="bg-[#3692ff] py-3 px-[23px] rounded-lg text-white"
+      className="bg-[#3692ff] py-3 px-[23px] rounded-lg text-white hover:bg-[#1967D6] disabled:bg-[#9CA3AF]"
     >
       {children}
     </button>
