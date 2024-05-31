fd87be5cab4ccc0bbd779e58e5fd496cae60dd14
songa <thddk0121@gmail.com>
2024-05-31 22:13:08 +0900

    style: 레이아웃을 위한 Container.tsx 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app';
+import Container from '@/src/components/layout/Container';
 import { GlobalStyle } from '@/src/styles/shared/Globalstyles';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
     </>
   );
 }
