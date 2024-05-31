28155c60975767985d98cc1a7430815deb2cc19a
yeonilil <gwagseoyeon@gmail.com>
2024-05-31 19:51:06 +0900

    Feat: post list 컴포넌트

--- mission/pages/boards.js
+++ mission/pages/boards.js
@@ -1,16 +1,30 @@
 import BestPostBoard from "@/components/BestPostBoard";
 import PostList from "@/components/PostList";
+import { useState, useEffect } from "react";
+import axios from "@/pages/api/axios";
+import styles from "@/styles/boards.module.scss";
 
 export default function Boards() {
+  const [posts, setPosts] = useState([]);
+  async function getPosts() {
+    const res = await axios.get("/articles");
+    const nextData = res.data.list ?? [];
+    setPosts(nextData);
+  }
+
+  useEffect(() => {
+    getPosts();
+    console.log(posts);
+  }, []);
   return (
     <div>
-      <h1>베스트 게시글</h1>
+      <h1 className={styles.title}>베스트 게시글</h1>
       <BestPostBoard />
-      <div>
-        <h1>게시글</h1>
-        <button>글쓰기</button>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>게시글</h1>
+        <button className={styles.writeButton}>글쓰기</button>
       </div>
-      <PostList />
+      <PostList posts={posts} />
     </div>
   );
 }
