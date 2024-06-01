2cda32c4cbe2fbfdaf9eba8175c9ac3b2e52455d
Lin <shl6534@naver.com>
2024-06-01 15:40:26 +0900

    feat: useDeviceState 훅 추가

---
+++ hooks/useDeviceState.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+const MIN_TABLET_WIDTH = 744;
+const MIN_DESKTOP_WIDTH = 1200;
+
+export enum Device {
+  MOBILE = "mobile",
+  TABLET = "tablet",
+  PC = "pc",
+}
+
+function getDeviceState() {
+  const width = window.innerWidth;
+
+  if (width < MIN_TABLET_WIDTH) return Device.MOBILE;
+  if (width < MIN_DESKTOP_WIDTH) return Device.TABLET;
+  return Device.PC;
+}
+
+export default function useDeviceState() {
+  const [deviceState, setDeviceState] = useState<Device>();
+
+  useEffect(() => {
+    function handleResize() {
+      window.requestAnimationFrame(() => {
+        setDeviceState(getDeviceState());
+      });
+    }
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return deviceState;
+}
