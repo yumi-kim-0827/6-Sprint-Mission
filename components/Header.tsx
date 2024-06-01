ef79da9795e29289fa5224b23546c213d5a1043c
YoungJun Bae <dudwns0213@gmail.com>
2024-06-02 05:19:38 +0900

    💄style: Header css 구현

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,69 @@
 import Image from "next/image";
 import Link from "next/link";
 import pandaLogo from "@/public/pandalogo.svg";
+import pandatext from "@/public/pandatext.svg";
 import userProfile from "@/public/userprofile.svg";
 import styles from "@/styles/Header.module.css";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter();
+
+  const pathname = router.pathname;
+
   return (
     <>
       <div className={styles.Header}>
         <div className={styles["Header-container"]}>
           <Link href="/">
-            <Image src={pandaLogo} alt="판다마켓 로고" />
+            <Image
+              className={styles["Header-logo"]}
+              src={pandaLogo}
+              alt="판다마켓 로고"
+            />
+            <Image
+              className={styles["Header-logo-mob"]}
+              src={pandatext}
+              alt="모바일 판다마켓 로고"
+            />
           </Link>
-          <Link href="/boards">자유게시판</Link>
-          <Link href="/">중고마켓</Link>
+          <div className={styles["Header-link-container"]}>
+            <Link
+              className={
+                styles[
+                  pathname === "/boards" ? "Header-link-act" : "Header-link"
+                ]
+              }
+              style={{ textDecoration: "none" }}
+              href="/boards"
+            >
+              자유게시판
+            </Link>
+            <Link
+              className={
+                styles[
+                  pathname === "/items" ? "Header-link-act" : "Header-link"
+                ]
+              }
+              style={{ textDecoration: "none" }}
+              href="/"
+            >
+              중고마켓
+            </Link>
+          </div>
         </div>
-        <div>
+        <div className={styles["Header-profile"]}>
           <Link href="/">
-            <Image src={userProfile} alt="유저 프로필 사진" />
+            <Image
+              width={40}
+              height={40}
+              src={userProfile}
+              alt="유저 프로필 사진"
+            />
           </Link>
         </div>
       </div>
+      <div className={styles["Header-empty"]}></div>
     </>
   );
 }
