3e207aa40684ef480ab8d21609c7e42e86d25d5a
YoungJun Bae <dudwns0213@gmail.com>
2024-06-02 06:41:13 +0900

    💄style: Article css 구현

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,5 +1,8 @@
 import { ArticleType } from "@/types/type";
 import Image from "next/image";
+import profileimg from "@/public/userprofile.svg";
+import heart from "@/public/heart.svg";
+import styles from "@/styles/Article.module.css";
 
 interface ArticleProps {
   //게시글 내부
@@ -17,23 +20,33 @@ function Article({ article }: ArticleProps) {
   const year = new Date(article.updatedAt).getFullYear();
   const createdDate = `${year}. ${month}. ${date}`;
   return (
-    <>
-      <div>리뷰 표시</div>
-      <div>{article.title}</div>
-      {article.image && (
-        <Image
-          src={article.image}
-          alt={`${article.title} 글 이미지`}
-          width={48}
-          height={44}
-        />
-      )}
-
-      <div>{article.writer.nickname}</div>
-      <div>{article.writer.id}</div>
-      <div>{article.likeCount}</div>
-      <div>{createdDate}</div>
-    </>
+    <div className={styles["Article-container"]}>
+      <div className={styles["Article-header"]}>
+        <div className={styles["Article-header-title"]}>{article.title}</div>
+        {article.image && (
+          <Image
+            className={styles["Article-header-img"]}
+            src={article.image}
+            alt={`${article.title} 게시글 이미지`}
+            width={44}
+            height={44}
+          />
+        )}
+      </div>
+      <div className={styles["Article-user"]}>
+        <Image src={profileimg} alt="유저 프로필 이미지" width={24} />
+        <div className={styles["Article-user-name"]}>
+          {article.writer.nickname}
+        </div>
+        <div className={styles["Article-date"]}>{createdDate}</div>
+        <div className={styles["Article-like"]}>
+          <Image src={heart} alt="하트!" width={24} />
+          <div className={styles["Article-like-text"]}>
+            {article.likeCount}+
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
