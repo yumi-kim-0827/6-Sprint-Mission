fff471374abd97bb17adb7c2dd585c3b60ef8476
YoungJun Bae <dudwns0213@gmail.com>
2024-06-02 04:25:36 +0900

    ✨featL app.tsx 작성 및 title 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>판다마켓</title>
+        <link rel="icon" href="/public/pandalogo.svg" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
